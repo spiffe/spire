7706386d1ae6e90e9b58fae8808cbc25b1f74b3a
walmav <madhu@skytale.io>
2017-07-26 15:30:52 -0700

    fix rebase issues

--- plugins/noderesolver/noop/noop.go
+++ plugins/noderesolver/noop/noop.go
@@ -6,6 +6,17 @@ import (
 	"github.com/spiffe/control-plane/plugins/noderesolver/proto"
 )
 
+type NoOp struct{}
+
+func (NoOp) Resolve(physicalSpiffeIdList []string) (resolutions map[string]*proto.NodeResolutionList, err error) {
+	return resolutions, nil
+}
+
+func (NoOp) Configure(configuration string) error {
+	return nil
+}
+
+
 func main() {
 	plugin.Serve(&plugin.ServeConfig{
 		HandshakeConfig: noderesolver.Handshake,
