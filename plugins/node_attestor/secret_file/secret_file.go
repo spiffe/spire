ccb0d0a7a461088a009a047b675caca75a82485e
Mariano Kunzi <kunzi.mariano@gmail.com>
2017-07-27 12:48:07 -0300

    Added secret file plugin for node_attestor.

--- plugins/node_attestor/secret_file/secret_file.go
+++ plugins/node_attestor/secret_file/secret_file.go
@@ -1,5 +1,26 @@
 package secretfile
 
-func main() {
+import (
+	"github.com/hashicorp/go-plugin"
+	"github.com/spiffe/node-agent/plugins/node_attestor"
+)
 
+type SecretFilePlugin struct{}
+
+func (SecretFilePlugin) FetchAttestationData() (attestationData []byte, err error) {
+	return []byte{}, nil
+}
+
+func (SecretFilePlugin) Configure(configuration string) error {
+	return nil
+}
+
+func main() {
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: nodeattestor.Handshake,
+		Plugins: map[string]plugin.Plugin{
+			"nodeattestor": nodeattestor.NodeAttestorPlugin{NodeAttestorImpl: &SecretFilePlugin{}},
+		},
+		GRPCServer: plugin.DefaultGRPCServer,
+	})
 }
