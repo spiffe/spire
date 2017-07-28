2a3b9794c3088a2ca828d5a0fb534f06fb7b38e9
Mariano Kunzi <kunzi.mariano@gmail.com>
2017-07-28 12:30:27 -0300

    Added sqlite plugin for data_store.

---
+++ plugins/data_store/interface.go
@@ -0,0 +1,67 @@
+package datastore
+
+import (
+	"net/rpc"
+
+	"google.golang.org/grpc"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/hashicorp/go-plugin"
+	"github.com/spiffe/control-plane/plugins/data_store/proto"
+)
+
+// Handshake is a common handshake that is shared between noderesolution and host.
+var Handshake = plugin.HandshakeConfig{
+	ProtocolVersion:  1,
+	MagicCookieKey:   "datastore_handshake",
+	MagicCookieValue: "datastore",
+}
+
+// PluginMap is the map of plugins we can dispense.
+var PluginMap = map[string]plugin.Plugin{
+	"datastore": &DataStorePlugin{},
+}
+
+type DataStore interface {
+	CreateFederatedEntry(*proto.FederatedBundle) error
+	ListFederatedEntry() (*proto.FederatedEntries, error)
+	UpdateFederatedEntry(*proto.FederatedBundle) error
+	DeleteFederatedEntry(*proto.Key) error
+	CreateAttestedNodeEntry(*proto.AttestedNodeEntry) error
+	FetchAttestedNodeEntry(*proto.Key) (*proto.AttestedNodeEntry, error)
+	FetchStaleNodeEntries() (*proto.AttestedNodes, error)
+	UpdateAttestedNodeEntry(*proto.AttestedNodeUpdate) error
+	DeleteAttestedNodeEntry(*proto.Key) error
+	CreateSelectorMapEntry(*proto.SelectorMapEntry) error
+	FetchSelectorMapEntry(*proto.Key) error
+	DeleteSelectorMapEntry(*proto.SelectorMapEntry) error
+	CreateRegistrationEntry(*proto.RegisteredEntry) error
+	FetchRegistrationEntry(*proto.RegisteredEntryKey) (*proto.RegisteredEntry, error)
+	UpdateRegistrationEntry(*proto.RegisteredEntry) error
+	DeleteRegistrationEntry(*proto.RegisteredEntryKey) error
+	FetchGroupedRegistrationEntries(*proto.GroupedRegistrationKey) (*proto.RegisteredEntries, error)
+	ListAttestorEntries(*proto.AttestorKey) (*proto.FederatedEntries, error)
+	ListSelectorEntries(*proto.SelectorKey) (*proto.FederatedEntries, error)
+	ListSpiffeEntries(*proto.Key) (*proto.FederatedEntries, error)
+}
+
+type DataStorePlugin struct {
+	DataStoreImpl DataStore
+}
+
+func (p DataStorePlugin) Server(*plugin.MuxBroker) (interface{}, error) {
+	return empty.Empty{}, nil
+}
+
+func (p DataStorePlugin) Client(b *plugin.MuxBroker, c *rpc.Client) (interface{}, error) {
+	return empty.Empty{}, nil
+}
+
+func (p DataStorePlugin) GRPCServer(s *grpc.Server) error {
+	proto.RegisterDataStoreServer(s, &GRPCServer{DataStoreImpl: p.DataStoreImpl})
+	return nil
+}
+
+func (p DataStorePlugin) GRPCClient(c *grpc.ClientConn) (interface{}, error) {
+	return &GRPCClient{client: proto.NewDataStoreClient(c)}, nil
+}
--- plugins/data_store/sqlite/sqlite_test.go
+++ plugins/data_store/sqlite/sqlite_test.go
@@ -1,1 +1,85 @@
-package main_test
+package sqlite
+
+import (
+	"testing"
+)
+
+func TestMemory_CreateFederatedEntry(t *testing.T) {
+
+}
+
+func TestMemory_ListFederatedEntry(t *testing.T) {
+
+}
+
+func TestMemory_UpdateFederatedEntry(t *testing.T) {
+
+}
+
+func TestMemory_DeleteFederatedEntry(t *testing.T) {
+
+}
+
+func TestMemory_CreateAttestedNodeEntry(t *testing.T) {
+
+}
+
+func TestMemory_FetchAttestedNodeEntry(t *testing.T) {
+
+}
+
+func TestMemory_FetchStaleNodeEntries(t *testing.T) {
+
+}
+
+func TestMemory_UpdateAttestedNodeEntry(t *testing.T) {
+
+}
+
+func TestMemory_DeleteAttestedNodeEntry(t *testing.T) {
+
+}
+
+func TestMemory_CreateSelectorMapEntry(t *testing.T) {
+
+}
+
+func TestMemory_FetchSelectorMapEntry(t *testing.T) {
+
+}
+
+func TestMemory_DeleteSelectorMapEntry(t *testing.T) {
+
+}
+
+func TestMemory_CreateRegistrationEntry(t *testing.T) {
+
+}
+
+func TestMemory_FetchRegistrationEntry(t *testing.T) {
+
+}
+
+func TestMemory_UpdateRegistrationEntry(t *testing.T) {
+
+}
+
+func TestMemory_DeleteRegistrationEntry(t *testing.T) {
+
+}
+
+func TestMemory_FetchGroupedRegistrationEntries(t *testing.T) {
+
+}
+
+func TestMemory_ListAttestorEntries(t *testing.T) {
+
+}
+
+func TestMemory_ListSelectorEntries(t *testing.T) {
+
+}
+
+func TestMemory_ListSpiffeEntries(t *testing.T) {
+
+}
