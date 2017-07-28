1076c413bad75db34e538e13bf5c3531ff2b9b4c
Mariano Kunzi <kunzi.mariano@gmail.com>
2017-07-28 12:34:07 -0300

    Fixed prefix for method names in sqlite_test.go

--- plugins/data_store/sqlite/sqlite_test.go
+++ plugins/data_store/sqlite/sqlite_test.go
@@ -4,82 +4,82 @@ import (
 	"testing"
 )
 
-func TestMemory_CreateFederatedEntry(t *testing.T) {
+func Sqlite_CreateFederatedEntry(t *testing.T) {
 
 }
 
-func TestMemory_ListFederatedEntry(t *testing.T) {
+func Sqlite_ListFederatedEntry(t *testing.T) {
 
 }
 
-func TestMemory_UpdateFederatedEntry(t *testing.T) {
+func Sqlite_UpdateFederatedEntry(t *testing.T) {
 
 }
 
-func TestMemory_DeleteFederatedEntry(t *testing.T) {
+func Sqlite_DeleteFederatedEntry(t *testing.T) {
 
 }
 
-func TestMemory_CreateAttestedNodeEntry(t *testing.T) {
+func Sqlite_CreateAttestedNodeEntry(t *testing.T) {
 
 }
 
-func TestMemory_FetchAttestedNodeEntry(t *testing.T) {
+func Sqlite_FetchAttestedNodeEntry(t *testing.T) {
 
 }
 
-func TestMemory_FetchStaleNodeEntries(t *testing.T) {
+func Sqlite_FetchStaleNodeEntries(t *testing.T) {
 
 }
 
-func TestMemory_UpdateAttestedNodeEntry(t *testing.T) {
+func Sqlite_UpdateAttestedNodeEntry(t *testing.T) {
 
 }
 
-func TestMemory_DeleteAttestedNodeEntry(t *testing.T) {
+func Sqlite_DeleteAttestedNodeEntry(t *testing.T) {
 
 }
 
-func TestMemory_CreateSelectorMapEntry(t *testing.T) {
+func Sqlite_CreateSelectorMapEntry(t *testing.T) {
 
 }
 
-func TestMemory_FetchSelectorMapEntry(t *testing.T) {
+func Sqlite_FetchSelectorMapEntry(t *testing.T) {
 
 }
 
-func TestMemory_DeleteSelectorMapEntry(t *testing.T) {
+func Sqlite_DeleteSelectorMapEntry(t *testing.T) {
 
 }
 
-func TestMemory_CreateRegistrationEntry(t *testing.T) {
+func Sqlite_CreateRegistrationEntry(t *testing.T) {
 
 }
 
-func TestMemory_FetchRegistrationEntry(t *testing.T) {
+func Sqlite_FetchRegistrationEntry(t *testing.T) {
 
 }
 
-func TestMemory_UpdateRegistrationEntry(t *testing.T) {
+func Sqlite_UpdateRegistrationEntry(t *testing.T) {
 
 }
 
-func TestMemory_DeleteRegistrationEntry(t *testing.T) {
+func Sqlite_DeleteRegistrationEntry(t *testing.T) {
 
 }
 
-func TestMemory_FetchGroupedRegistrationEntries(t *testing.T) {
+func Sqlite_FetchGroupedRegistrationEntries(t *testing.T) {
 
 }
 
-func TestMemory_ListAttestorEntries(t *testing.T) {
+func Sqlite_ListAttestorEntries(t *testing.T) {
 
 }
 
-func TestMemory_ListSelectorEntries(t *testing.T) {
+func Sqlite_ListSelectorEntries(t *testing.T) {
 
 }
 
-func TestMemory_ListSpiffeEntries(t *testing.T) {
+func Sqlite_ListSpiffeEntries(t *testing.T) {
 
 }
