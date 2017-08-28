186a956051fa09251d33e3c265c9783a649026e0
walmav <madhu@scytale.io>
2017-08-27 23:35:13 -0700

    Update memory_test.go

--- plugin/agent/keymanager-memory/memory_test.go
+++ plugin/agent/keymanager-memory/memory_test.go
@@ -13,33 +13,33 @@ import (
 func TestMemory_GenerateKeyPair(t *testing.T) {
 	var plugin MemoryPlugin
 	data, e := plugin.GenerateKeyPair(&keymanager.GenerateKeyPairRequest{})
-	assert.NoError(t,e)
+	require.NoError(t,e)
 	priv, err :=x509.ParseECPrivateKey(data.PrivateKey)
-	assert.NoError(t,err)
+	require.NoError(t,err)
 	assert.Equal(t, plugin.key,priv)
-	assert.NotEmpty(t,data)
 }
 
 func TestMemory_FetchPrivateKey(t *testing.T) {
 	var plugin MemoryPlugin
 	data, e :=plugin.GenerateKeyPair(&keymanager.GenerateKeyPairRequest{})
-	assert.NoError(t,err)
+	require.NoError(t,err)
 
 	priv, e := plugin.FetchPrivateKey(&keymanager.FetchPrivateKeyRequest{})
-	assert.NoError(t,err)
+	require.NoError(t,err)
 	assert.Equal(t, priv.PrivateKey, data.PrivateKey)
 }
 
 func TestMemory_Configure(t *testing.T) {
 	var plugin MemoryPlugin
 	data, e := plugin.Configure(&sriplugin.ConfigureRequest{})
+	require.NoError(t,err)
 	assert.Equal(t, &sriplugin.ConfigureResponse{}, data)
-	assert.NoError(t,err)
+
 }
 
 func TestMemory_GetPluginInfo(t *testing.T) {
 	var plugin MemoryPlugin
 	data, e := plugin.GetPluginInfo(&sriplugin.GetPluginInfoRequest{})
+	require.NoError(t,err)
 	assert.Equal(t, &sriplugin.GetPluginInfoResponse{}, data)
-	assert.NoError(t,err)
 }
