d6fcb7aba8350efbd0752978df8c5245b8633b07
Mariano Kunzi <kunzi.mariano@gmail.com>
2017-08-28 19:37:09 -0300

    Added GetSpiffeIDFromCSR to CA  service.

--- services/ca.go
+++ services/ca.go
@@ -1,10 +1,18 @@
 package services
 
-import "github.com/spiffe/sri/pkg/server/ca"
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+
+	"github.com/spiffe/go-spiffe/uri"
+	"github.com/spiffe/sri/pkg/server/ca"
+)
 
 //CA service interface.
 type CA interface {
 	SignCsr(csr []byte) (cert []byte, err error)
+	GetSpiffeIDFromCSR(csr []byte) (spiffeID string, err error)
 }
 
 //CAImpl is an implementation of the CA interface.
@@ -20,4 +28,23 @@ func NewCAImpl(serverCA ca.ControlPlaneCa) *CAImpl {
 //SignCsr with the stored intermediate certificate.
 func (ca *CAImpl) SignCsr(csr []byte) (cert []byte, err error) {
 	return ca.serverCA.SignCsr(csr)
+}
+
+//GetSpiffeIDFromCSR extracts an SpiffeID from a CSR
+func (ca *CAImpl) GetSpiffeIDFromCSR(csr []byte) (spiffeID string, err error) {
+	block, _ := pem.Decode(csr)
+	var parsedCSR *x509.CertificateRequest
+	if parsedCSR, err = x509.ParseCertificateRequest(block.Bytes); err != nil {
+		return spiffeID, err
+	}
+
+	var uris []string
+	uris, err = uri.GetURINamesFromExtensions(&parsedCSR.Extensions)
+
+	if len(uris) != 1 {
+		return spiffeID, errors.New("The CSR must have exactly one URI SAN")
+	}
+
+	spiffeID = uris[0]
+	return spiffeID, nil
 }
