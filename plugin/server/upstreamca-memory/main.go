b1a51708ff4f971d03de029f91e0ef10d4032b15
Agustín Martínez Fayó <amartinezfayo@users.noreply.github.com>
2017-08-31 23:25:28 -0300

    Add more logging
    - Add more logging in spire-server start
    - Add more logging in CA plugin
    - Add more logging in UpstreamCA plugin

--- plugin/server/upstreamca-memory/main.go
+++ plugin/server/upstreamca-memory/main.go
@@ -4,9 +4,12 @@ import (
 	"github.com/hashicorp/go-plugin"
 	"github.com/spiffe/sri/pkg/server/upstreamca"
 	"github.com/spiffe/sri/plugin/server/upstreamca-memory/pkg"
+	"log"
 )
 
 func main() {
+	log.Print("Starting plugin")
+
 	ca, err := pkg.NewWithDefault("../../plugin/server/upstreamca-memory/pkg/_test_data/keys/private_key.pem", "../../plugin/server/upstreamca-memory/pkg/_test_data/keys/cert.pem")
 	if err != nil {
 		panic(err.Error())
