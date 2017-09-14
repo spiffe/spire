7d264dab88394e915513f74df9edd2c82613bc9b
Agustín Martínez Fayó <amartinezfayo@users.noreply.github.com>
2017-09-14 18:36:34 -0300

    Have the selector type as a constant string

--- plugin/agent/workloadattestor-unix/unix.go
+++ plugin/agent/workloadattestor-unix/unix.go
@@ -14,6 +14,8 @@ import (
 
 type UnixPlugin struct{}
 
+const selectorType string = "unix"
+
 func (UnixPlugin) Attest(req *workloadattestor.AttestRequest) (*workloadattestor.AttestResponse, error) {
 	log.Printf("Attesting PID: %v", req.Pid)
 
@@ -32,11 +34,11 @@ func (UnixPlugin) Attest(req *workloadattestor.AttestRequest) (*workloadattestor
 
 	// Check if it's only the effective UID
 	if len(uids) == 1 {
-		resp.Selectors = append(resp.Selectors, &common.Selector{Type: "unix", Value: fmt.Sprintf("uid:%v", uids[0])})
+		resp.Selectors = append(resp.Selectors, &common.Selector{Type: selectorType, Value: fmt.Sprintf("uid:%v", uids[0])})
 	} else if len(uids) > 1 {
 		// We got at least real and effective UIDs
 		// use the effective UID
-		resp.Selectors = append(resp.Selectors, &common.Selector{Type: "unix", Value: fmt.Sprintf("uid:%v", uids[1])})
+		resp.Selectors = append(resp.Selectors, &common.Selector{Type: selectorType, Value: fmt.Sprintf("uid:%v", uids[1])})
 	} else {
 		return &resp, errors.New(fmt.Sprintf("Unable to get effective UID for PID: %v", req.Pid))
 	}
@@ -49,11 +51,11 @@ func (UnixPlugin) Attest(req *workloadattestor.AttestRequest) (*workloadattestor
 
 	// Check if it's only the effective GID
 	if len(gids) == 1 {
-		resp.Selectors = append(resp.Selectors, &common.Selector{Type: "unix", Value: fmt.Sprintf("gid:%v", gids[0])})
+		resp.Selectors = append(resp.Selectors, &common.Selector{Type: selectorType, Value: fmt.Sprintf("gid:%v", gids[0])})
 	} else if len(gids) > 1 {
 		// We got at least real and effective GIDs
 		// use the effective GID
-		resp.Selectors = append(resp.Selectors, &common.Selector{Type: "unix", Value: fmt.Sprintf("gid:%v", gids[1])})
+		resp.Selectors = append(resp.Selectors, &common.Selector{Type: selectorType, Value: fmt.Sprintf("gid:%v", gids[1])})
 	} else {
 		return &resp, errors.New(fmt.Sprintf("Unable to get effective GID for PID: %v", req.Pid))
 	}
