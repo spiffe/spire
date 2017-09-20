89a2809af9fb6cd59803196ba78fb0e4f9b8cd61
Evan Gilman <evan@scytale.io>
2017-09-20 14:30:53 -0700

    Update mocks for identity service and fix bugs

--- pkg/server/endpoints/node/service.go
+++ pkg/server/endpoints/node/service.go
@@ -11,6 +11,7 @@ import (
 	"github.com/spiffe/go-spiffe/uri"
 	pb "github.com/spiffe/spire/pkg/api/node"
 	"github.com/spiffe/spire/pkg/common"
+	"github.com/spiffe/spire/pkg/common/selector"
 	"github.com/spiffe/spire/pkg/common/util"
 	"github.com/spiffe/spire/pkg/server/ca"
 	"github.com/spiffe/spire/pkg/server/datastore"
@@ -252,9 +253,9 @@ func (s *service) fetchRegistrationEntries(selectors []*common.Selector, spiffeI
 	var entries []*common.RegistrationEntry
 	var selectorsEntries []*common.RegistrationEntry
 
-	set = selector.NewSet(selectors)
-	for s := range set.Power() {
-		listSelectorResponse, err := s.dataStore.ListSelectorEntries(&datastore.ListSelectorEntriesRequest{Selectors: s.Raw()})
+	set := selector.NewSet(selectors)
+	for subset := range set.Power() {
+		listSelectorResponse, err := s.dataStore.ListSelectorEntries(&datastore.ListSelectorEntriesRequest{Selectors: subset.Raw()})
 		if err != nil {
 			return nil, err
 		}
