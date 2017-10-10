2856545108788b9df63077755f5931f0a5459f5b
Evan Gilman <evan@scytale.io>
2017-10-10 15:20:08 -0700

    Update registration CLI for better usability This commit updates the registration CLI to provide a full set of flags for creating registration entries. A data file can also be used by specifying the path flag.
    See #234 for additional improvements that are needed.
    
    This commit solves #217

--- cmd/spire-server/cli/command/register.go
+++ cmd/spire-server/cli/command/register.go
@@ -2,105 +2,220 @@ package command
 
 import (
 	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
 	"io/ioutil"
-	"os"
-	"reflect"
-
-	"golang.org/x/net/context"
-
-	"log"
+	"strings"
 
 	"github.com/spiffe/spire/proto/api/registration"
 	"github.com/spiffe/spire/proto/common"
+
+	"golang.org/x/net/context"
 )
 
-type RegisterCommand struct {
-	Client registration.RegistrationClient
+type RegisterConfig struct {
+	// Address of SPIRE server
+	Addr string
+
+	// Path to an optional data file. If set, other
+	// opts will be ignored.
+	Path string
+
+	// Type and value are delimited by a colon (:)
+	// ex. "unix:uid:1000" or "spiffe_id:spiffe://example.org/foo"
+	Selectors SelectorFlag
+
+	ParentID string
+	SpiffeID string
+	Ttl      int
 }
 
-func (*RegisterCommand) Help() string {
-	return "Usage: spire-server register <data-file>"
+// Perform basic validation, even on fields that we
+// have defaults defined for
+func (rc *RegisterConfig) Validate() error {
+	if rc.Addr == "" {
+		return errors.New("a server address is required")
+	}
+
+	// If a path is set, we have all we need
+	if rc.Path != "" {
+		return nil
+	}
+
+	if len(rc.Selectors) < 1 {
+		return errors.New("at least one selector is required")
+	}
+
+	if rc.ParentID == "" {
+		return errors.New("a parent ID is required")
+	}
+
+	if rc.SpiffeID == "" {
+		return errors.New("a SPIFFE ID is required")
+	}
+
+	if rc.Ttl < 0 {
+		return errors.New("a TTL is required")
+	}
+
+	return nil
 }
 
-func (c *RegisterCommand) Run(args []string) int {
-	var err error
+type Register struct{}
 
-	// TODO: Make address configurable
-	if c.Client == nil {
-		c.Client, err = newRegistrationClient(defaultServerAddr)
-		if err != nil {
-			log.Fatalf("Failed: %v", err)
-			return -1
-		}
-	}
+func (Register) Synopsis() string {
+	return "Creates registration entries"
+}
 
-	// Get filename
-	if len(args) != 1 {
-		log.Fatalf("Exactly one argument expected but got %d", len(args))
-		return -1
-	}
-	dataFile := args[0]
-	if _, err := os.Stat(dataFile); os.IsNotExist(err) {
-		log.Fatalf("File not found: %s", dataFile)
-		return -1
-	}
+func (r Register) Help() string {
+	_, err := r.newConfig([]string{"-h"})
+	return err.Error()
+}
 
-	// Load entries from data file
-	entries := &common.RegistrationEntries{}
-	dat, err := ioutil.ReadFile(dataFile)
+func (r Register) Run(args []string) int {
+	config, err := r.newConfig(args)
 	if err != nil {
-		log.Fatalf("Failed: %v", err)
-		return -1
+		fmt.Println(err.Error())
+		return 1
 	}
 
-	json.Unmarshal(dat, &entries)
-
-	// Inject each entry and verify it
-	for index, registeredEntry := range entries.Entries {
-		log.Printf("Creating entry #%d...\n", index+1)
-		entityID, err := c.createEntry(registeredEntry)
-		if err != nil {
-			log.Fatalf("Failed: %v", err)
-			return -1
-		}
-		valid, err := c.validateEntry(entityID, registeredEntry)
-		if err != nil {
-			log.Fatalf("Failed: %v", err)
-			return -1
-		}
-		if valid {
-			log.Printf("Fetched entity %s is OK!\n\n", entityID)
-		} else {
-			log.Printf("Fetched entity %s mismatch! Aborting...\n", entityID)
-			return -1
-		}
+	if err = config.Validate(); err != nil {
+		fmt.Println(err.Error())
+		return 1
 	}
 
-	log.Printf("Registration OK!\n")
+	var entries []*common.RegistrationEntry
+	if config.Path != "" {
+		entries, err = r.parseFile(config.Path)
+	} else {
+		entries, err = r.parseConfig(config)
+	}
+	if err != nil {
+		fmt.Println(err.Error())
+		return 1
+	}
+
+	c, err := newRegistrationClient(config.Addr)
+	if err != nil {
+		fmt.Println(err.Error())
+		return 1
+	}
+
+	err = r.registerEntries(c, entries)
+	if err != nil {
+		fmt.Println(err.Error())
+		return 1
+	}
 
 	return 0
 }
 
-func (*RegisterCommand) Synopsis() string {
-	return "Registers data in server"
-}
-
-func (c *RegisterCommand) createEntry(registeredEntry *common.RegistrationEntry) (string, error) {
-	result, err := c.Client.CreateEntry(context.Background(), registeredEntry)
-	if err != nil {
-		return "", err
-	}
-	return result.Id, nil
-}
-
-func (c *RegisterCommand) validateEntry(entityID string, registeredEntry *common.RegistrationEntry) (ok bool, err error) {
-	registrationEntryID := &registration.RegistrationEntryID{Id: entityID}
-	fetchedRegisteredEntry, err := c.Client.FetchEntry(context.Background(), registrationEntryID)
-	if err != nil {
-		return
+// parseConfig builds a registration entry from the given config
+func (r Register) parseConfig(c *RegisterConfig) ([]*common.RegistrationEntry, error) {
+	e := &common.RegistrationEntry{
+		ParentId: c.ParentID,
+		SpiffeId: c.SpiffeID,
+		Ttl:      int32(c.Ttl),
 	}
 
-	ok = reflect.DeepEqual(fetchedRegisteredEntry, registeredEntry)
+	selectors := []*common.Selector{}
+	for _, s := range c.Selectors {
+		cs, err := r.parseSelector(s)
+		if err != nil {
+			return nil, err
+		}
 
-	return
+		selectors = append(selectors, cs)
+	}
+
+	e.Selectors = selectors
+	return []*common.RegistrationEntry{e}, nil
+}
+
+func (Register) parseFile(path string) ([]*common.RegistrationEntry, error) {
+	entries := &common.RegistrationEntries{}
+
+	dat, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	json.Unmarshal(dat, &entries)
+	return entries.Entries, nil
+}
+
+// parseSelector parses a CLI string from type:value into a selector type.
+// Everything to the right of the first ":" is considered a selector value.
+func (Register) parseSelector(str string) (*common.Selector, error) {
+	parts := strings.SplitAfterN(str, ":", 2)
+	if len(parts) < 2 {
+		return nil, fmt.Errorf("selector \"%s\" must be formatted as type:value", str)
+	}
+
+	s := &common.Selector{
+		// Strip the trailing delimiter
+		Type:  strings.TrimSuffix(parts[0], ":"),
+		Value: parts[1],
+	}
+	return s, nil
+}
+
+func (r Register) registerEntries(c registration.RegistrationClient, entries []*common.RegistrationEntry) error {
+	for _, e := range entries {
+		id, err := c.CreateEntry(context.TODO(), e)
+		if err != nil {
+			fmt.Println("FAILED to create the following entry:")
+			r.printEntry(e, "")
+			return err
+		}
+
+		r.printEntry(e, id.Id)
+	}
+
+	return nil
+}
+
+func (Register) printEntry(e *common.RegistrationEntry, id string) {
+	if id != "" {
+		fmt.Printf("Entry ID:\t%s\n", id)
+	}
+	fmt.Printf("SPIFFE ID:\t%s\n", e.SpiffeId)
+	fmt.Printf("Parent ID:\t%s\n", e.ParentId)
+	fmt.Printf("TTL:\t\t%v\n", e.Ttl)
+
+	for _, s := range e.Selectors {
+		fmt.Printf("Selector:\t%s:%s\n", s.Type, s.Value)
+	}
+
+	fmt.Println()
+}
+
+func (Register) newConfig(args []string) (*RegisterConfig, error) {
+	f := flag.NewFlagSet("register", flag.ContinueOnError)
+	c := &RegisterConfig{}
+
+	f.StringVar(&c.Addr, "serverAddr", defaultServerAddr, "Address of the SPIRE server")
+	f.StringVar(&c.ParentID, "parentID", "", "The SPIFFE ID of this record's parent")
+	f.StringVar(&c.SpiffeID, "spiffeID", "", "The SPIFFE ID that this record represents")
+	f.IntVar(&c.Ttl, "ttl", 3600, "A TTL, in seconds, for any SVID issued as a result of this record")
+
+	f.StringVar(&c.Path, "data", "", "Path to a file containing registration JSON (optional)")
+
+	f.Var(&c.Selectors, "selector", "A colon-delimeted type:value selector. Can be used more than once")
+
+	return c, f.Parse(args)
+}
+
+// Define a custom type for selectors. Doing
+// this allows us to support repeatable flags
+type SelectorFlag []string
+
+func (s *SelectorFlag) String() string {
+	return fmt.Sprint(*s)
+}
+
+func (s *SelectorFlag) Set(val string) error {
+	*s = append(*s, val)
+	return nil
 }
