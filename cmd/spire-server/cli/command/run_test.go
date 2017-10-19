60273be8bea6b7465fc570581808c5f420741935
paul-argeniss <paul@argeniss.com>
2017-10-19 19:16:45 -0300

    Added server run command tests.

---
+++ cmd/spire-server/cli/command/run_test.go
@@ -0,0 +1,65 @@
+package command
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseConfigGood(t *testing.T) {
+	c, err := parseFile("../../../../test/fixture/config/server_good.conf")
+	require.NoError(t, err)
+	assert.Equal(t, c.BindAddress, "127.0.0.1")
+	assert.Equal(t, c.BindPort, 8081)
+	assert.Equal(t, c.BindHTTPPort, 8080)
+	assert.Equal(t, c.TrustDomain, "example.org")
+	assert.Equal(t, c.PluginDir, "conf/server/plugin")
+	assert.Equal(t, c.LogLevel, "INFO")
+	assert.Equal(t, c.BaseSpiffeIDTTL, 999999)
+	assert.Equal(t, c.Umask, "")
+}
+
+func TestParseFlagsGood(t *testing.T) {
+	c, err := parseFlags([]string{
+		"-bindAddress=127.0.0.1",
+		"-bindHTTPPort=8080",
+		"-trustDomain=example.org",
+		"-pluginDir=conf/server/plugin",
+		"-logLevel=INFO",
+		"-baseSpiffeIDTTL=999999",
+		"-umask=",
+	})
+	require.NoError(t, err)
+	assert.Equal(t, c.BindAddress, "127.0.0.1")
+	assert.Equal(t, c.BindHTTPPort, 8080)
+	assert.Equal(t, c.TrustDomain, "example.org")
+	assert.Equal(t, c.PluginDir, "conf/server/plugin")
+	assert.Equal(t, c.LogLevel, "INFO")
+	assert.Equal(t, c.BaseSpiffeIDTTL, 999999)
+	assert.Equal(t, c.Umask, "")
+}
+
+func TestMergeConfigGood(t *testing.T) {
+	c := &CmdConfig{
+		BindAddress:     "127.0.0.1",
+		BindPort:        8081,
+		BindHTTPPort:    8080,
+		TrustDomain:     "example.org",
+		PluginDir:       "conf/server/plugin",
+		LogLevel:        "INFO",
+		BaseSpiffeIDTTL: 999999,
+		Umask:           "",
+	}
+	orig := newDefaultConfig()
+	err := mergeConfig(orig, c)
+	require.NoError(t, err)
+	assert.Equal(t, orig.BindAddress.IP.String(), "127.0.0.1")
+	assert.Equal(t, orig.BindHTTPAddress.IP.String(), "127.0.0.1")
+	assert.Equal(t, orig.BindAddress.Port, 8081)
+	assert.Equal(t, orig.BindHTTPAddress.Port, 8080)
+	assert.Equal(t, orig.TrustDomain.Scheme, "spiffe")
+	assert.Equal(t, orig.TrustDomain.Host, "example.org")
+	assert.Equal(t, orig.PluginDir, "conf/server/plugin")
+	assert.Equal(t, orig.Umask, 0077)
+}
