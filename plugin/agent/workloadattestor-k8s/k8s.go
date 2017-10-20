31c53f7c95e3d3148692088598f316290821aea9
Agustín Martínez Fayó <amartinezfayo@gmail.com>
2017-10-20 04:13:02 -0300

    Workload attestor plugin for k8s
    - Implementation of a workload attestor plugin for k8s
    To attest a given PID, the attestor relys on the /pods endpoint of the kubelet REST API, and the information provided by /proc/[pid]/cgroup
    
    This fixes #191

---
+++ plugin/agent/workloadattestor-k8s/k8s.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"sync"
+
+	"github.com/hashicorp/go-plugin"
+	"github.com/hashicorp/hcl"
+	"github.com/spiffe/spire/proto/agent/workloadattestor"
+	"github.com/spiffe/spire/proto/common"
+	spi "github.com/spiffe/spire/proto/common/plugin"
+)
+
+type k8sPlugin struct {
+	kubeletReadOnlyPort int
+	httpClient          httpClient
+	fs                  fileSystem
+	mtx                 *sync.RWMutex
+}
+
+type k8sPluginConfig struct {
+	KubeletReadOnlyPort int `hcl:"kubelet_read_only_port"`
+}
+
+type pod struct {
+	// We only care about namespace, serviceAccountName and containerID
+	Metadata struct {
+	} `json:"metadata"`
+	Items []struct {
+		Metadata struct {
+			Namespace string `json:"namespace"`
+		} `json:"metadata"`
+		Spec struct {
+			ServiceAccountName string `json:"serviceAccountName"`
+		} `json:"spec"`
+		Status struct {
+			ContainerStatuses []struct {
+				ContainerID string `json:"containerID"`
+			} `json:"containerStatuses"`
+		} `json:"status"`
+	} `json:"items"`
+}
+
+const (
+	selectorType string = "k8s"
+)
+
+func (p *k8sPlugin) Attest(req *workloadattestor.AttestRequest) (*workloadattestor.AttestResponse, error) {
+	p.mtx.RLock()
+	defer p.mtx.RUnlock()
+
+	log.Printf("Attesting PID: %v", req.Pid)
+	resp := workloadattestor.AttestResponse{}
+
+	httpResp, err := p.httpClient.Get(fmt.Sprintf("http://localhost:%v/pods", p.kubeletReadOnlyPort))
+	if err != nil {
+		return &resp, err
+	}
+	defer httpResp.Body.Close()
+	respBytes, err := ioutil.ReadAll(httpResp.Body)
+
+	var podInfo *pod
+	err = json.Unmarshal(respBytes, &podInfo)
+	if err != nil {
+		return &resp, err
+	}
+
+	cgroups, err := getCgroups(fmt.Sprintf("/proc/%v/cgroup", req.Pid), p.fs)
+	if err != nil {
+		return &resp, err
+	}
+
+	var containerID string
+	for _, cgroup := range cgroups {
+		// We are only interested in kube pods entries
+		if len(cgroup[2]) < 9 {
+			continue
+		}
+
+		substring := cgroup[2][:9]
+		if substring == "/kubepods" {
+			parts := strings.Split(cgroup[2], "/")
+
+			if len(parts) < 5 {
+				log.Printf("Kube pod entry found, but without container id: %v", substring)
+				continue
+			}
+			containerID = parts[4]
+			break
+		}
+	}
+
+	for _, item := range podInfo.Items {
+		for _, status := range item.Status.ContainerStatuses {
+			containerURL, err := url.Parse(status.ContainerID)
+			if err != nil {
+				return &resp, err
+			}
+
+			if containerID == containerURL.Host {
+				resp.Selectors = append(resp.Selectors, &common.Selector{Type: selectorType, Value: fmt.Sprintf("serviceAccountName:%v", item.Spec.ServiceAccountName)})
+				resp.Selectors = append(resp.Selectors, &common.Selector{Type: selectorType, Value: fmt.Sprintf("namespace:%v", item.Metadata.Namespace)})
+				log.Printf("Selectors found: %v", resp.Selectors)
+				return &resp, err
+			}
+		}
+	}
+
+	log.Print("No selectors found")
+	return &resp, err
+}
+
+func getCgroups(path string, fs fileSystem) (cgroups [][]string, err error) {
+	// http://man7.org/linux/man-pages/man7/cgroups.7.html
+	// https://www.kernel.org/doc/Documentation/cgroup-v2.txt
+
+	file, err := fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		token := scanner.Text()
+		substrings := strings.SplitN(token, ":", 3)
+		if len(substrings) < 3 {
+			return cgroups, fmt.Errorf("cgroup entry contains %v colons, but expected at least two colons: %v", len(substrings), token)
+		}
+		cgroups = append(cgroups, substrings)
+	}
+
+	return cgroups, err
+}
+
+func (p *k8sPlugin) Configure(req *spi.ConfigureRequest) (*spi.ConfigureResponse, error) {
+	p.mtx.Lock()
+	defer p.mtx.Unlock()
+
+	resp := &spi.ConfigureResponse{}
+
+	// Parse HCL config payload into config struct
+	config := &k8sPluginConfig{}
+	hclTree, err := hcl.Parse(req.Configuration)
+	if err != nil {
+		resp.ErrorList = []string{err.Error()}
+		return resp, err
+	}
+	err = hcl.DecodeObject(&config, hclTree)
+	if err != nil {
+		resp.ErrorList = []string{err.Error()}
+		return resp, err
+	}
+
+	// Set local vars from config struct
+	p.kubeletReadOnlyPort = config.KubeletReadOnlyPort
+	return &spi.ConfigureResponse{}, nil
+}
+
+func (*k8sPlugin) GetPluginInfo(*spi.GetPluginInfoRequest) (*spi.GetPluginInfoResponse, error) {
+	return &spi.GetPluginInfoResponse{}, nil
+}
+
+func New(client httpClient, fs fileSystem) workloadattestor.WorkloadAttestor {
+	return &k8sPlugin{
+		mtx:        &sync.RWMutex{},
+		httpClient: client,
+		fs:         fs,
+	}
+}
+
+func main() {
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: workloadattestor.Handshake,
+		Plugins: map[string]plugin.Plugin{
+			"wla_k8s": workloadattestor.WorkloadAttestorPlugin{WorkloadAttestorImpl: New(&http.Client{}, osFS{})},
+		},
+		GRPCServer: plugin.DefaultGRPCServer,
+	})
+}
