43a20cf0250cc5c3b7585045ac3eb6930dd76395
martincapello <m.a.capello@gmail.com>
2018-03-01 10:26:21 -0300

    Added URIs  length check.

--- pkg/agent/manager/util.go
+++ pkg/agent/manager/util.go
@@ -2,6 +2,7 @@ package manager
 
 import (
 	"crypto/x509"
+	"errors"
 
 	"github.com/spiffe/go-spiffe/uri"
 )
@@ -11,5 +12,10 @@ func getSpiffeIDFromSVID(svid *x509.Certificate) (string, error) {
 	if err != nil {
 		return "", err
 	}
+
+	if len(URIs) == 0 {
+		return "", errors.New("certificate does not have a SPIFFE ID")
+	}
+
 	return URIs[0], nil
 }
