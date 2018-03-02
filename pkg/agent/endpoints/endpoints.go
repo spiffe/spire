9f1a32630b39d508d4a85fc27c477235680981e1
walmav <madhu@scytale.io>
2018-03-02 12:12:10 -0800

    e2e build fixes

--- pkg/agent/endpoints/endpoints.go
+++ pkg/agent/endpoints/endpoints.go
@@ -2,12 +2,10 @@ package endpoints
 
 import (
 	"fmt"
-	"net"
-	"os"
-	"time"
-
 	"github.com/spiffe/spire/pkg/agent/auth"
 	"github.com/spiffe/spire/pkg/agent/endpoints/workload"
+	"net"
+	"os"
 
 	"google.golang.org/grpc"
 
@@ -60,15 +58,12 @@ func (e *endpoints) start(l net.Listener) error {
 
 func (e *endpoints) registerWorkloadAPI() {
 	w := &workload.Handler{
-		Bundle:   e.c.Bundle,
-		CacheMgr: e.c.Manager,
-		Catalog:  e.c.Catalog,
-		L:        e.c.Log.WithField("subsystem_name", "workload_api"),
-		MaxTTL:   1 * time.Minute,
-		MinTTL:   5 * time.Second,
+		Manager: e.c.Manager,
+		Catalog: e.c.Catalog,
+		L:       e.c.Log.WithField("subsystem_name", "workload_api"),
 	}
 
-	workload_pb.RegisterWorkloadServer(e.grpc, w)
+	workload_pb.RegisterSpiffeWorkloadAPIServer(e.grpc, w)
 }
 
 func (e *endpoints) createUDSListener() (net.Listener, error) {
