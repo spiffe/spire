e67c0dba02d2fd279ba3d82328d1cb8c023fddb9
martincapello <m.a.capello@gmail.com>
2018-03-06 15:17:05 -0800

    Removed  extra newline.

--- pkg/agent/agent.go
+++ pkg/agent/agent.go
@@ -202,7 +202,6 @@ func (a *Agent) newSVID(key *ecdsa.PrivateKey, bundle []*x509.Certificate) (*x50
 		Csr:          csr,
 	}
 	resp, err := c.FetchBaseSVID(context.TODO(), req)
-
 	if err != nil {
 		return nil, nil, fmt.Errorf("attesting to SPIRE server: %v", err)
 	}
