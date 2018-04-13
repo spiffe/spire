4556cba424a267d488a41582d527fe6857ea6d88
martincapello <m.a.capello@gmail.com>
2018-04-13 18:04:28 -0300

    Added new functions to create CA certificates and keys.
    Signed-off-by: martincapello <m.a.capello@gmail.com>

--- test/util/cert_generation.go
+++ test/util/cert_generation.go
@@ -7,14 +7,14 @@ import (
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"math/big"
-	"time"
-
 	mrand "math/rand"
+	"time"
 
 	"github.com/spiffe/go-spiffe/uri"
 )
 
-// Returns a default SVID template with the specified SPIFFE ID. Must be signed before it's valid.
+// NewSVIDTemplate returns a default SVID template with the specified SPIFFE ID. Must
+// be signed before it's valid.
 func NewSVIDTemplate(spiffeID string) (*x509.Certificate, error) {
 	cert := defaultSVIDTemplate()
 	err := addSpiffeExtension(spiffeID, cert)
@@ -22,18 +22,37 @@ func NewSVIDTemplate(spiffeID string) (*x509.Certificate, error) {
 	return cert, err
 }
 
-// Create a new self-signed certificate with the provided template.
+// NewCATemplate returns a default CA template with the specified trust domain. Must
+// be signed before it's valid.
+func NewCATemplate(trustDomain string) (*x509.Certificate, error) {
+	cert := defaultCATemplate()
+	err := addSpiffeExtension("spiffe://"+trustDomain, cert)
+
+	return cert, err
+}
+
+// SelfSign creates a new self-signed certificate with the provided template.
 func SelfSign(req *x509.Certificate) (*x509.Certificate, *ecdsa.PrivateKey, error) {
+	return Sign(req, req, nil)
+}
+
+// Sign creates a new certificate based on the provided template and signed using parent
+// certificate and signerPrivateKey.
+func Sign(req, parent *x509.Certificate, signerPrivateKey interface{}) (*x509.Certificate, *ecdsa.PrivateKey, error) {
 	key, err := ecdsa.GenerateKey(elliptic.P224(), rand.Reader)
 	if err != nil {
 		return nil, nil, err
+	}
+
+	if signerPrivateKey == nil {
+		signerPrivateKey = key
 	}
 
 	if req.SerialNumber == nil {
 		req.SerialNumber = randomSerial()
 	}
 
-	certData, err := x509.CreateCertificate(rand.Reader, req, req, key.Public(), key)
+	certData, err := x509.CreateCertificate(rand.Reader, req, parent, key.Public(), signerPrivateKey)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -60,6 +79,24 @@ func defaultSVIDTemplate() *x509.Certificate {
 			x509.KeyUsageKeyAgreement |
 			x509.KeyUsageDigitalSignature,
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
+		BasicConstraintsValid: true,
+	}
+}
+
+// Returns an CA template with many default values set.
+func defaultCATemplate() *x509.Certificate {
+	name := pkix.Name{
+		Country:      []string{"US"},
+		Organization: []string{"SPIRE"},
+	}
+	return &x509.Certificate{
+		Subject:   name,
+		Issuer:    name,
+		IsCA:      true,
+		NotBefore: time.Now(),
+		NotAfter:  time.Now().Add(1 * time.Hour),
+		KeyUsage:  x509.KeyUsageCertSign,
+		//ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
 		BasicConstraintsValid: true,
 	}
 }
