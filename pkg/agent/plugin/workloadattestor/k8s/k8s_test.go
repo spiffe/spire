d63a51d6d36c39d9c29f6e480ea52d495da17b21
Evan Gilman <evan@scytale.io>
2018-05-07 12:37:35 -0700

    [#450] Update kubernetes workload attestor to consider init container IDs Init container IDs live in a different part of the kubelet response than "regular" container IDs. This commit updates the kubernetes workload attestor to search for container IDs that belong to both init and non-init containers.
    Signed-off-by: Evan Gilman <evan@scytale.io>

--- pkg/agent/plugin/workloadattestor/k8s/k8s_test.go
+++ pkg/agent/plugin/workloadattestor/k8s/k8s_test.go
@@ -18,13 +18,14 @@ import (
 )
 
 const (
-	pid                   = 123
-	kubeletReadOnlyPort   = "10255"
-	validConfig           = `{"kubelet_read_only_port":"` + kubeletReadOnlyPort + `"}`
-	invalidConfig         = `{"kubelet_read_only_port":"invalid"}`
-	podListFilePath       = "../../../../../test/fixture/workloadattestor/k8s/pod_list.json"
-	cgPidInPodFilePath    = "../../../../../test/fixture/workloadattestor/k8s/cgroups_pid_in_pod.txt"
-	cgPidNotInPodFilePath = "../../../../../test/fixture/workloadattestor/k8s/cgroups_pid_not_in_pod.txt"
+	pid                    = 123
+	kubeletReadOnlyPort    = "10255"
+	validConfig            = `{"kubelet_read_only_port":"` + kubeletReadOnlyPort + `"}`
+	invalidConfig          = `{"kubelet_read_only_port":"invalid"}`
+	podListFilePath        = "../../../../../test/fixture/workloadattestor/k8s/pod_list.json"
+	cgPidInPodFilePath     = "../../../../../test/fixture/workloadattestor/k8s/cgroups_pid_in_pod.txt"
+	cgInitPidInPodFilePath = "../../../../../test/fixture/workloadattestor/k8s/cgroups_init_pid_in_pod.txt"
+	cgPidNotInPodFilePath  = "../../../../../test/fixture/workloadattestor/k8s/cgroups_pid_not_in_pod.txt"
 )
 
 func PluginGenerator(config string, client httpClient, fs fileSystem) (workloadattestor.WorkloadAttestor, *spi.ConfigureResponse, error) {
@@ -55,6 +56,29 @@ func TestK8s_AttestPidInPod(t *testing.T) {
 
 	mockFilesystem := filesystem_mock.NewMockfileSystem(mockCtrl)
 	mockFilesystem.EXPECT().Open(fmt.Sprintf("/proc/%v/cgroup", pid)).Return(os.Open(cgPidInPodFilePath))
+
+	plugin, _, err := PluginGenerator(validConfig, mockHttpClient, mockFilesystem)
+	req := workloadattestor.AttestRequest{Pid: int32(pid)}
+	resp, err := plugin.Attest(&req)
+	require.NoError(t, err)
+	require.NotEmpty(t, resp.Selectors)
+}
+
+func TestK8s_AttestInitPidInPod(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	podList, err := ioutil.ReadFile(podListFilePath)
+	require.NoError(t, err)
+
+	mockHttpClient := http_client_mock.NewMockhttpClient(mockCtrl)
+	mockHttpClient.EXPECT().Get("http://localhost:"+kubeletReadOnlyPort+"/pods").Return(
+		&http.Response{
+			Body: ioutil.NopCloser(bytes.NewReader(podList)),
+		}, nil)
+
+	mockFilesystem := filesystem_mock.NewMockfileSystem(mockCtrl)
+	mockFilesystem.EXPECT().Open(fmt.Sprintf("/proc/%v/cgroup", pid)).Return(os.Open(cgInitPidInPodFilePath))
 
 	plugin, _, err := PluginGenerator(validConfig, mockHttpClient, mockFilesystem)
 	req := workloadattestor.AttestRequest{Pid: int32(pid)}
