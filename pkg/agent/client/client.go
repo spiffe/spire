91660519f3374ab10e5dd8089261be8ba134c4a0
Marcos G. Yedro <marcosyedro@gmail.com>
2018-07-24 22:30:40 -0300

    Adding minor changes needed after merge
    Signed-off-by: Marcos G. Yedro <marcosyedro@gmail.com>

--- pkg/agent/client/client.go
+++ pkg/agent/client/client.go
@@ -171,7 +171,7 @@ func (c *client) FetchJWTSVID(ctx context.Context, jsr *node.JSR) (*JWTSVID, err
 	// We weren't able to make the request...close the client and return the error.
 	if err != nil {
 		c.Release()
-		c.c.Log.Errorf("%v: %v", ErrUnableToGetStream, err)
+		c.log.Errorf("%v: %v", ErrUnableToGetStream, err)
 		return nil, ErrUnableToGetStream
 	}
 
