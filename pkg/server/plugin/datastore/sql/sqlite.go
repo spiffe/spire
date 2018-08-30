301bf6e2021c8153d78586c567744f4db160ad66
Andrew Harding <azdagron@gmail.com>
2018-08-30 10:32:30 -0600

    fix journal_mode and foreign_keys support
    `journal_mode` and `foreign_key` settings are per-connection in
    sqlite3. The existing code sets the right PRAGMAs when the database is
    first opened, but that only impacts the first connection in the
    database/sql.(*DB) connection pool. When a new connection is created, it
    will not have those settings, which could lead to unecessary errors in
    the case of journal_mode (e.g. "database locked") or database
    inconsistencies in the case of foreign_keys.
    
    This change takes advantage of github.com/mattn/go-sqlite3 query value
    support for enabling those features for each connection. The code now
    parses the connection string, converts it into a file: URI if needed,
    and adds the proper _journal_mode and _foreign_keys query values.
    
    Signed-off-by: Andrew Harding <azdagron@gmail.com>

--- pkg/server/plugin/datastore/sql/sqlite.go
+++ pkg/server/plugin/datastore/sql/sqlite.go
@@ -1,6 +1,8 @@
 package sql
 
 import (
+	"net/url"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
@@ -8,18 +10,46 @@ import (
 type sqlite struct{}
 
 func (s sqlite) connect(connectionString string) (*gorm.DB, error) {
-	db, err := gorm.Open("sqlite3", connectionString)
+	embellished, err := embellishSQLite3ConnString(connectionString)
 	if err != nil {
-		return nil, sqlError.Wrap(err)
+		return nil, err
 	}
-	if err := db.Exec("PRAGMA journal_mode = WAL").Error; err != nil {
-		db.Close()
-		return nil, sqlError.Wrap(err)
-	}
-	if err := db.Exec("PRAGMA foreign_keys = ON").Error; err != nil {
-		db.Close()
+	db, err := gorm.Open("sqlite3", embellished)
+	if err != nil {
 		return nil, sqlError.Wrap(err)
 	}
 
 	return db, nil
+}
+
+// embellishSQLite3ConnString adds query values supported by
+// github.com/mattn/go-sqlite3 to enable journal mode and foreign key support.
+// These query values MUST be part of the connection string in order to be
+// enabled for *each* connection opened by db/sql. If the connection string is
+// not already a file: URI, it is converted first.
+func embellishSQLite3ConnString(connectionString string) (string, error) {
+	u, err := url.Parse(connectionString)
+	if err != nil {
+		return "", sqlError.Wrap(err)
+	}
+
+	switch {
+	case u.Scheme == "":
+		// connection string is a path. move the path section into the
+		// opaque section so it renders propertly for sqlite3, for example:
+		// data.db = file:data.db
+		// ./data.db = file:./data.db
+		// /data.db = file:/data.db
+		u.Scheme = "file"
+		u.Opaque, u.Path = u.Path, ""
+	case u.Scheme != "file":
+		// only no scheme (i.e. file path) or file scheme is supported
+		return "", sqlError.New("unsupported scheme %q", u.Scheme)
+	}
+
+	q := u.Query()
+	q.Set("_foreign_keys", "ON")
+	q.Set("_journal_mode", "WAL")
+	u.RawQuery = q.Encode()
+	return u.String(), nil
 }
