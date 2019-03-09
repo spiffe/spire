16ec4c3af162dca11d185dceada1ee6e98fedc22
Andrew Harding <azdagron@gmail.com>
2019-03-08 17:49:41 -0700

    Fixed agent hang on shutdown closing UDS gRPC server
    The UDS gRPC server credentials pull the peer PID off of the UDS.
    Obtaining the FD for the connection was via the File() method.
    Unfortunately, File() switches the underlying FD into blocking mode,
    which prevents gRPC from closing the conns appropriately.
    
    This changes up the credentials to instead obtain the FD (and do the
    control call) via the syscall.RawConn. Doing so allows gRPC to close
    down the connections appropriately, fixing the hang on
    (*grpc.Server).Close()
    
    Also removed the unnecessary call to close listeners, since the gRPC
    server does that for us.
    
    Signed-off-by: Andrew Harding <azdagron@gmail.com>

--- pkg/common/auth/uds_bsd.go
+++ pkg/common/auth/uds_bsd.go
@@ -3,34 +3,13 @@
 package auth
 
 import (
-	"net"
-
 	"golang.org/x/sys/unix"
 )
 
-func FromUDSConn(conn net.Conn) CallerInfo {
-	var info CallerInfo
-
-	uconn, ok := conn.(*net.UnixConn)
-	if !ok {
-		info.Err = ErrInvalidConnection
-		return info
-	}
-
-	file, err := uconn.File()
+func getPeerPID(fd uintptr) (pid int32, err error) {
+	result, err := unix.GetsockoptInt(int(fd), 0, 0x002) //getsockopt(fd, SOL_LOCAL, LOCAL_PEERPID)
 	if err != nil {
-		info.Err = err
-		return info
+		return 0, err
 	}
-	defer file.Close()
-
-	result, err := unix.GetsockoptInt(int(file.Fd()), 0, 0x002) //getsockopt(fd, SOL_LOCAL, LOCAL_PEERPID)
-	if err != nil {
-		info.Err = err
-		return info
-	}
-
-	info.Addr = uconn.RemoteAddr()
-	info.PID = int32(result)
-	return info
+	return int32(result), nil
 }
