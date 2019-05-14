29536b9874eba10bdc48ba8a8bd52e4f1932cdc8
Tyler Dixon <tylerd@uber.com>
2019-05-13 17:00:16 -0700

    [upstreamca/disk] support joining existing PKI
    Signed-off-by: Tyler Dixon <tylerd@uber.com>

--- pkg/server/plugin/upstreamca/disk/disk_test.go
+++ pkg/server/plugin/upstreamca/disk/disk_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/spiffe/spire/test/spiretest"
 	testutil "github.com/spiffe/spire/test/util"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 const (
@@ -81,6 +82,11 @@ func (s *DiskSuite) TestConfigureUsingEmptyKey() {
 	s.Require().Error(err)
 }
 
+func (s *DiskSuite) TestConfigureUsingEmptyCert() {
+	err := s.configureWith("_test_data/keys/EC/private_key.pem", "_test_data/keys/empty/cert.pem")
+	s.Require().Error(err)
+}
+
 func (s *DiskSuite) TestConfigureUsingUnknownKey() {
 	err := s.configureWith("_test_data/keys/unknonw/private_key.pem", "_test_data/keys/unknonw/cert.pem")
 	s.Require().Error(err)
@@ -102,6 +108,74 @@ func (s *DiskSuite) TestGetPluginInfo() {
 	s.Require().NotNil(res)
 }
 
+func (s *DiskSuite) TestImplicitRootFromCertFile() {
+	// On OSX
+	// openssl ecparam -name prime256v1 -genkey -noout -out root_key.pem
+	// openssl req -x509 -new -key root_key.pem -out root_cert.pem -config <(cat /etc/ssl/openssl.cnf ; printf "\n[v3]\nsubjectAltName=URI:spiffe://root\nbasicConstraints=CA:true") -extensions v3
+	// openssl ecparam -name prime256v1 -genkey -noout -out upstream_key.pem
+	// openssl req  -new -key upstream_key.pem -out upstream_csr.pem -config <(cat /etc/ssl/openssl.cnf ; printf "\n[v3]\nsubjectAltName=URI:spiffe://upstream\nbasicConstraints=CA:true") -extensions v3
+	// openssl x509 -req -CA root_cert.pem -CAkey root_key.pem -in upstream_csr.pem -out upstream_cert.pem -CAcreateserial -extfile <(cat /etc/ssl/openssl.cnf ; printf "\n[v3]\nsubjectAltName=URI:spiffe://upstream\nbasicConstraints=CA:true") -extensions v3
+	// cat upstream_cert.pem root_cert.pem > upstream_and_root.pem
+	require := s.Require()
+
+	err := s.configureWith("_test_data/keys/EC/upstream_key.pem", "_test_data/keys/EC/upstream_and_root.pem")
+	require.NoError(err)
+
+	csrPEM, err := ioutil.ReadFile("_test_data/csr_valid/csr_1.pem")
+	require.NoError(err)
+	block, rest := pem.Decode(csrPEM)
+	require.Len(rest, 0)
+
+	resp, err := s.p.SubmitCSR(ctx, &upstreamca.SubmitCSRRequest{Csr: block.Bytes})
+	require.NoError(err)
+	require.NotNil(resp)
+	require.NotNil(resp.SignedCertificate)
+
+	testCSRResp(s.T(), resp, []string{"spiffe://localhost", "spiffe://upstream"}, []string{"spiffe://root"})
+}
+
+func (s *DiskSuite) TestExplicitBundle() {
+	require := s.Require()
+
+	_, err := s.p.Configure(ctx, &spi.ConfigureRequest{
+		Configuration: `{
+  "key_file_path": "_test_data/keys/EC/upstream_key.pem",
+  "cert_file_path": "_test_data/keys/EC/upstream_cert.pem",
+  "bundle_file_path": "_test_data/keys/EC/root_cert.pem",
+  "ttl": "1h",
+}`,
+		GlobalConfig: &spi.ConfigureRequest_GlobalConfig{TrustDomain: "localhost"},
+	})
+	require.NoError(err)
+
+	csrPEM, err := ioutil.ReadFile("_test_data/csr_valid/csr_1.pem")
+	require.NoError(err)
+	block, rest := pem.Decode(csrPEM)
+	require.Len(rest, 0)
+
+	resp, err := s.p.SubmitCSR(ctx, &upstreamca.SubmitCSRRequest{Csr: block.Bytes})
+	require.NoError(err)
+	require.NotNil(resp)
+	require.NotNil(resp.SignedCertificate)
+
+	testCSRResp(s.T(), resp, []string{"spiffe://localhost", "spiffe://upstream"}, []string{"spiffe://root"})
+}
+
+func (s *DiskSuite) TestBadBundleFile() {
+	require := s.Require()
+
+	_, err := s.p.Configure(ctx, &spi.ConfigureRequest{
+		Configuration: `{
+  "key_file_path": "_test_data/keys/EC/upstream_key.pem",
+  "cert_file_path": "_test_data/keys/EC/upstream_cert.pem",
+  "bundle_file_path": "_test_data/keys/empty/cert.pem",
+  "ttl": "1h",
+}`,
+		GlobalConfig: &spi.ConfigureRequest_GlobalConfig{TrustDomain: "localhost"},
+	})
+	require.Error(err)
+}
+
 func (s *DiskSuite) TestSubmitValidCSR() {
 	require := s.Require()
 
@@ -120,15 +194,7 @@ func (s *DiskSuite) TestSubmitValidCSR() {
 		require.NotNil(resp)
 		require.NotNil(resp.SignedCertificate)
 
-		certs, err := x509.ParseCertificates(resp.SignedCertificate.CertChain)
-		require.NoError(err)
-		require.Len(certs, 1)
-		require.Equal("spiffe://localhost", certURI(certs[0]))
-
-		upstreamTrustBundle, err := x509.ParseCertificates(resp.SignedCertificate.Bundle)
-		require.NoError(err)
-		require.Len(upstreamTrustBundle, 1)
-		require.Equal("spiffe://local", certURI(upstreamTrustBundle[0]))
+		testCSRResp(s.T(), resp, []string{"spiffe://localhost"}, []string{"spiffe://local"})
 	}
 
 	for _, csrFile := range csrFiles {
@@ -144,6 +210,22 @@ func (s *DiskSuite) TestSubmitValidCSR() {
 
 	for _, csrFile := range csrFiles {
 		testCSR(csrFile)
+	}
+}
+
+func testCSRResp(t *testing.T, resp *upstreamca.SubmitCSRResponse, expectCertChainURIs []string, expectTrustBundleURIs []string) {
+	certs, err := x509.ParseCertificates(resp.SignedCertificate.CertChain)
+	require.NoError(t, err)
+
+	trustBundle, err := x509.ParseCertificates(resp.SignedCertificate.Bundle)
+	require.NoError(t, err)
+
+	for i, cert := range certs {
+		assert.Equal(t, expectCertChainURIs[i], certURI(cert))
+	}
+
+	for i, cert := range trustBundle {
+		assert.Equal(t, expectTrustBundleURIs[i], certURI(cert))
 	}
 }
 
