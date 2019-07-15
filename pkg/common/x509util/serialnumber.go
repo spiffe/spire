b6067d00be1e21e68b4df8cfb8f3c01ecc1a02b4
Marcos G. Yedro <marcosyedro@gmail.com>
2019-07-15 16:51:38 -0300

    Address PR comments
    - Readability improvements
    
    Signed-off-by: Marcos G. Yedro <marcosyedro@gmail.com>

--- pkg/common/x509util/serialnumber.go
+++ pkg/common/x509util/serialnumber.go
@@ -7,22 +7,22 @@ import (
 	"math/big"
 )
 
+var (
+	maxUint64 = new(big.Int).SetUint64(math.MaxUint64)
+	one       = big.NewInt(1)
+)
+
 // NewSerialNumber creates a random certificate serial number according to CA/Browser forum spec
 // Section 7.1:
 //   "Effective September 30, 2016, CAs SHALL generate non-sequential Certificate serial numbers greater than
 //   zero (0) containing at least 64 bits of output from a CSPRNG"
 func NewSerialNumber() (*big.Int, error) {
-	// Get MaxUint64 as big.Int
-	maxUInt64 := new(big.Int)
-	maxUInt64 = maxUInt64.SetUint64(math.MaxUint64)
-
 	// Creates random integer in range [0,MaxUint64)
-	s, err := rand.Int(rand.Reader, maxUInt64)
+	s, err := rand.Int(rand.Reader, maxUint64)
 	if err != nil {
 		return nil, fmt.Errorf("cannot create random number: %v", err)
 	}
 
 	// Adds 1 to return serial number [1,MaxUint64]
-	s.Add(s, big.NewInt(1))
-	return s, nil
+	return s.Add(s, one), nil
 }
