f01ee727d21834f43714f25a0bb1dd6c84b362d5
Marcos G. Yedro <marcosyedro@gmail.com>
2019-07-17 12:32:31 -0300

    Address PR comment
    - Parse max uint 128 on package init
    
    Signed-off-by: Marcos G. Yedro <marcosyedro@gmail.com>

--- pkg/common/x509util/serialnumber.go
+++ pkg/common/x509util/serialnumber.go
@@ -7,8 +7,8 @@ import (
 )
 
 var (
-	maxUint128, ok = new(big.Int).SetString("340282366920938463463374607431768211455", 10) //(2^128 − 1)
-	one            = big.NewInt(1)
+	maxUint128 = getMaxUint128()
+	one        = big.NewInt(1)
 )
 
 // NewSerialNumber creates a random certificate serial number according to CA/Browser forum spec
@@ -16,10 +16,6 @@ var (
 //   "Effective September 30, 2016, CAs SHALL generate non-sequential Certificate serial numbers greater than
 //   zero (0) containing at least 64 bits of output from a CSPRNG"
 func NewSerialNumber() (*big.Int, error) {
-	if !ok {
-		return nil, fmt.Errorf("cannot parse value for max unsigned int 128")
-	}
-
 	// Creates random integer in range [0,MaxUint128)
 	s, err := rand.Int(rand.Reader, maxUint128)
 	if err != nil {
@@ -28,4 +24,12 @@ func NewSerialNumber() (*big.Int, error) {
 
 	// Adds 1 to return serial number [1,MaxUint128]
 	return s.Add(s, one), nil
+}
+
+func getMaxUint128() *big.Int {
+	max, ok := new(big.Int).SetString("340282366920938463463374607431768211455", 10) //(2^128 − 1)
+	if !ok {
+		panic("cannot parse value for max unsigned int 128")
+	}
+	return max
 }
