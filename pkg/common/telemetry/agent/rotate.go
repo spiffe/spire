09182c60231bbda6a20f17289d1a9893a6400df4
Andrew Moore <andrew.s.moore@uber.com>
2019-08-26 13:05:19 -0700

    remove unnecessary var assignment
    Signed-off-by: Andrew Moore <andrew.s.moore@uber.com>

--- pkg/common/telemetry/agent/rotate.go
+++ pkg/common/telemetry/agent/rotate.go
@@ -10,8 +10,7 @@ import (
 // StartRotateAgentSVIDCall return metric for Agent's SVID
 // Rotation.
 func StartRotateAgentSVIDCall(m telemetry.Metrics) *telemetry.CallCounter {
-	cc := telemetry.StartCall(m, telemetry.AgentSVID, telemetry.Rotate)
-	return cc
+	return telemetry.StartCall(m, telemetry.AgentSVID, telemetry.Rotate)
 }
 
 // End Call Counters
