866dd7ba211d4b88a2727e607a4fe0652713dcd2
Andrew Harding <azdagron@gmail.com>
2019-09-12 10:02:05 -0600

    reuse rotator interval
    Signed-off-by: Andrew Harding <azdagron@gmail.com>

--- pkg/agent/manager/config.go
+++ pkg/agent/manager/config.go
@@ -48,7 +48,7 @@ func New(c *Config) (*manager, error) {
 	}
 
 	if c.RotationInterval == 0 {
-		c.RotationInterval = 5 * time.Second
+		c.RotationInterval = svid.DefaultRotatorInterval
 	}
 
 	if c.Clk == nil {
