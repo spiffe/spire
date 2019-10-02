0c1da220646792c04082b15b9bce381cc42fd69d
Tyler Dixon <tylerd@uber.com>
2019-10-02 07:08:27 -0700

    Add support for aws tags in agent ID
    This change adds support for templating EC2 instance tags into
    the agent base ID. There is also some refactoring that makes
    this easier to reason about, and avoids having mutliple
    code paths which create ec2 clients / fetch ec2 instances.
    
    There is a test that verifies even if tags are used in the
    agent ID but not supplied, the attestor will not panic.
    
    Some minor changes were made to tests as a result of now
    checking if an ec2 instance needs to be fetched earlier in
    the process. This means the attestor is doing less wasted work.
    
    Signed-off-by: Tyler Dixon <tylerd@uber.com>

--- pkg/server/plugin/nodeattestor/aws/spiffeid.go
+++ pkg/server/plugin/nodeattestor/aws/spiffeid.go
@@ -15,14 +15,18 @@ type agentPathTemplateData struct {
 	aws.InstanceIdentityDocument
 	PluginName  string
 	TrustDomain string
+	Tags        instanceTags
 }
 
+type instanceTags map[string]string
+
 // makeSpiffeID creates a spiffe ID from IID data
-func makeSpiffeID(trustDomain string, agentPathTemplate *template.Template, doc aws.InstanceIdentityDocument) (*url.URL, error) {
+func makeSpiffeID(trustDomain string, agentPathTemplate *template.Template, doc aws.InstanceIdentityDocument, tags instanceTags) (*url.URL, error) {
 	var agentPath bytes.Buffer
 	if err := agentPathTemplate.Execute(&agentPath, agentPathTemplateData{
 		InstanceIdentityDocument: doc,
 		PluginName:               aws.PluginName,
+		Tags:                     tags,
 	}); err != nil {
 		return nil, err
 	}
