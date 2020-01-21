b873d0f2ee29defbeb9c39f35f59a4d4b0dca40e
Andrew Harding <azdagron@gmail.com>
2020-01-21 14:13:36 -0700

    Cache bundles in node handler
    The node API FetchX509SVID RPC is called aggressively by agents. One of
    the responsibilities is to fetch bundle information for the agents. This
    results in large QPS on the database for a resource that does not change
    frequently. This change introduces a small bundle cache to the node
    handler with one second invalidation.
    
    Bundle caching has an impact on the smallest rotatable SVID TTL since it
    influences how fast a prepared but unactivated CA certificate is
    propogated. In normal usage scenarios, the SVID TTLs should be large
    enough that there should be no practical impact. However, the rotation
    integration test, which is very tightly coupled with rotation
    timing, needed a small bump in CA and SVID TTLs to accomodate the caching.
    
    Signed-off-by: Andrew Harding <azdagron@gmail.com>

---
+++ pkg/server/endpoints/node/cache_test.go
@@ -0,0 +1,61 @@
+package node
+
+import (
+	"testing"
+
+	"github.com/spiffe/spire/pkg/server/plugin/datastore"
+	"github.com/spiffe/spire/proto/spire/common"
+	"github.com/spiffe/spire/test/clock"
+	"github.com/spiffe/spire/test/fakes/fakedatastore"
+	"github.com/spiffe/spire/test/spiretest"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/net/context"
+)
+
+func TestFetchBundleCache(t *testing.T) {
+	req := &datastore.FetchBundleRequest{TrustDomainId: "spiffe://domain.test"}
+	bundle1 := &common.Bundle{TrustDomainId: "spiffe://domain.test", RefreshHint: 1}
+	bundle2 := &common.Bundle{TrustDomainId: "spiffe://domain.test", RefreshHint: 2}
+	ds := fakedatastore.New()
+	clock := clock.NewMock(t)
+	cache := newDatastoreCache(ds, clock)
+
+	// Assert bundle is missing
+	resp, err := cache.FetchBundle(context.Background(), req)
+	require.NoError(t, err)
+	require.Empty(t, resp.Bundle)
+
+	// Add bundle
+	_, err = cache.SetBundle(context.Background(), &datastore.SetBundleRequest{
+		Bundle: bundle1,
+	})
+	require.NoError(t, err)
+
+	// Assert that we didn't cache the bundle miss and that the newly added
+	// bundle is there
+	resp, err = cache.FetchBundle(context.Background(), req)
+	require.NoError(t, err)
+	spiretest.RequireProtoEqual(t, bundle1, resp.Bundle)
+
+	// Change bundle
+	_, err = cache.SetBundle(context.Background(), &datastore.SetBundleRequest{
+		Bundle: bundle2,
+	})
+	require.NoError(t, err)
+
+	// Assert bundle contents unchanged since cache is still valid
+	resp, err = cache.FetchBundle(context.Background(), req)
+	require.NoError(t, err)
+	spiretest.RequireProtoEqual(t, bundle1, resp.Bundle)
+
+	// Invalidate cache and assert bundle contents changed
+	clock.Add(datastoreCacheExpiry)
+	resp, err = cache.FetchBundle(context.Background(), req)
+	require.NoError(t, err)
+	spiretest.RequireProtoEqual(t, bundle2, resp.Bundle)
+
+	// Assert bundle contents unchanged since cache is still valid
+	resp, err = cache.FetchBundle(context.Background(), req)
+	require.NoError(t, err)
+	spiretest.RequireProtoEqual(t, bundle2, resp.Bundle)
+}
