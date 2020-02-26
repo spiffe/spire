158b15e8523d6068f2d0b05302fc899ddf70217b
Marcos Yacob <marcos@scytale.io>
2020-02-26 12:06:48 -0300

    Sort imports
    Signed-off-by: Marcos Yacob <marcos@scytale.io>

--- pkg/server/plugin/upstreamauthority/awssecret/awssecret_test.go
+++ pkg/server/plugin/upstreamauthority/awssecret/awssecret_test.go
@@ -8,18 +8,16 @@ import (
 	"testing"
 	"time"
 
-	"google.golang.org/grpc/codes"
-
-	"github.com/spiffe/spire/pkg/server/plugin/upstreamauthority"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/secretsmanager"
 	"github.com/spiffe/spire/pkg/common/cryptoutil"
 	"github.com/spiffe/spire/pkg/common/x509svid"
+	"github.com/spiffe/spire/pkg/server/plugin/upstreamauthority"
 	spi "github.com/spiffe/spire/proto/spire/common/plugin"
 	"github.com/spiffe/spire/test/clock"
 	"github.com/spiffe/spire/test/spiretest"
 	"github.com/spiffe/spire/test/util"
+	"google.golang.org/grpc/codes"
 )
 
 var (
