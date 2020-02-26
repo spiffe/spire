32c8391291c9ae9d45906bcf78e327cc2e7d94f6
Marcos G. Yedro <marcosyedro@gmail.com>
2020-02-26 15:44:01 -0300

    Port diks upstreamCA pluging to upstream authority
    Signed-off-by: Marcos G. Yedro <marcosyedro@gmail.com>

--- pkg/server/catalog/catalog.go
+++ pkg/server/catalog/catalog.go
@@ -31,15 +31,16 @@ import (
 	no_gcs_bundle "github.com/spiffe/spire/pkg/server/plugin/notifier/gcsbundle"
 	no_k8sbundle "github.com/spiffe/spire/pkg/server/plugin/notifier/k8sbundle"
 	"github.com/spiffe/spire/pkg/server/plugin/upstreamauthority"
+	up_disk "github.com/spiffe/spire/pkg/server/plugin/upstreamauthority/disk"
 	up_spire "github.com/spiffe/spire/pkg/server/plugin/upstreamauthority/spire"
 	"github.com/spiffe/spire/pkg/server/plugin/upstreamca"
 	up_aws_pca "github.com/spiffe/spire/pkg/server/plugin/upstreamca/aws"
 	up_awssecret "github.com/spiffe/spire/pkg/server/plugin/upstreamca/awssecret"
-	up_disk "github.com/spiffe/spire/pkg/server/plugin/upstreamca/disk"
 )
 
 var (
 	portedUpstreamCA = map[string]bool{
+		"disk":  true,
 		"spire": true,
 	}
 
@@ -60,10 +61,10 @@ var (
 		nr_aws_iid.BuiltIn(),
 		nr_azure_msi.BuiltIn(),
 		// UpstreamCAs
-		up_disk.BuiltIn(),
 		up_aws_pca.BuiltIn(),
 		up_awssecret.BuiltIn(),
 		// UpstreamAuthorities
+		up_disk.BuiltIn(),
 		up_spire.BuiltIn(),
 		// KeyManagers
 		km_disk.BuiltIn(),
