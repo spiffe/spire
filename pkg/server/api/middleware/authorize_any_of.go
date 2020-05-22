e3dc8bb80b6e636ce5d8f6a0976bc0ec05bac21c
Andrew Harding <andrew.harding@hpe.com>
2020-05-22 14:14:01 -0600

    AnyOf Authorizer
    This authorizer allows one to combine any number of authorizers. The
    behavior is as follows:
    
    1. If any authorizer returns any status code other than OK or
    PERMISSION_DENIED, the authorization fails.
    2. If all authorizers return PERMISSION_DENIED, then authorization
    fails.
    3. Otherwise, if at least one authorizer returns OK, authorization
    succeeds.
    
    Signed-off-by: Andrew Harding <andrew.harding@hpe.com>

---
+++ pkg/server/api/middleware/authorize_any_of.go
@@ -0,0 +1,67 @@
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/spiffe/spire/pkg/server/api/rpccontext"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// AuthorizeAnyOf combines authorizers where if any authorizer succeeds, then
+// the caller is authorized. Specifically:
+// 1. If any authorizer returns any status code other than OK or
+// PERMISSION_DENIED, the authorization fails.
+// 2. If all authorizers return PERMISSION_DENIED, then authorization
+// fails.
+// 3. Otherwise, if at least one authorizer returns OK, authorization
+// succeeds.
+func AuthorizeAnyOf(authorizers ...Authorizer) Authorizer {
+	names := make([]string, 0, len(authorizers))
+	for _, authorizer := range authorizers {
+		names = append(names, authorizer.Name())
+	}
+
+	return anyOfAuthorizer{
+		names:       names,
+		authorizers: authorizers,
+	}
+}
+
+type anyOfAuthorizer struct {
+	names       []string
+	authorizers []Authorizer
+}
+
+func (a anyOfAuthorizer) Name() string {
+	return fmt.Sprintf("any-of[%s]", strings.Join(a.names, ","))
+}
+
+func (a anyOfAuthorizer) AuthorizeCaller(ctx context.Context) (context.Context, error) {
+	if len(a.authorizers) == 0 {
+		rpccontext.Logger(ctx).Error("Authorization misconfigured; this is a bug")
+		return nil, status.Error(codes.Internal, "authorization misconfigured")
+	}
+
+	var authenticated bool
+	for _, authorizer := range a.authorizers {
+		nextCtx, err := authorizer.AuthorizeCaller(ctx)
+		st := status.Convert(err)
+		switch st.Code() {
+		case codes.OK:
+			ctx = nextCtx
+			authenticated = true
+		case codes.PermissionDenied:
+		default:
+			return nil, err
+		}
+	}
+
+	if !authenticated {
+		return nil, status.Errorf(codes.PermissionDenied, "caller must be one of %q", a.names)
+	}
+
+	return ctx, nil
+}
