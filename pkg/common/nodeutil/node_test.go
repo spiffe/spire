a38b5329c82a6d2effb4dee458b326062ada799f
Agustín Martínez Fayó <amartinezfayo@gmail.com>
2020-06-26 18:14:36 +0000

    Add IsAgentBanned function
    Signed-off-by: Agustín Martínez Fayó <amartinezfayo@gmail.com>

---
+++ pkg/common/nodeutil/node_test.go
@@ -0,0 +1,14 @@
+package nodeutil_test
+
+import (
+	"testing"
+
+	"github.com/spiffe/spire/pkg/common/nodeutil"
+	"github.com/spiffe/spire/proto/spire/common"
+	"github.com/stretchr/testify/require"
+)
+
+func TestIsAgentBanned(t *testing.T) {
+	require.True(t, nodeutil.IsAgentBanned(&common.AttestedNode{}))
+	require.False(t, nodeutil.IsAgentBanned(&common.AttestedNode{CertSerialNumber: "non-empty-serial"}))
+}
