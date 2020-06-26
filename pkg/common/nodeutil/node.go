7190d7b545a3e1248c881b48fad02e39aebb4b9e
Agustín Martínez Fayó <amartinezfayo@gmail.com>
2020-06-26 18:17:52 +0000

    Add IsAgentBanned function
    Signed-off-by: Agustín Martínez Fayó <amartinezfayo@gmail.com>

--- pkg/common/nodeutil/node.go
+++ pkg/common/nodeutil/node.go
@@ -7,8 +7,5 @@ import (
 // IsAgentBanned determines if a given attested node is banned or not.
 // An agent is considered as "banned" if its X509 SVID serial number is empty.
 func IsAgentBanned(node *common.AttestedNode) bool {
-	if node.CertSerialNumber == "" {
-		return true
-	}
-	return false
+	return node.CertSerialNumber == ""
 }
