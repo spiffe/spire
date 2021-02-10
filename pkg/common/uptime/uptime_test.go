ffa1610c0f0048705e5b4e1016fe70d7b9198534
Brad Blackard <bradb@uber.com>
2021-02-10 10:20:52 -0800

    Fix excessive execution time in update package test
    The test in the uptime package currently takes 60 seconds to run. This
    is due to the call to reportMetrics blocking until the parent context
    times out. That timeout was hard-coded to one minute.
    
    This commit improves the execution time of the test by removing the custom
    timeout and instead relying on explicit context cancellation when the
    expected assertion conditions have been met. Additional it simplies the
    test case logic.
    
    Signed-off-by: Brad Blackard <bradb@uber.com>

--- pkg/common/uptime/uptime_test.go
+++ pkg/common/uptime/uptime_test.go
@@ -5,40 +5,23 @@ import (
 	"testing"
 	"time"
 
+	"github.com/golang/mock/gomock"
 	"github.com/spiffe/spire/test/clock"
-	"github.com/spiffe/spire/test/fakes/fakemetrics"
-	"github.com/stretchr/testify/assert"
+	mocktelemetry "github.com/spiffe/spire/test/mock/common/telemetry"
 )
 
 func TestReportMetrics(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-	defer cancel()
+	const _testUptime = 200
+	metrics := mocktelemetry.NewMockMetrics(gomock.NewController(t))
+	ctx, cancel := context.WithCancel(context.Background())
+	metrics.EXPECT().SetGauge([]string{"uptime_in_ms"}, float32(_testUptime)).Do(func(_, _ interface{}) {
+		// The expected update cancels the context which causes reportMetrics to return
+		cancel()
+	})
 
-	for _, tt := range []struct {
-		name            string
-		reportInterval  time.Duration
-		testUpTime      time.Duration
-		expectedMetrics []fakemetrics.MetricItem
-	}{
-		{
-			name:           "report uptime metrics with 10 milliseconds interval",
-			reportInterval: 10 * time.Millisecond,
-			testUpTime:     200 * time.Millisecond,
-			expectedMetrics: []fakemetrics.MetricItem{
-				{Type: fakemetrics.SetGaugeType, Key: []string{"uptime_in_ms"}, Val: 200},
-			},
-		},
-	} {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			ctx, cancel := context.WithCancel(ctx)
-			defer cancel()
-			metrics := fakemetrics.New()
-			// overwrite the variable to use mock clock.
-			clk = clock.NewMock(t)
-			start = clk.Now().Add(-tt.testUpTime)
-			reportMetrics(ctx, 0*time.Nanosecond, metrics)
-			assert.Equal(t, tt.expectedMetrics, metrics.AllMetrics())
-		})
-	}
+	// overwrite the variable to use mock clock.
+	clk = clock.NewMock(t)
+	start = clk.Now().Add(-_testUptime*time.Millisecond)
+	reportMetrics(ctx, time.Nanosecond, metrics)
 }
+
