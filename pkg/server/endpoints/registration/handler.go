ab405c1603866823c15deae0084f068887804559
Brian Martin <bri365@gmail.com>
2021-04-13 09:41:25 -0700

    expiry as time cleanup
    Signed-off-by: Brian Martin <bri365@gmail.com>

--- pkg/server/endpoints/registration/handler.go
+++ pkg/server/endpoints/registration/handler.go
@@ -528,11 +528,11 @@ func (h *Handler) CreateJoinToken(ctx context.Context, request *registration.Joi
 	}
 
 	ds := h.getDataStore()
-	expiry := time.Now().Unix() + int64(request.Ttl)
+	expiry := time.Now().Add(time.Second * time.Duration(request.Ttl))
 
 	err = ds.CreateJoinToken(ctx, &datastore.JoinToken{
 		Token:  request.Token,
-		Expiry: time.Unix(expiry, 0),
+		Expiry: expiry,
 	})
 	if err != nil {
 		log.WithError(err).Error("Failed to register token")
