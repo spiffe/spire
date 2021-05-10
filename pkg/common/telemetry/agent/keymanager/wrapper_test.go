d96bcda2ae4bb7c0a53d9d74735345d08fa4bf50
Andrew Harding <aharding@vmware.com>
2021-05-10 11:19:26 -0600

    Adding Agent KeyManager V1 facade
    Signed-off-by: Andrew Harding <aharding@vmware.com>

--- pkg/common/telemetry/agent/keymanager/wrapper_test.go
+++ pkg/common/telemetry/agent/keymanager/wrapper_test.go
@@ -3,9 +3,11 @@ package keymanager
 import (
 	"context"
 	"crypto"
+	"io"
 	"strings"
 	"testing"
 
+	"github.com/spiffe/spire/pkg/agent/plugin/keymanager"
 	"github.com/spiffe/spire/pkg/common/telemetry"
 	"github.com/spiffe/spire/test/fakes/fakemetrics"
 	"github.com/stretchr/testify/assert"
@@ -14,21 +16,73 @@ import (
 
 type fakeKeyManager struct{}
 
-func (fakeKeyManager) GenerateKey(ctx context.Context) (crypto.Signer, error) {
-	return nil, nil
+func (fakeKeyManager) Name() string { return "" }
+
+func (fakeKeyManager) Type() string { return "" }
+
+func (fakeKeyManager) Single() (keymanager.SingleKeyManager, bool) {
+	return fakeSingleKeyManager{}, true
 }
 
-func (fakeKeyManager) GetKey(ctx context.Context) (crypto.Signer, error) {
-	return nil, nil
+func (fakeKeyManager) Multi() (keymanager.MultiKeyManager, bool) {
+	return fakeMultiKeyManager{}, true
 }
 
-func (fakeKeyManager) SetKey(ctx context.Context, key crypto.Signer) error {
+type fakeSingleKeyManager struct{}
+
+func (fakeSingleKeyManager) Name() string { return "" }
+
+func (fakeSingleKeyManager) Type() string { return "" }
+
+func (fakeSingleKeyManager) GenerateKey(ctx context.Context) (crypto.Signer, error) {
+	return fakeKey{}, nil
+}
+
+func (fakeSingleKeyManager) GetKey(ctx context.Context) (crypto.Signer, error) {
+	return fakeKey{}, nil
+}
+
+func (fakeSingleKeyManager) SetKey(ctx context.Context, key crypto.Signer) error {
 	return nil
 }
+
+type fakeMultiKeyManager struct{}
+
+func (fakeMultiKeyManager) Name() string { return "" }
+
+func (fakeMultiKeyManager) Type() string { return "" }
+
+func (fakeMultiKeyManager) GenerateKey(ctx context.Context, id string, keyType keymanager.KeyType) (_ keymanager.Key, err error) {
+	return fakeKey{}, nil
+}
+
+func (fakeMultiKeyManager) GetKey(ctx context.Context, id string) (_ keymanager.Key, err error) {
+	return fakeKey{}, nil
+}
+
+func (fakeMultiKeyManager) GetKeys(ctx context.Context) (_ []keymanager.Key, err error) {
+	return []keymanager.Key{fakeKey{}}, nil
+}
+
+type fakeKey struct{}
+
+func (fakeKey) ID() string { return "" }
+
+func (fakeKey) Sign(rand io.Reader, digest []byte, opts crypto.SignerOpts) ([]byte, error) {
+	return nil, nil
+}
+
+func (fakeKey) Public() crypto.PublicKey { return nil }
 
 func TestWithMetrics(t *testing.T) {
 	m := fakemetrics.New()
 	w := WithMetrics(fakeKeyManager{}, m)
+
+	single, ok := w.Single()
+	require.True(t, ok)
+	multi, ok := w.Multi()
+	require.True(t, ok)
+
 	for _, tt := range []struct {
 		key  string
 		call func() error
@@ -36,21 +90,42 @@ func TestWithMetrics(t *testing.T) {
 		{
 			key: "agent_key_manager.generate_key_pair",
 			call: func() error {
-				_, err := w.GenerateKey(context.Background())
+				_, err := single.GenerateKey(context.Background())
 				return err
 			},
 		},
 		{
 			key: "agent_key_manager.fetch_private_key",
 			call: func() error {
-				_, err := w.GetKey(context.Background())
+				_, err := single.GetKey(context.Background())
 				return err
 			},
 		},
 		{
 			key: "agent_key_manager.store_private_key",
 			call: func() error {
-				return w.SetKey(context.Background(), nil)
+				return single.SetKey(context.Background(), nil)
+			},
+		},
+		{
+			key: "agent_key_manager.generate_key",
+			call: func() error {
+				_, err := multi.GenerateKey(context.Background(), "", keymanager.ECP256)
+				return err
+			},
+		},
+		{
+			key: "agent_key_manager.get_key",
+			call: func() error {
+				_, err := multi.GetKey(context.Background(), "")
+				return err
+			},
+		},
+		{
+			key: "agent_key_manager.get_keys",
+			call: func() error {
+				_, err := multi.GetKeys(context.Background())
+				return err
 			},
 		},
 	} {
