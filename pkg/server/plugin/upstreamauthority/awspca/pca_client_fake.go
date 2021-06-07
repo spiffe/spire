e1ffe672c850c2ed85c7a2764bf52c74512d3fec
Marcos Yacob <marcos.yacob@hpe.com>
2021-06-07 15:19:35 -0300

    Migrate Server aws_pca UpstreamAuthority to V1 (#2338)
    Signed-off-by: Marcos Yacob <marcos.yacob@hpe.com>

--- pkg/server/plugin/upstreamauthority/awspca/pca_client_fake.go
+++ pkg/server/plugin/upstreamauthority/awspca/pca_client_fake.go
@@ -10,32 +10,35 @@ import (
 )
 
 type pcaClientFake struct {
-	t *testing.T
+	t testing.TB
 
 	describeCertificateOutput *acmpca.DescribeCertificateAuthorityOutput
 	expectedDescribeInput     *acmpca.DescribeCertificateAuthorityInput
+	describeCertificateErr    error
 
 	issueCertificateOutput *acmpca.IssueCertificateOutput
 	expectedIssueInput     *acmpca.IssueCertificateInput
+	issueCertifcateErr     error
 
 	expectedGetCertificateInput *acmpca.GetCertificateInput
 	getCertificateOutput        *acmpca.GetCertificateOutput
+	getCertificateErr           error
 
-	err error
+	waitUntilCertificateIssuedErr error
 }
 
 func (f *pcaClientFake) DescribeCertificateAuthorityWithContext(ctx aws.Context, input *acmpca.DescribeCertificateAuthorityInput, option ...request.Option) (*acmpca.DescribeCertificateAuthorityOutput, error) {
 	require.Equal(f.t, f.expectedDescribeInput, input)
-	if f.err != nil {
-		return nil, f.err
+	if f.describeCertificateErr != nil {
+		return nil, f.describeCertificateErr
 	}
 	return f.describeCertificateOutput, nil
 }
 
 func (f *pcaClientFake) IssueCertificateWithContext(ctx aws.Context, input *acmpca.IssueCertificateInput, option ...request.Option) (*acmpca.IssueCertificateOutput, error) {
 	require.Equal(f.t, f.expectedIssueInput, input)
-	if f.err != nil {
-		return nil, f.err
+	if f.issueCertifcateErr != nil {
+		return nil, f.issueCertifcateErr
 	}
 	return f.issueCertificateOutput, nil
 }
@@ -43,13 +46,13 @@ func (f *pcaClientFake) IssueCertificateWithContext(ctx aws.Context, input *acmp
 func (f *pcaClientFake) WaitUntilCertificateIssuedWithContext(ctx aws.Context, input *acmpca.GetCertificateInput, option ...request.WaiterOption) error {
 	require.Equal(f.t, f.expectedGetCertificateInput, input)
 
-	return f.err
+	return f.waitUntilCertificateIssuedErr
 }
 
 func (f *pcaClientFake) GetCertificateWithContext(ctx aws.Context, input *acmpca.GetCertificateInput, option ...request.Option) (*acmpca.GetCertificateOutput, error) {
 	require.Equal(f.t, f.expectedGetCertificateInput, input)
-	if f.err != nil {
-		return nil, f.err
+	if f.getCertificateErr != nil {
+		return nil, f.getCertificateErr
 	}
 	return f.getCertificateOutput, nil
 }
