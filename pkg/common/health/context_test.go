183e6ea840e400fed764a92a1fb5508b1f81b0ec
Andrew Harding <aharding@vmware.com>
2021-06-24 11:55:56 -0600

    Fix test after function renames
    Signed-off-by: Andrew Harding <aharding@vmware.com>

--- pkg/common/health/context_test.go
+++ pkg/common/health/context_test.go
@@ -1,13 +1,14 @@
-package health
+package health_test
 
 import (
 	"context"
 	"testing"
 
+	"github.com/spiffe/spire/pkg/common/health"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestContext(t *testing.T) {
-	assert.False(t, IsHealthCheck(context.Background()))
-	assert.True(t, IsHealthCheck(AsHealthCheck(context.Background())))
+	assert.False(t, health.IsCheck(context.Background()))
+	assert.True(t, health.IsCheck(health.CheckContext(context.Background())))
 }
