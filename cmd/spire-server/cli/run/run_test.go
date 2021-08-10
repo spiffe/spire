a719875eb76bb23175d14c66adef6859016d5f95
Marcos Yacob <marcos.yacob@hpe.com>
2021-08-10 14:16:28 -0300

    Reinsert audit_log_enabled configuration on spire-server client (#2442)
    Signed-off-by: Marcos Yacob <marcos.yacob@hpe.com>

--- cmd/spire-server/cli/run/run_test.go
+++ cmd/spire-server/cli/run/run_test.go
@@ -54,6 +54,7 @@ func TestParseConfigGood(t *testing.T) {
 	assert.NoError(t, err)
 	_, ok := trustDomainConfig.EndpointProfile.(bundleClient.HTTPSWebProfile)
 	assert.True(t, ok)
+	assert.True(t, c.Server.AuditLogEnabled)
 
 	// Check for plugins configurations
 	pluginConfigs := *c.Plugins
@@ -509,11 +510,13 @@ func TestMergeInput(t *testing.T) {
 			},
 		},
 		{
-			msg: "auditlog_enabled should be configurable by file",
+			msg: "audit_log_enabled should be configurable by file",
 			fileInput: func(c *Config) {
+				c.Server.AuditLogEnabled = true
 			},
 			cliInput: func(c *serverConfig) {},
 			test: func(t *testing.T, c *Config) {
+				require.True(t, c.Server.AuditLogEnabled)
 			},
 		},
 	}
@@ -1080,17 +1083,21 @@ func TestNewServerConfig(t *testing.T) {
 			},
 		},
 		{
-			msg: "auditlog_enabled is enabled",
+			msg: "audit_log_enabled is enabled",
 			input: func(c *Config) {
+				c.Server.AuditLogEnabled = true
 			},
 			test: func(t *testing.T, c *server.Config) {
+				require.True(t, c.AuditLogEnabled)
 			},
 		},
 		{
-			msg: "auditlog_enabled is disabled",
+			msg: "audit_log_enabled is disabled",
 			input: func(c *Config) {
+				c.Server.AuditLogEnabled = false
 			},
 			test: func(t *testing.T, c *server.Config) {
+				require.False(t, c.AuditLogEnabled)
 			},
 		},
 	}
