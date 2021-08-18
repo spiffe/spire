c015d360c93af047925c95a6b18c6d976389a525
amoore877 <andrew.s.moore@uber.com>
2021-08-18 13:47:59 -0700

    add log hook
    Signed-off-by: amoore877 <andrew.s.moore@uber.com>

--- pkg/common/peertracker/peertracker_test.go
+++ pkg/common/peertracker/peertracker_test.go
@@ -11,6 +11,8 @@ import (
 	"syscall"
 	"testing"
 
+	// "github.com/sirupsen/logrus"
+	logtest "github.com/sirupsen/logrus/hooks/test"
 	"github.com/spiffe/spire/test/spiretest"
 	"github.com/stretchr/testify/suite"
 )
@@ -25,6 +27,7 @@ type PeerTrackerTestSuite struct {
 	childPath string
 	ul        *Listener
 	unixAddr  *net.UnixAddr
+	logHook   *logtest.Hook
 }
 
 func (p *PeerTrackerTestSuite) SetupTest() {
@@ -42,7 +45,10 @@ func (p *PeerTrackerTestSuite) SetupTest() {
 		Name: path.Join(tempDir, "test.sock"),
 	}
 
-	p.ul, err = (&ListenerFactory{}).ListenUnix(p.unixAddr.Network(), p.unixAddr)
+	log, hook := logtest.NewNullLogger()
+	p.logHook = hook
+
+	p.ul, err = (&ListenerFactory{Log: log}).ListenUnix(p.unixAddr.Network(), p.unixAddr)
 	p.NoError(err)
 }
 
