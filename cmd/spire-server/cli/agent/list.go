a634d0706942c5a9e72dbbdc93a027a9a896f5aa
Marcos Yacob <marcos.yacob@hpe.com>
2021-08-18 15:32:22 -0300

    Implement -matchSelectorsOn flag in agent listt CLI
    Signed-off-by: Marcos Yacob <marcos.yacob@hpe.com>

--- cmd/spire-server/cli/agent/list.go
+++ cmd/spire-server/cli/agent/list.go
@@ -1,8 +1,10 @@
 package agent
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/mitchellh/cli"
@@ -16,7 +18,14 @@ import (
 	"golang.org/x/net/context"
 )
 
-type listCommand struct{}
+type listCommand struct {
+	// Type and value are delimited by a colon (:)
+	// ex. "unix:uid:1000" or "spiffe_id:spiffe://example.org/foo"
+	selectors common_cli.StringsFlag
+
+	// Match used when filtering agents by selectors
+	matchSelectorsOn string
+}
 
 // NewListCommand creates a new "list" subcommand for "agent" command.
 func NewListCommand() cli.Command {
@@ -39,6 +48,27 @@ func (listCommand) Synopsis() string {
 
 // Run lists attested agents
 func (c *listCommand) Run(ctx context.Context, env *common_cli.Env, serverClient util.ServerClient) error {
+	filter := &agentv1.ListAgentsRequest_Filter{}
+	if len(c.selectors) > 0 {
+		matchBehavior, err := parseToSelectorMatch(c.matchSelectorsOn)
+		if err != nil {
+			return err
+		}
+
+		selectors := make([]*types.Selector, len(c.selectors))
+		for i, sel := range c.selectors {
+			selector, err := parseSelector(sel)
+			if err != nil {
+				return fmt.Errorf("error parsing selectors: %w", err)
+			}
+			selectors[i] = selector
+		}
+		filter.BySelectorMatch = &types.SelectorMatch{
+			Selectors: selectors,
+			Match:     matchBehavior,
+		}
+	}
+
 	agentClient := serverClient.NewAgentClient()
 
 	pageToken := ""
@@ -47,6 +77,7 @@ func (c *listCommand) Run(ctx context.Context, env *common_cli.Env, serverClient
 		listResponse, err := agentClient.ListAgents(ctx, &agentv1.ListAgentsRequest{
 			PageSize:  1000, // comfortably under the (4 MB/theoretical maximum size of 1 agent in MB)
 			PageToken: pageToken,
+			Filter:    filter,
 		})
 		if err != nil {
 			return err
@@ -69,6 +100,8 @@ func (c *listCommand) Run(ctx context.Context, env *common_cli.Env, serverClient
 }
 
 func (c *listCommand) AppendFlags(fs *flag.FlagSet) {
+	fs.StringVar(&c.matchSelectorsOn, "matchSelectorsOn", "superset", "The match mode used when filtering by selectors. Options: exact, any, superset and subset")
+	fs.Var(&c.selectors, "selector", "A colon-delimited type:value selector. Can be used more than once")
 }
 
 func printAgents(env *common_cli.Env, agents ...*types.Agent) error {
@@ -103,4 +136,33 @@ func printAgents(env *common_cli.Env, agents ...*types.Agent) error {
 	}
 
 	return nil
+}
+
+func parseSelector(str string) (*types.Selector, error) {
+	parts := strings.SplitAfterN(str, ":", 2)
+	if len(parts) < 2 {
+		return nil, fmt.Errorf("selector \"%s\" must be formatted as type:value", str)
+	}
+
+	s := &types.Selector{
+		// Strip the trailing delimiter
+		Type:  strings.TrimSuffix(parts[0], ":"),
+		Value: parts[1],
+	}
+	return s, nil
+}
+
+func parseToSelectorMatch(match string) (types.SelectorMatch_MatchBehavior, error) {
+	switch match {
+	case "exact":
+		return types.SelectorMatch_MATCH_EXACT, nil
+	case "any":
+		return types.SelectorMatch_MATCH_ANY, nil
+	case "superset":
+		return types.SelectorMatch_MATCH_SUPERSET, nil
+	case "subset":
+		return types.SelectorMatch_MATCH_SUBSET, nil
+	default:
+		return types.SelectorMatch_MATCH_SUPERSET, errors.New("unsupported match behavior")
+	}
 }
