54b88e97864f377346e43cb710ecc68f84e39b93
Andrew Harding <aharding@vmware.com>
2021-09-13 16:02:39 -0600

    Remove v0 Agent KeyManager
    Signed-off-by: Andrew Harding <aharding@vmware.com>

--- Makefile
+++ Makefile
@@ -150,7 +150,6 @@ api-protos := \
 
 plugin-protos := \
 	proto/spire/common/plugin/plugin.proto \
-	proto/spire/plugin/agent/keymanager/v0/keymanager.proto \
 	proto/spire/plugin/agent/svidstore/v1/svidstore.proto \
 	proto/spire/plugin/server/keymanager/v0/keymanager.proto \
 	proto/spire/plugin/server/nodeattestor/v0/nodeattestor.proto \
--- pkg/agent/plugin/keymanager/memory/memory_test.go
+++ pkg/agent/plugin/keymanager/memory/memory_test.go
@@ -7,17 +7,14 @@ import (
 	"github.com/spiffe/spire/pkg/agent/plugin/keymanager/memory"
 	keymanagertest "github.com/spiffe/spire/pkg/agent/plugin/keymanager/test"
 	"github.com/spiffe/spire/test/plugintest"
-	"github.com/stretchr/testify/require"
 )
 
 func TestKeyManagerContract(t *testing.T) {
 	keymanagertest.Test(t, keymanagertest.Config{
-		Create: func(t *testing.T) keymanager.MultiKeyManager {
+		Create: func(t *testing.T) keymanager.KeyManager {
 			km := new(keymanager.V1)
 			plugintest.Load(t, memory.BuiltIn(), km)
-			multi, ok := km.Multi()
-			require.True(t, ok)
-			return multi
+			return km
 		},
 	})
 }
