921b3dbbb10f43089799eaf5c03eb8cc0f60cf38
Andrew Harding <aharding@vmware.com>
2021-10-26 10:09:39 -0600

    Clean up agent rotator tests
    - Removes the agent client mock
    - Restructures as simple table-driven tests
    - Replaces Catalog with SVID Key Manager
    
    Signed-off-by: Andrew Harding <aharding@vmware.com>

--- pkg/agent/svid/rotator_test.go
+++ pkg/agent/svid/rotator_test.go
@@ -2,12 +2,15 @@ package svid
 
 import (
 	"context"
+	"crypto"
+	"crypto/rand"
 	"crypto/x509"
 	"errors"
+	"math/big"
+	"net/url"
 	"testing"
 	"time"
 
-	"github.com/golang/mock/gomock"
 	"github.com/imkira/go-observer"
 	"github.com/sirupsen/logrus/hooks/test"
 	"github.com/spiffe/go-spiffe/v2/spiffeid"
@@ -16,217 +19,185 @@ import (
 	"github.com/spiffe/spire/pkg/agent/plugin/keymanager"
 	"github.com/spiffe/spire/pkg/common/telemetry"
 	"github.com/spiffe/spire/test/clock"
-	"github.com/spiffe/spire/test/fakes/fakeagentcatalog"
 	"github.com/spiffe/spire/test/fakes/fakeagentkeymanager"
-	mock_client "github.com/spiffe/spire/test/mock/agent/client"
-	"github.com/spiffe/spire/test/util"
-	"github.com/stretchr/testify/suite"
+	"github.com/spiffe/spire/test/testca"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRotator(t *testing.T) {
-	suite.Run(t, new(RotatorTestSuite))
+	caCert, caKey := testca.CreateCACertificate(t, nil, nil)
+
+	for _, tt := range []struct {
+		name         string
+		notAfter     time.Duration
+		checkAfter   time.Duration
+		shouldRotate bool
+	}{
+		{
+			name:         "not expired at startup",
+			notAfter:     time.Minute,
+			shouldRotate: false,
+		},
+		{
+			name:         "expired at startup",
+			notAfter:     0,
+			shouldRotate: true,
+		},
+		{
+			name:         "expires after startup",
+			notAfter:     2 * time.Minute,
+			checkAfter:   2 * time.Minute,
+			shouldRotate: true,
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			svidKM := keymanager.ForSVID(fakeagentkeymanager.New(t, ""))
+			clk := clock.NewMock(t)
+			log, _ := test.NewNullLogger()
+			client := &fakeClient{clk: clk, caCert: caCert, caKey: caKey}
+
+			// Create the starting SVID
+			svidKey, err := svidKM.GenerateKey(context.Background(), nil)
+			require.NoError(t, err)
+			svid := createSVID(t, svidKey, caCert, caKey, clk.Now(), clk.Now().Add(tt.notAfter))
+
+			// Initialize the rotator
+			rotator, _ := newRotator(&RotatorConfig{
+				SVIDKeyManager: svidKM,
+				Log:            log,
+				Metrics:        telemetry.Blackhole{},
+				TrustDomain:    spiffeid.RequireTrustDomainFromString("example.org"),
+				BundleStream:   cache.NewBundleStream(observer.NewProperty([]*x509.Certificate(nil)).Observe()),
+				Clk:            clk,
+				SVID:           svid,
+				SVIDKey:        svidKey,
+			})
+			rotator.client = client
+
+			// Hook the rotation loop so we can determine when the rotator
+			// has finished an rotation evaluation (does not imply anything
+			// was actually rotated, just that the rotator evaluated the
+			// SVID expiration and attempted rotation if needed).
+			rotationDone := make(chan struct{}, 1)
+			rotator.SetRotationFinishedHook(func() {
+				select {
+				case rotationDone <- struct{}{}:
+				default:
+				}
+			})
+
+			// Subscribe to SVID changes and run the rotator
+			stream := rotator.Subscribe()
+
+			ctx, cancel := context.WithCancel(context.Background())
+			errCh := make(chan error, 1)
+			go func() {
+				errCh <- rotator.Run(ctx)
+			}()
+
+			// Wait for the initial rotation check to finish
+			select {
+			case <-time.After(time.Minute):
+				t.Fatal("timed out waiting for rotation check to finish")
+			case <-rotationDone:
+			}
+
+			// Optionally advance the clock by the specified amount
+			// and wait for another rotation check to finish.
+			if tt.checkAfter != 0 {
+				require.Greater(t, tt.checkAfter, DefaultRotatorInterval, "checkAfter should be larger than the default rotator interval")
+				clk.Add(tt.checkAfter)
+				select {
+				case <-time.After(time.Minute):
+					t.Fatal("timed out waiting for rotation check to finish after clock adjustment")
+				case <-rotationDone:
+				}
+			}
+
+			// Shut down the rotator
+			cancel()
+			select {
+			case <-time.After(time.Minute):
+				t.Fatal("timed out waiting for the rotator to shut down")
+			case err = <-errCh:
+				require.True(t, errors.Is(err, context.Canceled), "expected %v, not %v", context.Canceled, err)
+			}
+
+			// If rotation was supposed to happen, wait for the SVID changes
+			// on the state stream.
+			if tt.shouldRotate {
+				require.True(t, stream.HasNext(), "SVID stream should have changes")
+				stream.Next()
+			} else {
+				require.False(t, stream.HasNext(), "SVID stream should not have changes")
+			}
+
+			// Assert that rotation happened and that the client was released
+			// the appropriate number of times.
+			state := stream.Value().(State)
+			require.Len(t, state.SVID, 1)
+			if tt.shouldRotate {
+				assert.NotEqual(t, svid, state.SVID)
+				assert.NotEqual(t, svidKey, state.Key)
+				assert.Equal(t, 2, client.releaseCount, "client might not released after rotation")
+			} else {
+				assert.Equal(t, svid, state.SVID)
+				assert.Equal(t, svidKey, state.Key)
+				assert.Equal(t, 1, client.releaseCount)
+			}
+		})
+	}
 }
 
-type RotatorTestSuite struct {
-	suite.Suite
-
-	ctrl *gomock.Controller
-
-	client *mock_client.MockClient
-
-	bundle observer.Property
-
-	svidKM keymanager.SVIDKeyManager
-
-	r *rotator
-
-	mockClock *clock.Mock
+type fakeClient struct {
+	clk          clock.Clock
+	caCert       *x509.Certificate
+	caKey        crypto.Signer
+	releaseCount int
 }
 
-func (s *RotatorTestSuite) SetupTest() {
-	s.ctrl = gomock.NewController(s.T())
-
-	s.client = mock_client.NewMockClient(s.ctrl)
-
-	b, err := util.LoadBundleFixture()
-	s.Require().NoError(err)
-	s.bundle = observer.NewProperty(b)
-
-	km := fakeagentkeymanager.New(s.T(), "")
-	s.svidKM = keymanager.ForSVID(km)
-
-	cat := fakeagentcatalog.New()
-	cat.SetKeyManager(km)
-
-	s.mockClock = clock.NewMock(s.T())
-	s.mockClock.Set(time.Now())
-	log, _ := test.NewNullLogger()
-	c := &RotatorConfig{
-		Catalog:      cat,
-		Log:          log,
-		Metrics:      telemetry.Blackhole{},
-		TrustDomain:  spiffeid.RequireTrustDomainFromString("example.org"),
-		BundleStream: cache.NewBundleStream(s.bundle.Observe()),
-		Clk:          s.mockClock,
+func (c *fakeClient) RenewSVID(ctx context.Context, csrBytes []byte) (*client.X509SVID, error) {
+	csr, err := x509.ParseCertificateRequest(csrBytes)
+	if err != nil {
+		return nil, err
 	}
-	s.r, _ = newRotator(c)
-	s.r.client = s.client
+
+	if err := csr.CheckSignature(); err != nil {
+		return nil, err
+	}
+
+	tmpl := &x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		NotBefore:    c.clk.Now(),
+		NotAfter:     c.clk.Now().Add(time.Hour),
+	}
+
+	svid, err := x509.CreateCertificate(rand.Reader, tmpl, c.caCert, csr.PublicKey, c.caKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return &client.X509SVID{
+		CertChain: svid,
+		ExpiresAt: tmpl.NotAfter.Unix(),
+	}, nil
 }
 
-func (s *RotatorTestSuite) TearDownTest() {
-	s.ctrl.Finish()
+func (c *fakeClient) Release() {
+	c.releaseCount++
 }
 
-func (s *RotatorTestSuite) TestRunWithGoodExistingSVID() {
-	key, err := s.svidKM.GenerateKey(context.Background(), nil)
-	s.Require().NoError(err)
-
-	// Cert that's valid for 1hr
-	temp, err := util.NewSVIDTemplate(s.mockClock, "spiffe://example.org/test")
-	s.Require().NoError(err)
-	goodCert, _, err := util.Sign(temp, temp, key)
-	s.Require().NoError(err)
-
-	state := State{
-		SVID: []*x509.Certificate{goodCert},
-		Key:  key,
+func createSVID(t *testing.T, svidKey crypto.Signer, ca *x509.Certificate, caKey crypto.Signer, notBefore time.Time, notAfter time.Time) []*x509.Certificate {
+	tmpl := &x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		NotBefore:    notBefore,
+		NotAfter:     notAfter,
+		URIs:         []*url.URL{{Scheme: "spiffe", Host: "example.org", Path: "/spire/agent/test"}},
 	}
-	s.r.state = observer.NewProperty(state)
-
-	s.client.EXPECT().Release()
-
-	stream := s.r.Subscribe()
-
-	rotationDone := make(chan struct{}, 1)
-	s.r.SetRotationFinishedHook(func() {
-		select {
-		case rotationDone <- struct{}{}:
-		default:
-		}
-	})
-
-	ctx, cancel := context.WithCancel(context.Background())
-	errCh := make(chan error, 1)
-	go func() {
-		errCh <- s.r.Run(ctx)
-	}()
-
-	// Wait for the first rotation check to finish
-	select {
-	case <-time.After(time.Minute):
-		s.FailNow("timed out waiting for rotation check to finish")
-	case <-rotationDone:
-	}
-
-	// We should have the latest state
-	s.Assert().False(stream.HasNext())
-
-	// Should be equal to the fixture
-	state = stream.Value().(State)
-	s.Require().Len(state.SVID, 1)
-	s.Assert().Equal(goodCert, state.SVID[0])
-	s.Assert().Equal(key, state.Key)
-
-	cancel()
-	err = <-errCh
-	s.Require().True(errors.Is(err, context.Canceled))
-}
-
-func (s *RotatorTestSuite) TestRunWithUpdates() {
-	// Cert that's valid for 1hr
-	temp, err := util.NewSVIDTemplate(s.mockClock, "spiffe://example.org/test")
-	s.Require().NoError(err)
-	goodCert, _, err := util.SelfSign(temp)
-	s.Require().NoError(err)
-
-	// Cert that's expiring
-	temp.NotBefore = s.mockClock.Now().Add(-1 * time.Hour)
-	temp.NotAfter = s.mockClock.Now()
-	badCert, _, err := util.SelfSign(temp)
-	s.Require().NoError(err)
-
-	state := State{
-		SVID: []*x509.Certificate{badCert},
-	}
-	s.r.state = observer.NewProperty(state)
-
-	s.expectSVIDRotation(goodCert)
-
-	stream := s.r.Subscribe()
-
-	rotationDone := make(chan struct{}, 1)
-	s.r.SetRotationFinishedHook(func() {
-		select {
-		case rotationDone <- struct{}{}:
-		default:
-		}
-	})
-
-	ctx, cancel := context.WithCancel(context.Background())
-	errCh := make(chan error, 1)
-	go func() {
-		errCh <- s.r.Run(ctx)
-	}()
-
-	s.mockClock.Add(s.r.c.Interval)
-
-	// Wait for the first rotation check to finish
-	select {
-	case <-time.After(time.Minute):
-		s.FailNow("timed out waiting for rotation check to finish")
-	case <-rotationDone:
-	}
-
-	select {
-	case <-time.After(time.Second):
-		s.T().Error("timed out while waiting for expected SVID rotation")
-	case <-stream.Changes():
-		state = stream.Next().(State)
-		s.Require().Len(state.SVID, 1)
-		s.Assert().Equal(goodCert, state.SVID[0])
-	}
-
-	cancel()
-	err = <-errCh
-	s.Require().True(errors.Is(err, context.Canceled))
-}
-
-func (s *RotatorTestSuite) TestRotateSVID() {
-	// Cert that's valid for 1hr
-	temp, err := util.NewSVIDTemplate(s.mockClock, "spiffe://example.org/test")
-	s.Require().NoError(err)
-	goodCert, _, err := util.SelfSign(temp)
-	s.Require().NoError(err)
-
-	// Cert that's expiring
-	temp.NotBefore = s.mockClock.Now().Add(-1 * time.Hour)
-	temp.NotAfter = s.mockClock.Now()
-	badCert, _, err := util.SelfSign(temp)
-	s.Require().NoError(err)
-
-	state := State{
-		SVID: []*x509.Certificate{badCert},
-	}
-	s.r.state = observer.NewProperty(state)
-
-	stream := s.r.Subscribe()
-	s.expectSVIDRotation(goodCert)
-	err = s.r.rotateSVID(context.Background())
-	s.Assert().NoError(err)
-	s.Require().True(stream.HasNext())
-
-	state = stream.Next().(State)
-	s.Require().Len(state.SVID, 1)
-	s.Assert().True(goodCert.Equal(state.SVID[0]))
-}
-
-// expectSVIDRotation sets the appropriate expectations for an SVID rotation, and returns
-// the the provided certificate to the client.Client caller.
-func (s *RotatorTestSuite) expectSVIDRotation(cert *x509.Certificate) {
-	s.client.EXPECT().
-		RenewSVID(gomock.Any(), gomock.Any()).
-		Return(&client.X509SVID{
-			CertChain: cert.Raw,
-		}, nil)
-	s.client.EXPECT().Release().MaxTimes(2)
+	svidBytes, err := x509.CreateCertificate(rand.Reader, tmpl, ca, svidKey.Public(), caKey)
+	require.NoError(t, err)
+	svid, err := x509.ParseCertificate(svidBytes)
+	require.NoError(t, err)
+	return []*x509.Certificate{svid}
 }
