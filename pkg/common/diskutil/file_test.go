df9eb89c6134dba8114c4b9e94553196662aabed
Marcos Yacob <marcos.yacob@hpe.com>
2021-11-23 15:25:31 -0300

    PR changes
    Signed-off-by: Marcos Yacob <marcos.yacob@hpe.com>

--- pkg/common/diskutil/file_test.go
+++ pkg/common/diskutil/file_test.go
@@ -6,6 +6,7 @@ package diskutil
 import (
 	"os"
 	"path/filepath"
+	"runtime"
 	"testing"
 
 	"github.com/spiffe/spire/test/spiretest"
@@ -16,24 +17,32 @@ func TestAtomicWriteFile(t *testing.T) {
 	dir := spiretest.TempDir(t)
 
 	tests := []struct {
-		name string
-		data []byte
-		mode os.FileMode
+		name              string
+		data              []byte
+		mode              os.FileMode
+		expectPosixMode   os.FileMode
+		expectWindowsMode os.FileMode
 	}{
 		{
-			name: "basic test",
-			data: []byte("Hello, World"),
-			mode: 0600,
+			name:              "basic test",
+			data:              []byte("Hello, World"),
+			mode:              0600,
+			expectPosixMode:   0600,
+			expectWindowsMode: 0666,
 		},
 		{
-			name: "empty",
-			data: []byte{},
-			mode: 0440,
+			name:              "empty",
+			data:              []byte{},
+			mode:              0440,
+			expectPosixMode:   0440,
+			expectWindowsMode: 0444,
 		},
 		{
-			name: "binary",
-			data: []byte{0xFF, 0, 0xFF, 0x3D, 0xD8, 0xA9, 0xDC, 0xF0, 0x9F, 0x92, 0xA9},
-			mode: 0644,
+			name:              "binary",
+			data:              []byte{0xFF, 0, 0xFF, 0x3D, 0xD8, 0xA9, 0xDC, 0xF0, 0x9F, 0x92, 0xA9},
+			mode:              0644,
+			expectPosixMode:   0644,
+			expectWindowsMode: 0666,
 		},
 	}
 	for _, tt := range tests {
@@ -45,7 +54,12 @@ func TestAtomicWriteFile(t *testing.T) {
 
 			info, err := os.Stat(file)
 			require.NoError(t, err)
-			require.EqualValues(t, tt.mode, info.Mode())
+			switch runtime.GOOS {
+			case "windows":
+				require.EqualValues(t, tt.expectWindowsMode, info.Mode())
+			default:
+				require.EqualValues(t, tt.expectPosixMode, info.Mode())
+			}
 
 			content, err := os.ReadFile(file)
 			require.NoError(t, err)
