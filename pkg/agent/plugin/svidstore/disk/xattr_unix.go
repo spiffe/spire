1ed9fceb46b8f899542b24fdd5a914922a2f4fd1
Agustín Martínez Fayó <amartinezfayo@gmail.com>
2021-12-03 14:37:12 -0300

    SVIDStore `disk` plugin
    Signed-off-by: Agustín Martínez Fayó <amartinezfayo@gmail.com>

--- pkg/agent/plugin/svidstore/disk/xattr_unix.go
+++ pkg/agent/plugin/svidstore/disk/xattr_unix.go
@@ -11,14 +11,14 @@ import (
 )
 
 func setxattr(filePath, attr string, data []byte) error {
-	if err := unix.Setxattr(filePath, attr, data, 0); !errors.Is(err, unix.ENOTSUP) {
+	if err := unix.Setxattr(filePath, attr, data, 0); err != nil && !errors.Is(err, unix.ENOTSUP) {
 		return fmt.Errorf("error setting extended attribute: %w", err)
 	}
 	return nil
 }
 
 func getxattr(filePath, attr string, dest []byte) (err error) {
-	if _, err = unix.Getxattr(filePath, attr, dest); !errors.Is(err, unix.ENOTSUP) {
+	if _, err = unix.Getxattr(filePath, attr, dest); err != nil && !errors.Is(err, unix.ENOTSUP) {
 		return fmt.Errorf("error getting extended attribute: %w", err)
 	}
 	return nil
