41e3b84ced7f812e49ccc2d7e518b56611aab0b8
Marcos Yacob <marcos.yacob@hpe.com>
2022-01-11 10:21:47 -0300

    Solve unit tests to work in windows and posix (#2655)
    * Solve unit tests to run on windows and posix
    
    Signed-off-by: Marcos Yacob <marcos.yacob@hpe.com>

---
+++ test/spiretest/errors_posix.go
@@ -0,0 +1,20 @@
+//go:build !windows
+// +build !windows
+
+package spiretest
+
+const (
+	fileNotFound = "no such file or directory"
+)
+
+func PathNotFound() string {
+	return fileNotFound
+}
+
+func FileNotFound() string {
+	return fileNotFound
+}
+
+func SocketFileNotFound() string {
+	return fileNotFound
+}
