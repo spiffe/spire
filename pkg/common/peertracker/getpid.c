0b107519f228e690607f690d11414d9d4948b1c2
Agustín Martínez Fayó <amartinezfayo@gmail.com>
2022-02-03 12:36:53 -0300

    Fix indentation
    Signed-off-by: Agustín Martínez Fayó <amartinezfayo@gmail.com>

--- pkg/common/peertracker/getpid.c
+++ pkg/common/peertracker/getpid.c
@@ -4,49 +4,49 @@
 
 unsigned long getOwningPIDFromLocalConn(int localPort, int remotePort, int *pid)
 {
-    PMIB_TCPTABLE_OWNER_PID pTcpTable = NULL;
-    DWORD dwSize = 0;
-    unsigned long retCode = 0;
+	PMIB_TCPTABLE_OWNER_PID pTcpTable = NULL;
+	DWORD dwSize = 0;
+	unsigned long retCode = 0;
 
 	for (;;) {
-        if (dwSize > 0) {
-            pTcpTable = (PMIB_TCPTABLE_OWNER_PID)calloc(dwSize, 1);
-            if (pTcpTable == NULL) {
-                return ERROR_OUTOFMEMORY;
-            }
-        }
+		if (dwSize > 0) {
+			pTcpTable = (PMIB_TCPTABLE_OWNER_PID)calloc(dwSize, 1);
+			if (pTcpTable == NULL) {
+				return ERROR_OUTOFMEMORY;
+			}
+		}
 
-        retCode = GetExtendedTcpTable(pTcpTable, &dwSize, TRUE, AF_INET, TCP_TABLE_OWNER_PID_ALL, 0);
-        if (retCode == NO_ERROR) {
-            break;
-        }
-        if (retCode != ERROR_INSUFFICIENT_BUFFER) {
-            free(pTcpTable);
-            return retCode;
-        }
+		retCode = GetExtendedTcpTable(pTcpTable, &dwSize, TRUE, AF_INET, TCP_TABLE_OWNER_PID_ALL, 0);
+		if (retCode == NO_ERROR) {
+			break;
+		}
+		if (retCode != ERROR_INSUFFICIENT_BUFFER) {
+			free(pTcpTable);
+			return retCode;
+		}
 	}
 
-    for (DWORD i = 0; i < pTcpTable->dwNumEntries; i++) {
-        if (pTcpTable->table[i].dwLocalAddr == htonl(INADDR_LOOPBACK) &&
-            pTcpTable->table[i].dwRemoteAddr == htonl(INADDR_LOOPBACK)  &&
-            ntohs((u_short)pTcpTable->table[i].dwLocalPort) == localPort &&
-            ntohs((u_short)pTcpTable->table[i].dwRemotePort) == remotePort &&
-            pTcpTable->table[i].dwState == MIB_TCP_STATE_ESTAB) {
-                *pid = pTcpTable->table[i].dwOwningPid;
+	for (DWORD i = 0; i < pTcpTable->dwNumEntries; i++) {
+		if (pTcpTable->table[i].dwLocalAddr == htonl(INADDR_LOOPBACK) &&
+			pTcpTable->table[i].dwRemoteAddr == htonl(INADDR_LOOPBACK)  &&
+			ntohs((u_short)pTcpTable->table[i].dwLocalPort) == localPort &&
+			ntohs((u_short)pTcpTable->table[i].dwRemotePort) == remotePort &&
+			pTcpTable->table[i].dwState == MIB_TCP_STATE_ESTAB) {
+				*pid = pTcpTable->table[i].dwOwningPid;
 
-                if (pTcpTable != NULL) {
-                    free(pTcpTable);
-                    pTcpTable = NULL;
-                }
+				if (pTcpTable != NULL) {
+					free(pTcpTable);
+					pTcpTable = NULL;
+				}
 
-                return NO_ERROR;
-        }
-    }
-    
-    if (pTcpTable != NULL) {
-        free(pTcpTable);
-        pTcpTable = NULL;
-    }
+				return NO_ERROR;
+		}
+	}
 
-    return ERROR_NOT_FOUND;
+	if (pTcpTable != NULL) {
+		free(pTcpTable);
+		pTcpTable = NULL;
+	}
+
+	return ERROR_NOT_FOUND;
 }
