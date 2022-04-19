4dae598505ac20d508c892db09f433cf88faa9e3
Andrew Harding <aharding@vmware.com>
2022-04-19 13:23:11 -0600

    Remove deprecated "ensure leading slash" behavior (#2963)
    SPIRE APIs and agent path templates have allowed paths to be
    specified/generated that did not have a leading slash. This behavior was
    deprecated in 1.2 and is now being removed for 1.3.
    
    Signed-off-by: Andrew Harding <aharding@vmware.com>

--- pkg/common/agentpathtemplate/template.go
+++ pkg/common/agentpathtemplate/template.go
@@ -3,17 +3,6 @@ package agentpathtemplate
 import (
 	"bytes"
 	"text/template"
-	"time"
-
-	"github.com/sirupsen/logrus"
-	"github.com/spiffe/spire/pkg/common/idutil"
-	"github.com/spiffe/spire/pkg/common/telemetry"
-	"golang.org/x/time/rate"
-)
-
-var (
-	ensureLeadingSlashLog        logrus.FieldLogger
-	ensureLeadingSlashLogLimiter = rate.NewLimiter(rate.Every(time.Minute), 1)
 )
 
 // Parse parses an agent path template. It changes the behavior for missing
@@ -49,21 +38,5 @@ func (t *Template) Execute(args interface{}) (string, error) {
 	if err := t.tmpl.Execute(buf, args); err != nil {
 		return "", err
 	}
-	path := buf.String()
-
-	// Deprecated: remove in SPIRE 1.3
-	ensuredPath, modified := idutil.EnsureLeadingSlashForBackcompat(path)
-	if modified && ensureLeadingSlashLog != nil && ensureLeadingSlashLogLimiter.Allow() {
-		ensureLeadingSlashLog.WithField(telemetry.Path, path).
-			Warn("Support for agent path templates which produce paths without leading slashes are deprecated and will be removed in a future release")
-	}
-
-	return ensuredPath, nil
-}
-
-// SetEnsureLeadingSlashLog sets the logger used to report "ensure leading slash"
-// related deprecation warnings. Called by server/server.go. This is a hack.
-// Deprecated: remove in SPIRE 1.3
-func SetEnsureLeadingSlashLog(log logrus.FieldLogger) {
-	ensureLeadingSlashLog = log
+	return buf.String(), nil
 }
