a553967da8e0b7bd1696da694c12887660b232e9
Anton Kaymakchi <tonysignal@gmail.com>
2022-04-29 17:56:00 +0100

    Add option to disable Envoy SDS custom validation (#3009)
    Signed-off-by: Anton Kaymakchi <anton.kaymakchi@transferwise.com>

--- pkg/agent/endpoints/endpoints_test.go
+++ pkg/agent/endpoints/endpoints_test.go
@@ -158,15 +158,16 @@ func TestEndpoints(t *testing.T) {
 			metrics := fakemetrics.New()
 			addr := getTestAddr(t)
 			endpoints := New(Config{
-				BindAddr:                addr,
-				Log:                     log,
-				Metrics:                 metrics,
-				Attestor:                FakeAttestor{},
-				Manager:                 FakeManager{},
-				DefaultSVIDName:         "DefaultSVIDName",
-				DefaultBundleName:       "DefaultBundleName",
-				DefaultAllBundlesName:   "DefaultAllBundlesName",
-				AllowedForeignJWTClaims: tt.allowedClaims,
+				BindAddr:                    addr,
+				Log:                         log,
+				Metrics:                     metrics,
+				Attestor:                    FakeAttestor{},
+				Manager:                     FakeManager{},
+				DefaultSVIDName:             "DefaultSVIDName",
+				DefaultBundleName:           "DefaultBundleName",
+				DefaultAllBundlesName:       "DefaultAllBundlesName",
+				DisableSPIFFECertValidation: true,
+				AllowedForeignJWTClaims:     tt.allowedClaims,
 
 				// Assert the provided config and return a fake Workload API server
 				newWorkloadAPIServer: func(c workload.Config) workload_pb.SpiffeWorkloadAPIServer {
@@ -199,6 +200,7 @@ func TestEndpoints(t *testing.T) {
 					assert.Equal(t, "DefaultSVIDName", c.DefaultSVIDName)
 					assert.Equal(t, "DefaultBundleName", c.DefaultBundleName)
 					assert.Equal(t, "DefaultAllBundlesName", c.DefaultAllBundlesName)
+					assert.Equal(t, true, c.DisableSPIFFECertValidation)
 					return FakeSDSv3Server{Attestor: attestor}
 				},
 
