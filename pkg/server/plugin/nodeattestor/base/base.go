2d8113a6f51a6e84a912fbcc55fb4a2a69c2a7ad
Andrew Harding <aharding@vmware.com>
2022-06-30 14:08:51 -0600

    Log agent SPIFFE ID when TOFU check fails
    Attestors currently do not return to the agent or even log which agent
    failed a TOFU check during attestation.
    
    To prevent information leakage, we probably don't want to include the
    agent ID in the error returned from the server. However, we should be at
    least logging this information on the server to facilitate debugging.
    
    Due to the current attestation flow, this is a little hard to log from
    the core service handler. However, each nodeattestor that enforces TOFU
    obtains that information from a common helper.
    
    This change updates that helper to log a message that includes the
    SPIFFE ID as well as a uniform PermissionDenied status to callers.
    
    Fixes: #3205
    
    Signed-off-by: Andrew Harding <aharding@vmware.com>

--- pkg/server/plugin/nodeattestor/base/base.go
+++ pkg/server/plugin/nodeattestor/base/base.go
@@ -4,9 +4,13 @@ import (
 	"context"
 	"errors"
 
+	"github.com/hashicorp/go-hclog"
 	"github.com/spiffe/spire-plugin-sdk/pluginsdk"
 	agentstorev1 "github.com/spiffe/spire-plugin-sdk/proto/spire/hostservice/server/agentstore/v1"
+	"github.com/spiffe/spire/pkg/common/telemetry"
 	"github.com/spiffe/spire/pkg/server/hostservice/agentstore"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Base struct {
@@ -22,6 +26,15 @@ func (p *Base) BrokerHostServices(broker pluginsdk.ServiceBroker) error {
 	return nil
 }
 
-func (p *Base) IsAttested(ctx context.Context, agentID string) (bool, error) {
-	return agentstore.IsAttested(ctx, p.store, agentID)
+func (p *Base) AssessTOFU(ctx context.Context, agentID string, log hclog.Logger) error {
+	attested, err := agentstore.IsAttested(ctx, p.store, agentID)
+	switch {
+	case err != nil:
+		return err
+	case attested:
+		log.Error("Attestation data has already been used to attest an agent", telemetry.SPIFFEID, agentID)
+		return status.Error(codes.PermissionDenied, "attestation data has already been used to attest an agent")
+	default:
+		return nil
+	}
 }
