7bb386d3ef52d3027fb891bcb3f77f8c20c6bca4
Marcos Yacob <marcos.yacob@hpe.com>
2022-07-22 19:35:44 -0300

    resolve conflicts
    Signed-off-by: Marcos Yacob <marcos.yacob@hpe.com>

--- pkg/agent/plugin/nodeattestor/k8spsat/psat_posix_test.go
+++ pkg/agent/plugin/nodeattestor/k8spsat/psat_posix_test.go
@@ -1,7 +1,7 @@
 //go:build !windows
 // +build !windows
 
-package psat
+package k8spsat
 
 import (
 	"testing"
--- pkg/agent/plugin/nodeattestor/k8ssat/sat_windows_test.go
+++ pkg/agent/plugin/nodeattestor/k8ssat/sat_windows_test.go
@@ -1,7 +1,7 @@
 //go:build windows
 // +build windows
 
-package sat
+package k8ssat
 
 import (
 	"testing"
