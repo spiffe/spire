ec9a133cdb73012ef2d1c3b6f6d1e0577bff2d0f
Prasad Borole <prasadb@uber.com>
2022-07-26 13:40:24 -0700

    fix lint
    Signed-off-by: Prasad Borole <prasadb@uber.com>

--- pkg/agent/api/delegatedidentity/v1/service.go
+++ pkg/agent/api/delegatedidentity/v1/service.go
@@ -121,6 +121,10 @@ func (s *Service) SubscribeToX509SVIDs(req *delegatedidentityv1.SubscribeToX509S
 	}
 
 	subscriber, err := s.manager.SubscribeToCacheChanges(ctx, selectors)
+	if err != nil {
+		log.WithError(err).Error("Subscribe to cache changes failed")
+		return err
+	}
 	defer subscriber.Finish()
 
 	for {
