9562435bafa55c288b401aeb3cfe0265bf270d6e
Ryuma Yoshida <ryumyosh@zlab.co.jp>
2022-09-04 18:07:18 +0900

    Fix incorrect data type
    Signed-off-by: Ryuma Yoshida <ryumyosh@zlab.co.jp>

--- pkg/server/bundle/client/client.go
+++ pkg/server/bundle/client/client.go
@@ -73,7 +73,7 @@ func NewClient(config ClientConfig) (Client, error) {
 func (c *client) FetchBundle(ctx context.Context) (*bundleutil.Bundle, error) {
 	resp, err := c.client.Get(c.c.EndpointURL)
 	if err != nil {
-		var hostnameError *x509.HostnameError
+		var hostnameError x509.HostnameError
 		if errors.As(err, &hostnameError) && c.c.SPIFFEAuth == nil && len(hostnameError.Certificate.URIs) > 0 {
 			if id, idErr := spiffeid.FromString(hostnameError.Certificate.URIs[0].String()); idErr == nil {
 				return nil, errs.New("failed to authenticate bundle endpoint using web authentication but the server certificate contains SPIFFE ID %q: maybe use https_spiffe instead of https_web: %v", id, err)
