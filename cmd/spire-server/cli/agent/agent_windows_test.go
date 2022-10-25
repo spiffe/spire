5ee823122f7fe6274eb73234df42e95da2d85b8b
Guilherme Carvalho <guilhermbrsp@gmail.com>
2022-10-25 19:42:38 -0300

    Update agent-windows-test vars
    Signed-off-by: Guilherme Carvalho <guilhermbrsp@gmail.com>

--- cmd/spire-server/cli/agent/agent_windows_test.go
+++ cmd/spire-server/cli/agent/agent_windows_test.go
@@ -5,42 +5,42 @@ package agent_test
 
 var (
 	listUsage = `Usage of agent list:
-  -output value
-    	Desired output format (pretty, json)
   -matchSelectorsOn string
     	The match mode used when filtering by selectors. Options: exact, any, superset and subset (default "superset")
   -namedPipeName string
     	Pipe name of the SPIRE Server API named pipe (default "\\spire-server\\private\\api")
+  -output value
+    	Desired output format (pretty, json)
   -selector value
     	A colon-delimited type:value selector. Can be used more than once
 `
 	banUsage = `Usage of agent ban:
-  -output value
-    	Desired output format (pretty, json)
   -namedPipeName string
     	Pipe name of the SPIRE Server API named pipe (default "\\spire-server\\private\\api")
+  -output value
+    	Desired output format (pretty, json)
   -spiffeID string
     	The SPIFFE ID of the agent to ban (agent identity)
 `
 	evictUsage = `Usage of agent evict:
-  -output value
-    	Desired output format (pretty, json)
   -namedPipeName string
     	Pipe name of the SPIRE Server API named pipe (default "\\spire-server\\private\\api")
+  -output value
+    	Desired output format (pretty, json)
   -spiffeID string
     	The SPIFFE ID of the agent to evict (agent identity)
 `
 	countUsage = `Usage of agent count:
-  -output value
-    	Desired output format (pretty, json)
   -namedPipeName string
     	Pipe name of the SPIRE Server API named pipe (default "\\spire-server\\private\\api")
+  -output value
+    	Desired output format (pretty, json)
 `
 	showUsage = `Usage of agent show:
-  -output value
-    	Desired output format (pretty, json)
   -namedPipeName string
     	Pipe name of the SPIRE Server API named pipe (default "\\spire-server\\private\\api")
+  -output value
+    	Desired output format (pretty, json)
   -spiffeID string
     	The SPIFFE ID of the agent to show (agent identity)
 `
