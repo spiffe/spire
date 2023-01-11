45febb17934c17507a59733bb62ad6264c622329
Guilherme Carvalho <guilhermbrsp@gmail.com>
2023-01-11 09:15:39 -0300

    Increase test coverage
    Signed-off-by: Guilherme Carvalho <guilhermbrsp@gmail.com>

--- pkg/common/cliprinter/internal/protojson/protojson_test.go
+++ pkg/common/cliprinter/internal/protojson/protojson_test.go
@@ -2,12 +2,14 @@ package protojson
 
 import (
 	"bytes"
+	"encoding/json"
 	"testing"
 
 	agentapi "github.com/spiffe/spire-api-sdk/proto/spire/api/server/agent/v1"
 	"github.com/spiffe/spire-api-sdk/proto/spire/api/server/trustdomain/v1"
 	"github.com/spiffe/spire-api-sdk/proto/spire/api/types"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -65,6 +67,49 @@ func TestPrint(t *testing.T) {
 			assert.Nil(t, err)
 			assert.Equal(t, c.stdout, stdout.String())
 			assert.Equal(t, c.stderr, stderr.String())
+		})
+	}
+}
+
+func TestRemoveNulls(t *testing.T) {
+	cases := []struct {
+		name   string
+		input  []byte
+		output []byte
+	}{
+		{
+			name:   "remove null values",
+			input:  []byte(`{"nullField":null,"int":1,"string":"value","bool":true,"array":[1,2,3],"object":{"key":"value"}}`),
+			output: []byte(`{"int":1,"string":"value","bool":true,"array":[1,2,3],"object":{"key":"value"}}`),
+		},
+		{
+			name:   "remove nested null values",
+			input:  []byte(`{"someField":{"nestedField1":{"nestedField2": null}}}`),
+			output: []byte(`{"someField":{"nestedField1":{}}}`),
+		},
+		{
+			name:   "remove null values from array",
+			input:  []byte(`{"someFieldArray":[null,{"nestedField1":null},null,{"nestedField2":"value"},null]}`),
+			output: []byte(`{"someFieldArray":[{},{"nestedField2":"value"}]}`),
+		},
+		{
+			name:   "remove null values from nested arrays",
+			input:  []byte(`{"someFieldArray":[[null,1,null,2,[null,null,null,3]],[null,{"nestedField2":"value"}]]}`),
+			output: []byte(`{"someFieldArray":[[1,2,[3]],[{"nestedField2":"value"}]]}`),
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			var input, output map[string]interface{}
+			err := json.Unmarshal(c.input, &input)
+			require.NoError(t, err)
+			err = json.Unmarshal(c.output, &output)
+			require.NoError(t, err)
+
+			removeNulls(input)
+
+			assert.Equal(t, output, input)
 		})
 	}
 }
