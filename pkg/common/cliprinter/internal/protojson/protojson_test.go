e9da888aea8aa6b11336a713cca80510f2939596
Guilherme Oliveira do Carmo Carvalho <33766735+guilhermocc@users.noreply.github.com>
2023-01-12 17:14:07 -0300

    Remove null values from cli json priting (#3715)
    * Remove fields with null value from proto-json conversion result
    
    Signed-off-by: Guilherme Carvalho <guilhermbrsp@gmail.com>
    
    * Fix broken tests from federation command
    
    Signed-off-by: Guilherme Carvalho <guilhermbrsp@gmail.com>
    
    * Refactor variable names
    
    Signed-off-by: Guilherme Carvalho <guilhermbrsp@gmail.com>
    
    * Address review comments
    
    Signed-off-by: Guilherme Carvalho <guilhermbrsp@gmail.com>
    
    * Add logic for interface array nil values
    
    Signed-off-by: Guilherme Carvalho <guilhermbrsp@gmail.com>
    
    * Refactor code
    
    Signed-off-by: Guilherme Carvalho <guilhermbrsp@gmail.com>
    
    * Increase test coverage
    
    Signed-off-by: Guilherme Carvalho <guilhermbrsp@gmail.com>
    
    Signed-off-by: Guilherme Carvalho <guilhermbrsp@gmail.com>
    Co-authored-by: Agustín Martínez Fayó <amartinezfayo@gmail.com>

--- pkg/common/cliprinter/internal/protojson/protojson_test.go
+++ pkg/common/cliprinter/internal/protojson/protojson_test.go
@@ -2,10 +2,14 @@ package protojson
 
 import (
 	"bytes"
+	"encoding/json"
 	"testing"
 
 	agentapi "github.com/spiffe/spire-api-sdk/proto/spire/api/server/agent/v1"
+	"github.com/spiffe/spire-api-sdk/proto/spire/api/server/trustdomain/v1"
+	"github.com/spiffe/spire-api-sdk/proto/spire/api/types"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -19,13 +23,13 @@ func TestPrint(t *testing.T) {
 		{
 			name:      "normal_protobuf_message",
 			protoFunc: normalCountAgentsResponseMessage,
-			stdout:    "{\"count\":42}\n",
+			stdout:    `{"count":42}` + "\n",
 			stderr:    "",
 		},
 		{
 			name:      "double_protobuf_message",
 			protoFunc: doubleCountAgentsResponseMessage,
-			stdout:    "[{\"count\":42},{\"count\":42}]\n",
+			stdout:    `[{"count":42},{"count":42}]` + "\n",
 			stderr:    "",
 		},
 		{
@@ -41,9 +45,15 @@ func TestPrint(t *testing.T) {
 			stderr:    "",
 		},
 		{
-			name:      "message_with_unpopulated_fields",
-			protoFunc: unpopulatedFieldsMessage,
-			stdout:    "{\"count\":0}\n",
+			name:      "message_with_zeroed_values",
+			protoFunc: zeroedValuesMessage,
+			stdout:    `{"count":0}` + "\n",
+			stderr:    "",
+		},
+		{
+			name:      "message_with_null_pointers",
+			protoFunc: nullPointerMessage,
+			stdout:    `{"federation_relationships":[{"bundle_endpoint_url":"https://example.org/bundle","trust_domain":"example.org"}],"next_page_token":""}` + "\n",
 			stderr:    "",
 		},
 	}
@@ -61,6 +71,49 @@ func TestPrint(t *testing.T) {
 	}
 }
 
+func TestRemoveNulls(t *testing.T) {
+	cases := []struct {
+		name   string
+		input  []byte
+		output []byte
+	}{
+		{
+			name:   "remove null values",
+			input:  []byte(`{"nullField":null,"int":1,"string":"value","bool":true,"array":[1,2,3],"object":{"key":"value"}}`),
+			output: []byte(`{"int":1,"string":"value","bool":true,"array":[1,2,3],"object":{"key":"value"}}`),
+		},
+		{
+			name:   "remove nested null values",
+			input:  []byte(`{"someField":{"nestedField1":{"nestedField2": null}}}`),
+			output: []byte(`{"someField":{"nestedField1":{}}}`),
+		},
+		{
+			name:   "remove null values from array",
+			input:  []byte(`{"someFieldArray":[null,{"nestedField1":null},null,{"nestedField2":"value"},null]}`),
+			output: []byte(`{"someFieldArray":[{},{"nestedField2":"value"}]}`),
+		},
+		{
+			name:   "remove null values from nested arrays",
+			input:  []byte(`{"someFieldArray":[[null,1,null,2,[null,null,null,3]],[null,{"nestedField2":"value"}]]}`),
+			output: []byte(`{"someFieldArray":[[1,2,[3]],[{"nestedField2":"value"}]]}`),
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			var input, output map[string]interface{}
+			err := json.Unmarshal(c.input, &input)
+			require.NoError(t, err)
+			err = json.Unmarshal(c.output, &output)
+			require.NoError(t, err)
+
+			removeNulls(input)
+
+			assert.Equal(t, output, input)
+		})
+	}
+}
+
 func normalCountAgentsResponseMessage(_ *testing.T) []proto.Message {
 	return []proto.Message{
 		&agentapi.CountAgentsResponse{
@@ -69,10 +122,21 @@ func normalCountAgentsResponseMessage(_ *testing.T) []proto.Message {
 	}
 }
 
-func unpopulatedFieldsMessage(_ *testing.T) []proto.Message {
+func zeroedValuesMessage(_ *testing.T) []proto.Message {
 	return []proto.Message{
-		&agentapi.CountAgentsResponse{
-			Count: int32(0),
+		&agentapi.CountAgentsResponse{},
+	}
+}
+
+func nullPointerMessage(_ *testing.T) []proto.Message {
+	return []proto.Message{
+		&trustdomain.ListFederationRelationshipsResponse{
+			FederationRelationships: []*types.FederationRelationship{
+				{
+					TrustDomain:       "example.org",
+					BundleEndpointUrl: "https://example.org/bundle",
+				},
+			},
 		},
 	}
 }
