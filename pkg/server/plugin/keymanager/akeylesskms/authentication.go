2b434c06aba2813954a558383efe44a22ab61135
Dmitry Gorochovsky <d.goro@yahoo.com>
2023-05-07 15:10:42 +0300

    Akeyless plugin implementation

---
+++ pkg/server/plugin/keymanager/akeylesskms/authentication.go
@@ -0,0 +1,169 @@
+package akeylesskms
+
+import (
+	"context"
+	"fmt"
+
+	"reflect"
+	"runtime"
+	"sync"
+	"time"
+
+	"github.com/akeylesslabs/akeyless-go/v3"
+	log "github.com/hashicorp/go-hclog"
+
+	"github.com/akeylesslabs/akeyless-go-cloud-id/cloudprovider/aws"
+	"github.com/akeylesslabs/akeyless-go-cloud-id/cloudprovider/azure"
+	"github.com/akeylesslabs/akeyless-go-cloud-id/cloudprovider/gcp"
+)
+
+const (
+	authenticationInterval = time.Second * 870 // 14.5 minutes
+)
+
+var (
+	akeylessAuthToken string
+	mutexAuthToken    = &sync.RWMutex{}
+	authenticator     = func(ctx context.Context, aklClient *akeyless.V2ApiService) error { return nil }
+)
+
+func setAuthToken(t string) {
+	mutexAuthToken.Lock()
+	defer mutexAuthToken.Unlock()
+
+	akeylessAuthToken = t
+}
+
+func GetAuthToken() string {
+	mutexAuthToken.RLock()
+	defer mutexAuthToken.RUnlock()
+
+	return akeylessAuthToken
+}
+
+func (c *Config) authenticate(ctx context.Context, aklClient *akeyless.V2ApiService, authBody *akeyless.Auth) error {
+	authBody.SetAccessId(c.AkeylessAccessID)
+
+	authOut, _, err := aklClient.Auth(ctx).Body(*authBody).Execute()
+	if err != nil {
+		return fmt.Errorf("authentication failed %v, %w", c.AkeylessGatewayURL, err)
+	}
+
+	setAuthToken(authOut.GetToken())
+	return nil
+}
+
+func (c *Config) authWithAccessKey(ctx context.Context, aklClient *akeyless.V2ApiService) error {
+	authBody := akeyless.NewAuthWithDefaults()
+	authBody.SetAccessType(string(AccessKey))
+	authBody.SetAccessKey(c.AkeylessAccessKey)
+	err := c.authenticate(ctx, aklClient, authBody)
+
+	if err != nil {
+		log.L().Error("authWithAccessKey ERR: %v", err.Error())
+	}
+	return err
+}
+
+func (c *Config) authWithAWS(ctx context.Context, aklClient *akeyless.V2ApiService) error {
+	authBody := akeyless.NewAuthWithDefaults()
+	authBody.SetAccessType(string(AWSIAM))
+	cloudId, err := aws.GetCloudId()
+	if err != nil {
+		return fmt.Errorf("requested access type %v but failed to get cloud ID, error: %v", AWSIAM, err)
+	}
+	authBody.SetCloudId(cloudId)
+	return c.authenticate(ctx, aklClient, authBody)
+}
+
+func (c *Config) authWithAzure(ctx context.Context, aklClient *akeyless.V2ApiService) error {
+	authBody := akeyless.NewAuthWithDefaults()
+	authBody.SetAccessType(string(AzureAD))
+	cloudId, err := azure.GetCloudId(c.AkeylessAzureObjectID)
+	if err != nil {
+		return fmt.Errorf("requested access type %v but failed to get cloud ID, error: %v", AzureAD, err)
+	}
+	authBody.SetCloudId(cloudId)
+	return c.authenticate(ctx, aklClient, authBody)
+}
+
+func (c *Config) authWithGCP(ctx context.Context, aklClient *akeyless.V2ApiService) error {
+	authBody := akeyless.NewAuthWithDefaults()
+	authBody.SetAccessType(string(GCP))
+	cloudId, err := gcp.GetCloudID(c.AkeylessGCPAudience)
+	if err != nil {
+		return fmt.Errorf("requested access type %v but failed to get cloud ID, error: %v", GCP, err)
+	}
+	authBody.SetCloudId(cloudId)
+	return c.authenticate(ctx, aklClient, authBody)
+}
+
+func (c *Config) StartAuthentication(ctx context.Context, closed chan bool) error {
+	accType := c.AkeylessAccessType
+
+	switch accessType(accType) {
+	case AccessKey:
+		authenticator = c.authWithAccessKey
+
+	case AWSIAM:
+		authenticator = c.authWithAWS
+
+	case AzureAD:
+		authenticator = c.authWithAzure
+
+	case GCP:
+		authenticator = c.authWithGCP
+	}
+
+	// Get new token every authenticationInterval seconds
+	runForeverWithContext(ctx, func() error {
+		ticker := time.NewTicker(authenticationInterval)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ctx.Done():
+				closed <- true
+				return nil
+			case <-ticker.C:
+				log.Default().Info("retrieving new token")
+				err := authenticator(ctx, AklClient)
+				if err != nil {
+					return err
+				}
+				log.Default().Info("successfully retrieved new token")
+			}
+		}
+	}, closed)
+
+	return nil
+}
+
+func getFunctionName(i interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+}
+
+func runForeverWithContext(ctx context.Context, fn func() error, notifier chan bool) {
+	runForeverWithContextEx(ctx, fn, "daemon", notifier)
+}
+
+func runForeverWithContextEx(ctx context.Context, fn func() error, routineType string, notifier chan bool) {
+	go func() {
+		t := time.NewTicker(time.Second)
+		defer t.Stop()
+
+		for {
+			select {
+			case <-ctx.Done():
+				notifier <- true
+				return
+			case <-t.C:
+				func() {
+					err := fn()
+					if err != nil {
+						log.Default().Error("%s %s ended with an error. %s", routineType, getFunctionName(fn), err)
+					}
+				}()
+			}
+		}
+	}()
+}
---
+++ pkg/server/plugin/keymanager/akeylesskms/utils.go
@@ -0,0 +1,73 @@
+package akeylesskms
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"fmt"
+
+	keymanagerv1 "github.com/spiffe/spire-plugin-sdk/proto/spire/plugin/server/keymanager/v1"
+)
+
+func keySpecFromKeyType(keyType keymanagerv1.KeyType) string {
+	switch keyType {
+	case keymanagerv1.KeyType_RSA_2048:
+		return "RSA2048"
+	case keymanagerv1.KeyType_RSA_4096:
+		return "RSA4096"
+	case keymanagerv1.KeyType_EC_P256:
+		return "EC256"
+	case keymanagerv1.KeyType_EC_P384:
+		return "EC384"
+	default:
+		return ""
+	}
+}
+
+func keyTypeFromKeySpec(keySpec string) keymanagerv1.KeyType {
+	switch keySpec {
+	case "RSA2048":
+		return keymanagerv1.KeyType_RSA_2048
+	case "RSA4096":
+		return keymanagerv1.KeyType_RSA_4096
+	case "EC256":
+		return keymanagerv1.KeyType_EC_P256
+	case "EC384":
+		return keymanagerv1.KeyType_EC_P384
+	default:
+		return keymanagerv1.KeyType_UNSPECIFIED_KEY_TYPE
+	}
+}
+
+func makeFingerprint(pkixData []byte) string {
+	s := sha256.Sum256(pkixData)
+	return hex.EncodeToString(s[:])
+}
+
+func defineSigningAlgorithm(keyType keymanagerv1.KeyType, signerOpts interface{}) (string, error) {
+	var hashAlgo keymanagerv1.HashAlgorithm
+	switch opts := signerOpts.(type) {
+	case *keymanagerv1.SignDataRequest_HashAlgorithm:
+		hashAlgo = opts.HashAlgorithm
+	case *keymanagerv1.SignDataRequest_PssOptions:
+		if opts.PssOptions == nil {
+			return "", errors.New("PSS options are required")
+		}
+		hashAlgo = opts.PssOptions.HashAlgorithm
+	default:
+		return "", fmt.Errorf("unsupported signer opts type %T", opts)
+	}
+
+	switch hashAlgo {
+	case keymanagerv1.HashAlgorithm_UNSPECIFIED_HASH_ALGORITHM:
+		return "", errors.New("hash algorithm is required")
+	case keymanagerv1.HashAlgorithm_SHA256:
+		return "SHA256", nil
+	case keymanagerv1.HashAlgorithm_SHA384:
+		return "SHA384", nil
+	case keymanagerv1.HashAlgorithm_SHA512:
+		return "SHA512", nil
+	default:
+		return "", fmt.Errorf("unsupported combination of keytype: %v and hashing algorithm: %v", keyType, hashAlgo)
+	}
+}
