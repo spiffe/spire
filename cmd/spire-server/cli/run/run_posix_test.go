135e990241a792a19b242f50bfb6a26931b07999
Guilherme Carvalho <guilhermocc@proton.me>
2023-06-12 14:08:25 -0300

    Fix spire server run command tests
    Signed-off-by: Guilherme Carvalho <guilhermocc@proton.me>

--- cmd/spire-server/cli/run/run_posix_test.go
+++ cmd/spire-server/cli/run/run_posix_test.go
@@ -6,7 +6,9 @@ package run
 import (
 	"bytes"
 	"fmt"
+	"net"
 	"os"
+	"strconv"
 	"strings"
 	"syscall"
 	"testing"
@@ -27,6 +29,8 @@ const (
 )
 
 func TestCommand_Run(t *testing.T) {
+	availablePort, err := getAvailablePort()
+	require.NoError(t, err)
 	testTempDir := t.TempDir()
 	testLogFile := testTempDir + "/spire-server.log"
 
@@ -74,6 +78,7 @@ func TestCommand_Run(t *testing.T) {
 			args: args{
 				args: []string{
 					"-config", startConfigFile,
+					"-serverPort", strconv.Itoa(availablePort),
 					"-namedPipeName", "\\spire-agent\\public\\api",
 				},
 			},
@@ -95,6 +100,7 @@ func TestCommand_Run(t *testing.T) {
 			args: args{
 				args: []string{
 					"-config", crashConfigFile,
+					"-serverPort", strconv.Itoa(availablePort),
 					"-dataDir", fmt.Sprintf("%s/crash/data", testTempDir),
 					"-expandEnv", "true",
 				},
@@ -116,6 +122,7 @@ func TestCommand_Run(t *testing.T) {
 			name: "create data dir when config is loaded and server stops",
 			args: args{
 				args: []string{
+					"-serverPort", strconv.Itoa(availablePort),
 					"-config", startConfigFile,
 					"-dataDir", fmt.Sprintf("%s/data", testTempDir),
 					"-expandEnv", "true",
@@ -266,4 +273,13 @@ func newServerConfigCasesOS() []newServerConfigCase {
 
 func testParseConfigGoodOS(t *testing.T, c *Config) {
 	assert.Equal(t, c.Server.SocketPath, "/tmp/spire-server/private/api-test.sock")
+}
+
+func getAvailablePort() (int, error) {
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		return -1, err
+	}
+	defer l.Close()
+	return l.Addr().(*net.TCPAddr).Port, nil
 }
