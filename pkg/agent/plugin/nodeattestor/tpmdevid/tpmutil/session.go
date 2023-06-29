2c8e1b2f656205274f2bbd755c80c447dea9c66e
Ryan Turner <turner@uber.com>
2023-06-29 13:37:37 -0700

    Update Go module paths
    Signed-off-by: Ryan Turner <turner@uber.com>

--- pkg/agent/plugin/nodeattestor/tpmdevid/tpmutil/session.go
+++ pkg/agent/plugin/nodeattestor/tpmdevid/tpmutil/session.go
@@ -7,7 +7,7 @@ import (
 	"io"
 
 	"github.com/google/go-tpm-tools/client"
-	"github.com/google/go-tpm/tpm2"
+	"github.com/google/go-tpm/legacy/tpm2"
 	"github.com/google/go-tpm/tpmutil"
 	"github.com/hashicorp/go-hclog"
 	"github.com/spiffe/spire/pkg/common/plugin/tpmdevid"
