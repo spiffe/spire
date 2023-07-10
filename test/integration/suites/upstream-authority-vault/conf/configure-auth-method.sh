d17b2a71f1176955ea02da9107ea3096d646b2e4
Tomoya Usami <tousami@zlab.co.jp>
2023-07-10 14:23:59 +0900

    Add token renewal test
    Signed-off-by: Tomoya Usami <tousami@zlab.co.jp>

--- test/integration/suites/upstream-authority-vault/conf/configure-auth-method.sh
+++ test/integration/suites/upstream-authority-vault/conf/configure-auth-method.sh
@@ -9,8 +9,7 @@ vault policy write spire /tmp/spire.hcl
 vault auth enable approle
 vault write auth/approle/role/spire \
       secret_id_ttl=120m \
-      token_ttl=60m \
-      token_max_tll=120m \
+      token_ttl=1m \
       policies="spire"
 
 # Configure K8s Auth Method
@@ -19,11 +18,13 @@ vault write auth/kubernetes/config kubernetes_host=https://$KUBERNETES_SERVICE_H
 vault write auth/kubernetes/role/my-role \
       bound_service_account_names=spire-server \
       bound_service_account_namespaces=spire \
+      token_ttl=1m \
       policies=spire
 
 # Configure Cert Auth Method
 vault auth enable cert
 vault write auth/cert/certs/my-role \
       display_name=spire \
+      token_ttl=1m \
       policies=spire \
       certificate=@/tmp/cert_auth_ca.pem
