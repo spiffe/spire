07ca5e889036510d816b55feb197f9cdf4e6b4d8
Andrew Harding <azdagron@gmail.com>
2023-09-13 15:48:22 -0600

    address some nits
    Signed-off-by: Andrew Harding <azdagron@gmail.com>

--- .github/workflows/scripts/build_linux_artifacts.sh
+++ .github/workflows/scripts/build_linux_artifacts.sh
@@ -7,12 +7,6 @@ REPODIR=$(git rev-parse --show-toplevel)
 TAG=${TAG:-$(git log -n1 --pretty=%h)}
 OUTDIR=${OUTDIR:-"${REPODIR}/artifacts"}
 
-TARCMD=tar
-if [[ $(uname -s) == "Darwin" ]]; then
-    # When building linux artifacts from darwin, gtar is required.
-    TARCMD="gtar"
-fi
-
 TAROPTS=("--owner=root" "--group=root")
 
 TMPDIR=$(mktemp -d)
@@ -20,7 +14,6 @@ cleanup() {
     rm -rf "${TMPDIR}"
 }
 trap cleanup EXIT
-
 
 copy_binary_from_multiarch_tar() {
     local arch=$1
@@ -44,50 +37,48 @@ copy_binary_from_multiarch_tar() {
 }
 
 build_artifact() {
-    ARCH="$1"
+    local arch="$1"
 
-    ARTIFACT="${OUTDIR}/spire-${TAG}-linux-${ARCH}-musl.tar.gz"
-    CHECKSUM="${OUTDIR}/spire-${TAG}-linux-${ARCH}-musl_sha256sum.txt"
+    local artifact="${OUTDIR}/spire-${TAG}-linux-${arch}-musl.tar.gz"
+    local checksum="${OUTDIR}/spire-${TAG}-linux-${arch}-musl_sha256sum.txt"
 
-    EXTRAS_ARTIFACT="${OUTDIR}/spire-extras-${TAG}-linux-${ARCH}-musl.tar.gz"
-    EXTRAS_CHECKSUM="${OUTDIR}/spire-extras-${TAG}-linux-${ARCH}-musl_sha256sum.txt"
+    local extras_artifact="${OUTDIR}/spire-extras-${TAG}-linux-${arch}-musl.tar.gz"
+    local extras_checksum="${OUTDIR}/spire-extras-${TAG}-linux-${arch}-musl_sha256sum.txt"
 
-    TARDIR="${TMPDIR}/${ARCH}/tar" 
-    mkdir -p "${TARDIR}"
+    local tardir="${TMPDIR}/${arch}/tar" 
+    local staging="${tardir}"/spire/spire-${TAG}
+    local extras_staging="${tardir}"/spire-extras/spire-extras-${TAG}
 
-    STAGING="${TARDIR}"/spire/spire-${TAG}
-    EXTRAS_STAGING="${TARDIR}"/spire-extras/spire-extras-${TAG}
-    mkdir -p "${STAGING}" "${EXTRAS_STAGING}"
+    mkdir -p "${tardir}"
+    mkdir -p "${staging}"/bin 
+    mkdir -p "${extras_staging}"/bin
+    mkdir -p "${OUTDIR}"
 
-    echo "Creating \"${ARTIFACT}\" and \"${EXTRAS_ARTIFACT}\""
+    echo "Creating \"${artifact}\" and \"${extras_artifact}\""
 
     # Copy in the contents under release/
-    cp -r "${REPODIR}"/release/posix/spire/* "${STAGING}"
-    cp -r "${REPODIR}"/release/posix/spire-extras/* "${EXTRAS_STAGING}"
+    cp -r "${REPODIR}"/release/posix/spire/* "${staging}"
+    cp -r "${REPODIR}"/release/posix/spire-extras/* "${extras_staging}"
 
     # Copy in the LICENSE
-    cp "${REPODIR}"/LICENSE "${STAGING}"
-    cp "${REPODIR}"/LICENSE "${EXTRAS_STAGING}"
+    cp "${REPODIR}"/LICENSE "${staging}"
+    cp "${REPODIR}"/LICENSE "${extras_staging}"
 
     # Copy in the SPIRE binaries from the docker images:
     # 1. import the image from the multiarch tarball into the OCI directory
-    mkdir -p "${STAGING}"/bin "${EXTRAS_STAGING}"/bin
-    copy_binary_from_multiarch_tar "$ARCH" "spire-server" "${STAGING}/bin"
-    copy_binary_from_multiarch_tar "$ARCH" "spire-agent" "${STAGING}/bin"
-    copy_binary_from_multiarch_tar "$ARCH" "oidc-discovery-provider" "${EXTRAS_STAGING}/bin"
-
-    mkdir -p "${OUTDIR}"
+    copy_binary_from_multiarch_tar "$arch" "spire-server" "${staging}/bin"
+    copy_binary_from_multiarch_tar "$arch" "spire-agent" "${staging}/bin"
+    copy_binary_from_multiarch_tar "$arch" "oidc-discovery-provider" "${extras_staging}/bin"
 
     # Create the tarballs and checksums
-    (cd "${TARDIR}/spire"; ${TARCMD} -cvzf "${ARTIFACT}" "${TAROPTS[@]}" -- *)
-    (cd "${TARDIR}/spire-extras"; ${TARCMD} -cvzf "${EXTRAS_ARTIFACT}" "${TAROPTS[@]}" -- *)
+    (cd "${tardir}/spire"; tar -cvzf "${artifact}" "${TAROPTS[@]}" -- *)
+    (cd "${tardir}/spire-extras"; tar -cvzf "${extras_artifact}" "${TAROPTS[@]}" -- *)
 
-    (cd "$(dirname "${ARTIFACT}")"; shasum -a 256 "$(basename "${ARTIFACT}")" > "${CHECKSUM}" )
-    (cd "$(dirname "${EXTRAS_ARTIFACT}")"; shasum -a 256 "$(basename "${EXTRAS_ARTIFACT}")" > "${EXTRAS_CHECKSUM}" )
+    (cd "$(dirname "${artifact}")"; shasum -a 256 "$(basename "${artifact}")" > "${checksum}" )
+    (cd "$(dirname "${extras_artifact}")"; shasum -a 256 "$(basename "${extras_artifact}")" > "${extras_checksum}" )
 }
 
 command -v regctl >/dev/null 2>&1 || { echo -e "The regctl cli is required to run this script." >&2 ; exit 1; }
-command -v "${TARCMD}" >/dev/null 2>&1 || { echo -e "The ${TARCMD} command is required to run this script." >&2 ; exit 1; }
 
 build_artifact amd64
 build_artifact arm64
