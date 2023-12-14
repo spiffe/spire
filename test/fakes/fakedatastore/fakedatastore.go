29b94901abd6dc2f32c8838e2a4941f2c14b8f60
Faisal Memon <fymemon@yahoo.com>
2023-12-14 08:55:14 -0800

    Make events based cache flag experimental (#4723)
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>

--- test/fakes/fakedatastore/fakedatastore.go
+++ test/fakes/fakedatastore/fakedatastore.go
@@ -36,7 +36,7 @@ var _ datastore.DataStore = (*DataStore)(nil)
 func New(tb testing.TB) *DataStore {
 	log, _ := test.NewNullLogger()
 
-	ds := sql.New(log)
+	ds := sql.New(log, false)
 	ds.SetUseServerTimestamps(true)
 
 	err := ds.Configure(ctx, fmt.Sprintf(`
