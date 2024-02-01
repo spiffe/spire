374111aefeaa49b74b9124367d020e242f8bb5d6
Kevin Fox <Kevin.Fox@pnnl.gov>
2024-02-01 13:57:52 -0800

    Incorperate feedback
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- cmd/spire-agent/cli/common/defaults_windows.go
+++ cmd/spire-agent/cli/common/defaults_windows.go
@@ -5,4 +5,6 @@ package common
 const (
 	// DefaultNamedPipeName is the SPIRE agent's default named pipe name
 	DefaultNamedPipeName = "\\spire-agent\\public\\api"
+	// DefaultAdminNamedPipeName is the SPIRE agent's default admin named pipe name
+	DefaultAdminSocketPath = "\\spire-agent\\private\\admin"
 )
