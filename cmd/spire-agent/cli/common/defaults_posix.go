9da8bc95bc6c2778bc4c5a1ea7095c35e4d1f909
kfox1111 <Kevin.Fox@pnnl.gov>
2024-02-02 07:28:29 -0800

    Set a default admin socket path for spire-agent cli use (#4856)
    * Set a default admin socket path for spire-agent cli use
    
    Set a standard default location the spire-agent cli
    will use to look for the agent socket when using commands against
    the spire-agent daemon. Actual functionality needing this variable
    will come in future patches.
    
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>
    
    * Incorperate feedback
    
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>
    
    * Fix filename
    
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>
    
    ---------
    
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>
    Signed-off-by: Faisal Memon <fymemon@yahoo.com>

--- cmd/spire-agent/cli/common/defaults_posix.go
+++ cmd/spire-agent/cli/common/defaults_posix.go
@@ -5,4 +5,6 @@ package common
 const (
 	// DefaultSocketPath is the SPIRE agent's default socket path
 	DefaultSocketPath = "/tmp/spire-agent/public/api.sock"
+	// DefaultAdminSocketPath is the SPIRE agent's default admin socket path
+	DefaultAdminSocketPath = "/tmp/spire-agent/private/admin.sock"
 )
