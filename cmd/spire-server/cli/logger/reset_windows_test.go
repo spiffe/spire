44ad1b3fdb6b4ab51a1bdfd25518baae47193092
Marcos Yacob <marcosyacob@gmail.com>
2024-03-11 17:08:04 -0300

    Resolve identation issues on windows...
    Signed-off-by: Marcos Yacob <marcosyacob@gmail.com>

--- cmd/spire-server/cli/logger/reset_windows_test.go
+++ cmd/spire-server/cli/logger/reset_windows_test.go
@@ -5,8 +5,8 @@ package logger_test
 var (
 	resetUsage = `Usage of logger reset:
   -namedPipeName string
-	Pipe name of the SPIRE Server API named pipe (default "\\spire-server\\private\\api")
+    	Pipe name of the SPIRE Server API named pipe (default "\\spire-server\\private\\api")
   -output value
-	Desired output format (pretty, json); default: pretty.
+    	Desired output format (pretty, json); default: pretty.
 `
 )
