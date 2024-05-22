cba8c8cf10987a5897ea721bd2536032602ec565
Andrew Harding <azdagron@gmail.com>
2024-05-22 11:29:11 -0600

    fix lint on windows
    Signed-off-by: Andrew Harding <azdagron@gmail.com>

--- pkg/common/catalog/configure_windows.go
+++ pkg/common/catalog/configure_windows.go
@@ -2,7 +2,7 @@ package catalog
 
 import "context"
 
-func ReconfigureOnSignal(ctx context.Context, reconfigurer Reconfigurer) error {
+func ReconfigureOnSignal(ctx context.Context, _ Reconfigurer) error {
 	// TODO: maybe drive this using an event?
 	<-ctx.Done()
 	return ctx.Err()
