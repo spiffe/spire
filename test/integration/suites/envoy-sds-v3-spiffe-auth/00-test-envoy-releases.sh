d16e1e8fb1d9a463c4822473803b73eaf250e7da
Marcos Yacob <marcosyacob@gmail.com>
2024-08-03 17:55:36 -0300

    Update ITs to use docker compose
    Signed-off-by: Marcos Yacob <marcosyacob@gmail.com>

--- test/integration/suites/envoy-sds-v3-spiffe-auth/00-test-envoy-releases.sh
+++ test/integration/suites/envoy-sds-v3-spiffe-auth/00-test-envoy-releases.sh
@@ -7,24 +7,24 @@ setup-tests() {
 
     # Bootstrap agents
     log-debug "bootstrapping downstream federated agent..."
-    docker-compose exec -T downstream-federated-spire-server \
+    docker compose exec -T downstream-federated-spire-server \
         /opt/spire/bin/spire-server bundle show > conf/downstream-federated/agent/bootstrap.crt
     
     log-debug "bootstrapping upstream agent..."
-    docker-compose exec -T upstream-spire-server \
+    docker compose exec -T upstream-spire-server \
         /opt/spire/bin/spire-server bundle show > conf/upstream/agent/bootstrap.crt
 
-    docker-compose exec -T upstream-spire-server \
+    docker compose exec -T upstream-spire-server \
         /opt/spire/bin/spire-server bundle show > conf/downstream/agent/bootstrap.crt
     
     log-debug "creating federation relationship from downstream federated to upstream server and set bundle in same command..."
-    docker-compose exec -T downstream-federated-spire-server \
+    docker compose exec -T downstream-federated-spire-server \
         /opt/spire/bin/spire-server bundle show -format spiffe > conf/upstream/server/federated-domain.test.bundle
 
     # On macOS, there can be a delay propagating the file on the bind mount to the other container
     sleep 1
 
-    docker-compose exec -T upstream-spire-server \
+    docker compose exec -T upstream-spire-server \
         /opt/spire/bin/spire-server federation create \
         -bundleEndpointProfile "https_spiffe" \
         -bundleEndpointURL "https://downstream-federated-spire-server:8443" \
@@ -34,17 +34,17 @@ setup-tests() {
         -trustDomainBundlePath "/opt/spire/conf/server/federated-domain.test.bundle"
     
     log-debug "bootstrapping bundle from upstream to downstream federated server..."
-    docker-compose exec -T upstream-spire-server \
+    docker compose exec -T upstream-spire-server \
         /opt/spire/bin/spire-server bundle show -format spiffe > conf/downstream-federated/server/domain.test.bundle
 
     # On macOS, there can be a delay propagating the file on the bind mount to the other container
     sleep 1
 
-    docker-compose exec -T downstream-federated-spire-server \
+    docker compose exec -T downstream-federated-spire-server \
         /opt/spire/bin/spire-server bundle set -format spiffe -id spiffe://domain.test -path /opt/spire/conf/server/domain.test.bundle
 
     log-debug "creating federation relationship from upstream to downstream federated server..."
-    docker-compose exec -T downstream-federated-spire-server \
+    docker compose exec -T downstream-federated-spire-server \
         /opt/spire/bin/spire-server federation create \
         -bundleEndpointProfile "https_spiffe" \
         -bundleEndpointURL "https://upstream-spire-server" \
@@ -53,7 +53,7 @@ setup-tests() {
 
     # Register workloads
     log-debug "creating registration entry for downstream federated proxy..."
-    docker-compose exec -T downstream-federated-spire-server \
+    docker compose exec -T downstream-federated-spire-server \
         /opt/spire/bin/spire-server entry create \
         -parentID "spiffe://federated-domain.test/spire/agent/x509pop/$(fingerprint conf/downstream-federated/agent/agent.crt.pem)" \
         -spiffeID "spiffe://federated-domain.test/downstream-proxy" \
@@ -62,7 +62,7 @@ setup-tests() {
         -ttl 0
     
     log-debug "creating registration entry for upstream proxy..."
-    docker-compose exec -T upstream-spire-server \
+    docker compose exec -T upstream-spire-server \
         /opt/spire/bin/spire-server entry create \
         -parentID "spiffe://domain.test/spire/agent/x509pop/$(fingerprint conf/upstream/agent/agent.crt.pem)" \
         -spiffeID "spiffe://domain.test/upstream-proxy" \
@@ -71,7 +71,7 @@ setup-tests() {
         -ttl 0
 
     log-debug "creating registration entry for downstream proxy..."
-    docker-compose exec -T upstream-spire-server \
+    docker compose exec -T upstream-spire-server \
         /opt/spire/bin/spire-server entry create \
         -parentID "spiffe://domain.test/spire/agent/x509pop/$(fingerprint conf/downstream/agent/agent.crt.pem)" \
         -spiffeID "spiffe://domain.test/downstream-proxy" \
@@ -87,11 +87,11 @@ test-envoy() {
     local check_interval=1
 	
     # Remove howdy, it i necessary for VERIFY to get again messages
-    docker-compose exec -T upstream-socat rm -f /tmp/howdy
+    docker compose exec -T upstream-socat rm -f /tmp/howdy
     
     log-debug "Checking mTLS: ${mTLSSocat}"
-    TRY() { docker-compose exec -T ${mTLSSocat} /bin/sh -c 'echo HELLO_MTLS | socat -u STDIN TCP:localhost:8001'; }
-    VERIFY() { docker-compose exec -T upstream-socat cat /tmp/howdy | grep -q HELLO_MTLS; }
+    TRY() { docker compose exec -T ${mTLSSocat} /bin/sh -c 'echo HELLO_MTLS | socat -u STDIN TCP:localhost:8001'; }
+    VERIFY() { docker compose exec -T upstream-socat cat /tmp/howdy | grep -q HELLO_MTLS; }
     
     local mtls_federated_ok=
     for ((i=1;i<=max_checks_per_port;i++)); do
@@ -105,8 +105,8 @@ test-envoy() {
     done
     
     log-debug "Checking TLS: ${tlsSocat}"
-    TRY() { docker-compose exec -T ${tlsSocat} /bin/sh -c 'echo HELLO_TLS | socat -u STDIN TCP:localhost:8002'; }
-    VERIFY() { docker-compose exec -T upstream-socat cat /tmp/howdy | grep -q HELLO_TLS; }
+    TRY() { docker compose exec -T ${tlsSocat} /bin/sh -c 'echo HELLO_TLS | socat -u STDIN TCP:localhost:8002'; }
+    VERIFY() { docker compose exec -T upstream-socat cat /tmp/howdy | grep -q HELLO_TLS; }
     
     tls_federated_ok=
     for ((i=1;i<=max_checks_per_port;i++)); do
@@ -160,7 +160,7 @@ for release in "${ENVOY_RELEASES_TO_TEST[@]}"; do
     test-envoy "downstream-federated-socat-mtls" "downstream-federated-socat-tls"
 
     # stop and clear everything but the server container
-    docker-compose stop \
+    docker compose stop \
         upstream-proxy \
         downstream-proxy \
         downstream-federated-proxy \
@@ -170,5 +170,5 @@ for release in "${ENVOY_RELEASES_TO_TEST[@]}"; do
         downstream-federated-socat-mtls \
         downstream-federated-socat-tls
 
-    docker-compose rm -f
+    docker compose rm -f
 done
