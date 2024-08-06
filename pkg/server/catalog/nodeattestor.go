5b1966b5c86df9946d8f62d655c41dc27c053750
kfox1111 <Kevin.Fox@pnnl.gov>
2024-08-05 20:37:45 -0700

    Add http node attestor (#4909)
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- pkg/server/catalog/nodeattestor.go
+++ pkg/server/catalog/nodeattestor.go
@@ -6,6 +6,7 @@ import (
 	"github.com/spiffe/spire/pkg/server/plugin/nodeattestor/awsiid"
 	"github.com/spiffe/spire/pkg/server/plugin/nodeattestor/azuremsi"
 	"github.com/spiffe/spire/pkg/server/plugin/nodeattestor/gcpiit"
+	"github.com/spiffe/spire/pkg/server/plugin/nodeattestor/httpchallenge"
 	"github.com/spiffe/spire/pkg/server/plugin/nodeattestor/jointoken"
 	"github.com/spiffe/spire/pkg/server/plugin/nodeattestor/k8spsat"
 	"github.com/spiffe/spire/pkg/server/plugin/nodeattestor/k8ssat"
@@ -37,6 +38,7 @@ func (repo *nodeAttestorRepository) BuiltIns() []catalog.BuiltIn {
 		awsiid.BuiltIn(),
 		azuremsi.BuiltIn(),
 		gcpiit.BuiltIn(),
+		httpchallenge.BuiltIn(),
 		jointoken.BuiltIn(),
 		k8spsat.BuiltIn(),
 		k8ssat.BuiltIn(),
