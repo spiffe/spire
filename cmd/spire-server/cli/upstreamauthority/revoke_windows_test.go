d5faa6dce7489d6331463efcb523cfe5faaeef9d
Agustín Martínez Fayó <amartinezfayo@gmail.com>
2024-09-25 15:43:12 -0300

    Fix TestRevokeHelp Windows test
    Signed-off-by: Agustín Martínez Fayó <amartinezfayo@gmail.com>

--- cmd/spire-server/cli/upstreamauthority/revoke_windows_test.go
+++ cmd/spire-server/cli/upstreamauthority/revoke_windows_test.go
@@ -4,10 +4,10 @@ package upstreamauthority_test
 
 var (
 	revokeUsage = `Usage of upstreamauthority revoke:
-  -output value
-    	Desired output format (pretty, json); default: pretty.
   -namedPipeName string
     	Pipe name of the SPIRE Server API named pipe (default "\\spire-server\\private\\api")
+  -output value
+    	Desired output format (pretty, json); default: pretty.
   -subjectKeyID string
     	The X.509 Subject Key Identifier (or SKID) of the authority's CA certificate of the upstream X.509 authority to revoke
 `
