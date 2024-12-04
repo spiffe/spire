063b205fb30c5004567e05b793f7e42bf0d4069a
Agustín Martínez Fayó <amartinezfayo@gmail.com>
2024-12-03 23:51:00 -0300

    Validate server cli command
    Signed-off-by: Agustín Martínez Fayó <amartinezfayo@gmail.com>

--- cmd/spire-server/cli/validate/validate.go
+++ cmd/spire-server/cli/validate/validate.go
@@ -1,9 +1,13 @@
 package validate
 
 import (
+	"context"
+
 	"github.com/mitchellh/cli"
 	"github.com/spiffe/spire/cmd/spire-server/cli/run"
 	commoncli "github.com/spiffe/spire/pkg/common/cli"
+	"github.com/spiffe/spire/pkg/common/log"
+	"github.com/spiffe/spire/pkg/server"
 )
 
 const commandName = "validate"
@@ -32,8 +36,18 @@ func (c *validateCommand) Synopsis() string {
 }
 
 func (c *validateCommand) Run(args []string) int {
-	if _, err := run.LoadConfig(commandName, args, nil, c.env.Stderr, false); err != nil {
+	config, err := run.LoadConfig(commandName, args, []log.Option{log.WithOutputFile("/dev/null")}, c.env.Stderr, false)
+
+	if err != nil {
 		// Ignore error since a failure to write to stderr cannot very well be reported
+		_ = c.env.ErrPrintf("SPIRE server configuration file is invalid: %v\n", err)
+		return 1
+	}
+
+	s := server.New(*config)
+
+	err = s.ValidateConfig(context.Background())
+	if err != nil {
 		_ = c.env.ErrPrintf("SPIRE server configuration file is invalid: %v\n", err)
 		return 1
 	}
