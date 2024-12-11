d316d78eff629034d55f526ffe0f5c5591bce63e
Amit Upadhyay <amit.upadhyay021@outlook.com>
2024-12-12 01:17:42 +0530

    Remove the deprecated k8s_sat node attestor plugin
    Signed-off-by: Amit Upadhyay <amit.upadhyay021@outlook.com>

--- pkg/server/catalog/nodeattestor.go
+++ pkg/server/catalog/nodeattestor.go
@@ -9,7 +9,6 @@ import (
 	"github.com/spiffe/spire/pkg/server/plugin/nodeattestor/httpchallenge"
 	"github.com/spiffe/spire/pkg/server/plugin/nodeattestor/jointoken"
 	"github.com/spiffe/spire/pkg/server/plugin/nodeattestor/k8spsat"
-	"github.com/spiffe/spire/pkg/server/plugin/nodeattestor/k8ssat"
 	"github.com/spiffe/spire/pkg/server/plugin/nodeattestor/sshpop"
 	"github.com/spiffe/spire/pkg/server/plugin/nodeattestor/tpmdevid"
 	"github.com/spiffe/spire/pkg/server/plugin/nodeattestor/x509pop"
@@ -41,7 +40,6 @@ func (repo *nodeAttestorRepository) BuiltIns() []catalog.BuiltIn {
 		httpchallenge.BuiltIn(),
 		jointoken.BuiltIn(),
 		k8spsat.BuiltIn(),
-		k8ssat.BuiltIn(),
 		sshpop.BuiltIn(),
 		tpmdevid.BuiltIn(),
 		x509pop.BuiltIn(),
