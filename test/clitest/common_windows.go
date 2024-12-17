189eb5268def97d9d9f238a7307031888799585f
Ryan Turner <ryan.turner253@icloud.com>
2024-12-17 01:15:55 -0800

    Fix capitalization of expected error message in Windows tests
    Signed-off-by: Ryan Turner <ryan.turner253@icloud.com>

--- test/clitest/common_windows.go
+++ test/clitest/common_windows.go
@@ -4,7 +4,7 @@ package clitest
 
 var (
 	AddrArg         = "-namedPipeName"
-	AddrError       = "rpc error: code = Unavailable desc = connection error: desc = \"transport: error while dialing: open \\\\\\\\.\\\\pipe\\\\does-not-exist: The system cannot find the file specified.\"\n"
+	AddrError       = "rpc error: code = Unavailable desc = connection error: desc = \"transport: Error while dialing: open \\\\\\\\.\\\\pipe\\\\does-not-exist: The system cannot find the file specified.\"\n"
 	AddrOutputUsage = `
   -namedPipeName string
     	Pipe name of the SPIRE Server API named pipe (default "\\spire-server\\private\\api")
