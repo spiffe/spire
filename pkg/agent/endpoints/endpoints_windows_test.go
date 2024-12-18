98c88e996aa7ddc66a2001bd103993320e9b2303
Ryan Turner <ryan.turner253@icloud.com>
2024-12-17 19:37:27 -0800

    Attempt to fix Windows unit test
    Signed-off-by: Ryan Turner <ryan.turner253@icloud.com>

--- pkg/agent/endpoints/endpoints_windows_test.go
+++ pkg/agent/endpoints/endpoints_windows_test.go
@@ -3,6 +3,7 @@
 package endpoints
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"os"
@@ -13,6 +14,7 @@ import (
 	"github.com/spiffe/spire/test/spiretest"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/sys/windows"
+	"google.golang.org/grpc/health/grpc_health_v1"
 )
 
 func getTestAddr(*testing.T) net.Addr {
@@ -25,7 +27,11 @@ func testRemoteCaller(t *testing.T, target string) {
 
 	// Use the host name instead of "." in the target, as it would be a remote caller
 	targetAsRemote := strings.ReplaceAll(target, "\\\\.\\", fmt.Sprintf("\\\\%s\\", hostName))
-	_, err = util.NewGRPCClient(targetAsRemote)
+	conn, err := util.NewGRPCClient(targetAsRemote)
+	require.NoError(t, err)
+
+	healthClient := grpc_health_v1.NewHealthClient(conn)
+	_, err = healthClient.Check(context.Background(), &grpc_health_v1.HealthCheckRequest{})
 
 	// Remote calls must be denied
 	require.ErrorIs(t, err, windows.ERROR_ACCESS_DENIED)
