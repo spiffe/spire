d708a650f274f55e5abbb5bf1b29b84567793c19
Kevin Fox <Kevin.Fox@pnnl.gov>
2025-01-15 07:11:59 -0800

    SPIFFE_ENDPOINT_SOCKET env suport for spire-agent
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- cmd/spire-agent/cli/common/defaults_posix.go
+++ cmd/spire-agent/cli/common/defaults_posix.go
@@ -2,9 +2,24 @@
 
 package common
 
+import (
+	"os"
+)
+
 const (
-	// DefaultSocketPath is the SPIRE agent's default socket path
-	DefaultSocketPath = "/tmp/spire-agent/public/api.sock"
+	// DefaultRunSocketPath is the SPIRE agent's default socket path
+	DefaultRunSocketPath = "/tmp/spire-agent/public/api.sock"
 	// DefaultAdminSocketPath is the SPIRE agent's default admin socket path
 	DefaultAdminSocketPath = "/tmp/spire-agent/private/admin.sock"
 )
+
+// DefaultSocketPath is the SPIRE agent's default socket path
+var DefaultSocketPath string
+
+func init() {
+	DefaultSocketPath = DefaultRunSocketPath
+	ses := os.Getenv("SPIFFE_ENDPOINT_SOCKET")
+	if ses != "" {
+		DefaultSocketPath = ses
+	}
+}
