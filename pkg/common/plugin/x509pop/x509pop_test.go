db83c24bcb40104d2e7fbc2dcd6a5717f2842c8d
Ryan Turner <ryan.turner253@icloud.com>
2025-02-24 11:28:57 -0800

    Replace hardcoded keys with ones generated by testkey
    Signed-off-by: Ryan Turner <ryan.turner253@icloud.com>

--- pkg/common/plugin/x509pop/x509pop_test.go
+++ pkg/common/plugin/x509pop/x509pop_test.go
@@ -1,53 +1,28 @@
 package x509pop
 
 import (
-	"crypto/ecdsa"
 	"crypto/rand"
-	"crypto/rsa"
 	"crypto/x509"
 	"crypto/x509/pkix"
-	"encoding/pem"
 	"math/big"
 	"testing"
 
 	"github.com/spiffe/go-spiffe/v2/spiffeid"
 	"github.com/spiffe/spire/pkg/common/agentpathtemplate"
+	"github.com/spiffe/spire/test/testkey"
 	"github.com/stretchr/testify/require"
 )
 
-const (
-	testRSAKey = `-----BEGIN PRIVATE KEY-----
-MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAL3dnfnX6Q5czIJt
-WqnxeLPmmDJrTZFVmbTEhcMgPY1GRV2a09Gg5zVIipluuaCqdLlBI6qCgHV1zWvE
-9glfiZMSgJeuO9a7S0ndvttSuWcIrVGfz4xVENCIir3elHAYGCjkkV8CjDrw4byd
-GAHWCS4bmUq5VZBrxguRSuKmu8f/AgMBAAECgYAgWUf3L4A3wDvf5P3Tgp7MF/fK
-b1nR70G2+NzJeA7EqTpcTmrUhpOeEnP63FCoPSKSBbUr8fwtBxsux2ocArkPmG6c
-4OTWNkp5d1XzbEtS4kHsyF5V305kZ7W5O5ilMXuNvf9QZmh0CZhh1gDH8TOLNCWc
-l+uvAlDL18829mF34QJBAO+VKgITRZJJz3AEGqo3lB81Q+JHpNVqhQHUorUx/m6Y
-cUkrtRQB1RGk604En8ZBOcEaJb2PbCH5TkvMdlcmTVcCQQDK4E1D8KVyif1YPNJO
-Y03mj8gshDsBi+PZYdoE+swJUMfw732Bkrupq8vMMhssLRkW2BVP9GL4UpT/iwPd
-rImZAkB0qfUnx4ykPK73g/gcFS5auAxTXsHEbrdP+eQ1SZ/06OSZVeA6Wt4PVniC
-qDvJRa/C+WGRWgq0zHq07F5f97XHAkB6wxneF7t1wMw93agA6RRVhQHC9kziVa0N
-eKm9w6k9pdUnFTRj+M94D03mBRfuNmftK9pRpHdS/l3M+GjhdatZAkEAxHFUYXQE
-8cLac+6VtceeKdMAjAOejinwT5pB20cGuX8Z+sRXTwhP9TYeF7BCbKB7JRp+uB3o
-I9uYKLUGAFfSBA==
------END PRIVATE KEY-----`
-	testECDSAKey = `-----BEGIN PRIVATE KEY-----
-MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgMmjo28H7LEOxWD2t
-74mWp5XPrZwzb/VyukdPxHGOoOOhRANCAARhpK2KSCTiyeNZzrB8c2eZ4K+yZGrp
-4MpWREMXQMIwbP/QWGYXQ8GWhp16J6IYXkywB/SJnKPY+iV6Mnbxp31K
------END PRIVATE KEY-----`
+var (
+	testRSAKey   = testkey.MustRSA2048()
+	testECDSAKey = testkey.MustEC256()
 )
 
 func TestChallengeResponse(t *testing.T) {
 	require := require.New(t)
 
 	// load up RSA key and create a self-signed certificate over the public key
-	pemBlock, _ := pem.Decode([]byte(testRSAKey))
-	require.NotNil(pemBlock)
-	privateKey, err := x509.ParsePKCS8PrivateKey(pemBlock.Bytes)
-	require.NoError(err)
-	rsaPrivateKey := privateKey.(*rsa.PrivateKey)
+	rsaPrivateKey := testRSAKey
 	rsaPublicKey := &rsaPrivateKey.PublicKey
 	rsaCert, err := createCertificate(rsaPrivateKey, rsaPublicKey)
 	require.NoError(err)
@@ -61,11 +36,7 @@ func TestChallengeResponse(t *testing.T) {
 	require.NoError(err)
 
 	// load up ECDSA key and create a self-signed certificate over the public key
-	pemBlock, _ = pem.Decode([]byte(testECDSAKey))
-	require.NotNil(pemBlock)
-	privateKey, err = x509.ParsePKCS8PrivateKey(pemBlock.Bytes)
-	require.NoError(err)
-	ecdsaPrivateKey := privateKey.(*ecdsa.PrivateKey)
+	ecdsaPrivateKey := testECDSAKey
 	ecdsaPublicKey := &ecdsaPrivateKey.PublicKey
 	ecdsaCert, err := createCertificate(ecdsaPrivateKey, ecdsaPublicKey)
 	require.NoError(err)
