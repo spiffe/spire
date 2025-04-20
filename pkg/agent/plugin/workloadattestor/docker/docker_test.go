96c6ef356bc2ac653905fe91ea4757c9010769d4
Sorin Dumitru <sdumitru@bloomberg.net>
2025-04-20 09:23:43 +0100

    Update docker engine API version in test
    Signed-off-by: Sorin Dumitru <sdumitru@bloomberg.net>

--- pkg/agent/plugin/workloadattestor/docker/docker_test.go
+++ pkg/agent/plugin/workloadattestor/docker/docker_test.go
@@ -236,7 +236,7 @@ func TestDockerConfigDefault(t *testing.T) {
 
 	require.NotNil(t, p.docker)
 	require.Equal(t, dockerclient.DefaultDockerHost, p.docker.(*dockerclient.Client).DaemonHost())
-	require.Equal(t, "1.48", p.docker.(*dockerclient.Client).ClientVersion())
+	require.Equal(t, "1.49", p.docker.(*dockerclient.Client).ClientVersion())
 	verifyConfigDefault(t, p.c)
 }
 
