7d2716ba3487e52a054d129f2ab7ea11cfb5444b
Carlo Teubner <cteubner1@bloomberg.net>
2025-05-08 19:35:20 +0100

    Delete some dead code
    I ran
        go run golang.org/x/tools/cmd/deadcode@latest -test ./...
    and manually reviewed the result.
    
    I selected a subset of "dead code" (never-called functions) to delete. I
    did not delete some other functions that seemed like they could still be
    useful in future.
    
    One function flagged as dead was "DeltaSecretsTest" in
    pkg/agent/endpoints/sdsv3/handler_test.go, which seems like it was a
    misspelled function that was intended as a test case. I tried renaming
    it to "TestDeltaSecrets" but it turned out to be broken and hard to fix.
    Since it only tests that an RPC method is unimplemented, I opted to just
    remove the function instead.
    
    Signed-off-by: Carlo Teubner <cteubner1@bloomberg.net>

--- pkg/common/errorutil/wrapper.go
+++ pkg/common/errorutil/wrapper.go
@@ -1,19 +1,10 @@
 package errorutil
 
 import (
-	"fmt"
-
 	"github.com/spiffe/spire-api-sdk/proto/spire/api/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
-
-// WrapError creates a new error in the format: "<newErrStr>: <err>".
-// This function is intended to be used to wrap errors
-// when an error is received from calling a function/method inside a function or private method.
-func WrapError(err error, newErrStr string) error {
-	return fmt.Errorf(newErrStr+": %v", err)
-}
 
 // PermissionDenied formats a PermissionDenied error with an error string.
 func PermissionDenied(reason types.PermissionDeniedDetails_Reason, format string, args ...any) error {
