3b6991ed0fb6f4324af0b025cbaa586566923a76
Kevin Fox <Kevin.Fox@pnnl.gov>
2025-05-09 12:36:48 -0700

    Fix metrics/health checks when retry_bootstrap=true
    When retry_bootstrap is true, during the startup/retrying, health
    checks / metrics are not started. This time is currently is capped at
    5 minutes, requiring fairly long health check delays on Kubernetes,
    and no ability to collect metrics on what its doing.
    
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- pkg/common/util/task.go
+++ pkg/common/util/task.go
@@ -7,6 +7,68 @@ import (
 	"sync"
 )
 
+type TaskRunner struct {
+	wg        sync.WaitGroup
+	ctx       context.Context
+	cancels   []context.CancelFunc
+	errch     chan error
+	taskCount int
+}
+
+func NewTaskRunner(ctx context.Context) *TaskRunner {
+	return &TaskRunner{
+		ctx:   ctx,
+		errch: make(chan error, 1),
+	}
+}
+
+func (t *TaskRunner) StartTasks(tasks ...func(context.Context) error) {
+	ctx, cancel := context.WithCancel(t.ctx)
+	t.cancels = append(t.cancels, cancel)
+
+	runTask := func(task func(context.Context) error) (err error) {
+		defer func() {
+			if r := recover(); r != nil {
+				err = fmt.Errorf("panic: %v\n%s\n", r, string(debug.Stack()))
+			}
+			t.wg.Done()
+		}()
+		return task(ctx)
+	}
+
+	lenTasks := len(tasks)
+	t.taskCount += lenTasks
+	t.wg.Add(lenTasks)
+	for _, task := range tasks {
+		go func() {
+			t.errch <- runTask(task)
+		}()
+	}
+}
+
+func (t *TaskRunner) Wait() error {
+	defer func() {
+		for _, cancel := range t.cancels {
+			cancel()
+		}
+		t.wg.Wait()
+	}()
+
+	for complete := 0; complete < t.taskCount; {
+		select {
+		case <-t.ctx.Done():
+			return t.ctx.Err()
+		case err := <-t.errch:
+			if err != nil {
+				return err
+			}
+			complete++
+		}
+	}
+
+	return nil
+}
+
 // RunTasks executes all the provided functions concurrently and waits for
 // them all to complete. If a function returns an error, all other functions
 // are canceled (i.e. the context they are passed is canceled) and the error is
@@ -16,45 +78,9 @@ import (
 // RunTasks MUST support cancellation via the provided context for RunTasks to
 // work properly.
 func RunTasks(ctx context.Context, tasks ...func(context.Context) error) error {
-	var wg sync.WaitGroup
-	ctx, cancel := context.WithCancel(ctx)
-	defer func() {
-		cancel()
-		wg.Wait()
-	}()
-
-	errch := make(chan error, len(tasks))
-
-	runTask := func(task func(context.Context) error) (err error) {
-		defer func() {
-			if r := recover(); r != nil {
-				err = fmt.Errorf("panic: %v\n%s\n", r, string(debug.Stack()))
-			}
-			wg.Done()
-		}()
-		return task(ctx)
-	}
-
-	wg.Add(len(tasks))
-	for _, task := range tasks {
-		go func() {
-			errch <- runTask(task)
-		}()
-	}
-
-	for complete := 0; complete < len(tasks); {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		case err := <-errch:
-			if err != nil {
-				return err
-			}
-			complete++
-		}
-	}
-
-	return nil
+	t := NewTaskRunner(ctx)
+	t.StartTasks(tasks...)
+	return t.Wait()
 }
 
 // SerialRun executes all the provided functions serially.
