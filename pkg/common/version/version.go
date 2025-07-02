dff8a88df0c0ce1f739fdf6e8a9c83e0eb053bac
Ryan Turner <rturner3@users.noreply.github.com>
2025-07-01 22:48:49 -0700

    Prepare main branch for upcoming 1.13.0 release (#6163)
    * Changelog for v.1.12.4 release (#6162)
    
    Signed-off-by: Agustín Martínez Fayó <amartinezfayo@gmail.com>
    
    * Prepare main branch for upcoming 1.13.0 release
    
    Signed-off-by: Ryan Turner <ryan.turner253@icloud.com>
    
    ---------
    
    Signed-off-by: Agustín Martínez Fayó <amartinezfayo@gmail.com>
    Signed-off-by: Ryan Turner <ryan.turner253@icloud.com>
    Co-authored-by: Agustín Martínez Fayó <amartinezfayo@gmail.com>

--- pkg/common/version/version.go
+++ pkg/common/version/version.go
@@ -8,7 +8,7 @@ const (
 	// IMPORTANT: When updating, make sure to reconcile the versions list that
 	// is part of the upgrade integration test. See
 	// test/integration/suites/upgrade/README.md for details.
-	Base = "1.12.4"
+	Base = "1.13.0"
 )
 
 var (
