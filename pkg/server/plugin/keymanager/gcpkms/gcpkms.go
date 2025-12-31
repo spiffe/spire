108cbd139caa28818a907f15fd01a6101fee04db
Agustín Martínez Fayó <amartinezfayo@gmail.com>
2025-12-31 10:04:55 -0300

    Use option.WithAuthCredentialsFile with option.ServiceAccount instead of option.WithCredentialsFile to authenticates API calls with the given service account credentials file
    Signed-off-by: Agustín Martínez Fayó <amartinezfayo@gmail.com>

--- pkg/server/plugin/keymanager/gcpkms/gcpkms.go
+++ pkg/server/plugin/keymanager/gcpkms/gcpkms.go
@@ -232,7 +232,7 @@ func (p *Plugin) Configure(ctx context.Context, req *configv1.ConfigureRequest) 
 
 	var opts []option.ClientOption
 	if newConfig.ServiceAccountFile != "" {
-		opts = append(opts, option.WithCredentialsFile(newConfig.ServiceAccountFile))
+		opts = append(opts, option.WithAuthCredentialsFile(option.ServiceAccount, newConfig.ServiceAccountFile))
 	}
 
 	kc, err := p.hooks.newKMSClient(ctx, opts...)
