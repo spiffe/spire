a46cb8144d34e25fc7bb7cdae85b3f4de682ea25
Valentin Fadeev <vfadeev@bloomberg.net>
2026-01-08 23:45:00 +0000

    undid the change to fetchEntries
    Signed-off-by: Valentin Fadeev <vfadeev@bloomberg.net>

--- pkg/agent/manager/sync.go
+++ pkg/agent/manager/sync.go
@@ -345,9 +345,7 @@ func (m *manager) fetchEntries(ctx context.Context) (_ *cache.UpdateEntries, _ *
 		case entry.StoreSvid:
 			storeEntries[entryID] = entry
 		default:
-			if !entry.CacheHintFlags.GetDisableX509SvidPrefetch() {
-				cacheEntries[entryID] = entry
-			}
+			cacheEntries[entryID] = entry
 		}
 	}
 
