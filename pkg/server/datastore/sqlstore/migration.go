d156b8f9ad0c61ba1e82ef1f392137a467621152
Valentin Fadeev <vfadeev@bloomberg.net>
2026-02-18 10:51:56 +0000

    created a separate migration for registration entries
    Signed-off-by: Valentin Fadeev <vfadeev@bloomberg.net>

--- pkg/server/datastore/sqlstore/migration.go
+++ pkg/server/datastore/sqlstore/migration.go
@@ -273,7 +273,7 @@ import (
 
 const (
 	// the latest schema version of the database in the code
-	latestSchemaVersion = 24
+	latestSchemaVersion = 25
 
 	// lastMinorReleaseSchemaVersion is the schema version supported by the
 	// last minor release. When the migrations are opportunistically pruned
@@ -506,6 +506,8 @@ func migrateVersion(tx *gorm.DB, currVersion int, log logrus.FieldLogger) (versi
 	switch currVersion {
 	case 23:
 		err = migrateToV24(tx)
+	case 24:
+		err = migrateToV25(tx)
 	default:
 		err = newSQLError("no migration support for unknown schema version %d", currVersion)
 	}
@@ -517,7 +519,16 @@ func migrateVersion(tx *gorm.DB, currVersion int, log logrus.FieldLogger) (versi
 }
 
 func migrateToV24(tx *gorm.DB) error {
-	if err := tx.AutoMigrate(&AttestedNode{}, &RegisteredEntry{}).Error; err != nil {
+	// Add agent_version column to attested_node_entries table
+	if err := tx.AutoMigrate(&AttestedNode{}).Error; err != nil {
+		return newWrappedSQLError(err)
+	}
+	return nil
+}
+
+func migrateToV25(tx *gorm.DB) error {
+	// Add additional_attributes column to registered_entries table
+	if err := tx.AutoMigrate(&RegisteredEntry{}).Error; err != nil {
 		return newWrappedSQLError(err)
 	}
 	return nil
