405aa481836683098ee820933bcd61f9f8dc6988
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-04-11 13:38:48 -0700

    Fix wit mint
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- cmd/spire-server/cli/wit/mint_test.go
+++ cmd/spire-server/cli/wit/mint_test.go
@@ -41,20 +41,6 @@ dlmdCTY3trEN+pXoR+kecSyZFcvjYBaND9mOPsSHCAc5AAtFPQF/j0H/
 -----END PRIVATE KEY-----
 `))
 	availableFormats = []string{"pretty", "json"}
-	expectedUsage    = `Usage of wit mint:
-  -i string
-    	Instance name to substitute into socket templates (env SPIRE_SERVER_PRIVATE_SOCKET_TEMPLATE). If omitted and the env var is set, defaults to 'main'.
-  -keyType string
-    	Key type of the WIT-SVID (default "ec-p256")` + clitest.AddrOutputForCasesWhereOptionsStartWithS +
-		`  -signingAlgorithm string
-    	Signing algorithm for the workload signing key (default "ES256")` + clitest.AddrSocketPathUsageForCasesWhereOptionsStartWithS +
-		`  -spiffeID string
-    	SPIFFE ID of the WIT-SVID
-  -ttl duration
-    	TTL of the WIT-SVID
-  -write string
-    	Directory to write output to instead of stdout
-`
 )
 
 func TestMintSynopsis(t *testing.T) {
