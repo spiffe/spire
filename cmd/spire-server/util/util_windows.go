63b13dcff5c48bd6e546d4eea71b40ba12aa045d
Devansh1093 <dr30031102@gmail.com>
2026-05-21 19:30:57 +0530

    Add Windows instance flag support for named pipe templates
    Signed-off-by: Devansh1093 <dr30031102@gmail.com>

--- cmd/spire-server/util/util_windows.go
+++ cmd/spire-server/util/util_windows.go
@@ -8,16 +8,21 @@ import (
 	"net"
 	"strings"
 
+	"fmt"
+	"os"
+
 	"github.com/Microsoft/go-winio"
 	"github.com/spiffe/spire/pkg/common/namedpipe"
 )
 
 type adapterOS struct {
 	namedPipeName string
+	instance      string
 }
 
 func (a *Adapter) addOSFlags(flags *flag.FlagSet) {
 	flags.StringVar(&a.namedPipeName, "namedPipeName", DefaultNamedPipeName, "Pipe name of the SPIRE Server API named pipe")
+	flags.StringVar(&a.instance, "instance", "", "Instance name to substitute into socket templates (env SPIRE_SERVER_PRIVATE_SOCKET_TEMPLATE).")
 }
 
 func dialer(ctx context.Context, addr string) (net.Conn, error) {
@@ -28,14 +33,41 @@ func dialer(ctx context.Context, addr string) (net.Conn, error) {
 }
 
 func (a *Adapter) getGRPCAddr() (string, error) {
-	if a.namedPipeName == "" {
-		a.namedPipeName = DefaultNamedPipeName
+	tpl := os.Getenv("SPIRE_SERVER_PRIVATE_SOCKET_TEMPLATE")
+	pipe := os.Getenv("SPIRE_SERVER_PRIVATE_SOCKET")
+
+	if a.instance != "" {
+		if tpl == "" {
+			return "", fmt.Errorf(
+				"you must define %s to use the instance flag",
+				"SPIRE_SERVER_PRIVATE_SOCKET_TEMPLATE",
+			)
+		}
+
+		if !strings.Contains(tpl, "%i") {
+			return "", fmt.Errorf(
+				"failed to find %%i in %s",
+				"SPIRE_SERVER_PRIVATE_SOCKET_TEMPLATE",
+			)
+		}
 	}
 
+	namedPipeName := DefaultNamedPipeName
+
+	switch {
+	case a.namedPipeName != DefaultNamedPipeName:
+		namedPipeName = a.namedPipeName
+
+	case a.instance != "":
+		namedPipeName = strings.ReplaceAll(tpl, "%i", a.instance)
+
+	case pipe != "":
+		namedPipeName = pipe
+	}
 	// When grpc-go deprecated grpc.DialContext() in favor of grpc.NewClient(),
 	// they made a breaking change to always use the DNS resolver, even when overriding the context dialer.
 	// This is problematic for clients that do not use DNS for address resolution and don't set a resolver in the address.
 	// As a workaround, use the passthrough resolver to prevent using the DNS resolver.
 	// More context can be found in this issue: https://github.com/grpc/grpc-go/issues/1786#issuecomment-2114124036
-	return "passthrough:" + namedpipe.AddrFromName(a.namedPipeName).String(), nil
+	return "passthrough:" + namedpipe.AddrFromName(namedPipeName).String(), nil
 }
