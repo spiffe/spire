80acd655b3c1762e774bdb119a0f56f57fd2a041
Carlo Teubner <cteubner1@bloomberg.net>
2026-06-20 19:46:53 +0100

    refactor: use standard `fs.FS` to simplify code (#7046)
    Simplify filesystem interfaces using stdlib
    
    Replace our custom FileSystem interface with the standard library's
    fs.FS interface, and benefit from its associated helpers.
    
    The only slight downside is that we must now open "proc/[pid]/cgroups"
    rather than "/proc/[pid]/cgroups", due to fs.FS interface semantics,
    which looks a little bit odd perhaps, but it seems like an overall win
    to me.
    
    Also delete OSFileSystem type, which was entirely unused.
    
    Signed-off-by: Carlo Teubner <cteubner1@bloomberg.net>

--- pkg/common/containerinfo/extract.go
+++ pkg/common/containerinfo/extract.go
@@ -5,7 +5,6 @@ package containerinfo
 import (
 	"errors"
 	"fmt"
-	"io"
 	"io/fs"
 	"os"
 	"path"
@@ -137,7 +136,7 @@ func (e *Extractor) extractPodUIDAndContainerIDFromMountInfo(pid int32, log hclo
 }
 
 func (e *Extractor) extractPodUIDAndContainerIDFromCGroups(pid int32, log hclog.Logger, extractPodUID bool) (types.UID, string, error) {
-	cgroups, err := cgroups.GetCgroups(pid, dirFS(e.RootDir))
+	cgroups, err := cgroups.GetCgroups(pid, os.DirFS(e.RootDir))
 	if err != nil {
 		if errors.Is(err, fs.ErrNotExist) {
 			return "", "", nil
@@ -163,12 +162,6 @@ func (e *Extractor) extractPodUIDAndContainerIDFromCGroups(pid int32, log hclog.
 		}
 	}
 	return ex.PodUID(), ex.ContainerID(), nil
-}
-
-type dirFS string
-
-func (d dirFS) Open(p string) (io.ReadCloser, error) {
-	return os.Open(filepath.Join(string(d), p))
 }
 
 type extractor struct {
