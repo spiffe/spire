0eb2b927ce1c68b391e35f3ca0c66f9cdfb51eb9
Sorin Dumitru <sorin@returnze.ro>
2026-07-17 10:21:18 +0100

    Move thest testing functionality out of the main DataStore interface
    Signed-off-by: Sorin Dumitru <sorin@returnze.ro>

--- pkg/common/telemetry/server/datastore/event.go
+++ pkg/common/telemetry/server/datastore/event.go
@@ -16,18 +16,6 @@ func StartPruneRegistrationEntryEventsCall(m telemetry.Metrics) *telemetry.CallC
 	return telemetry.StartCall(m, telemetry.Datastore, telemetry.RegistrationEntryEvent, telemetry.Prune)
 }
 
-// StartCreateRegistrationEntryEventForTestingCall return metric
-// for server's datastore, on creating a registration entry event.
-func StartCreateRegistrationEntryEventForTestingCall(m telemetry.Metrics) *telemetry.CallCounter {
-	return telemetry.StartCall(m, telemetry.Datastore, telemetry.RegistrationEntryEvent, telemetry.Create)
-}
-
-// StartDeleteRegistrationEntryEventForTestingCall return metric
-// for server's datastore, on deleting a registration entry event.
-func StartDeleteRegistrationEntryEventForTestingCall(m telemetry.Metrics) *telemetry.CallCounter {
-	return telemetry.StartCall(m, telemetry.Datastore, telemetry.RegistrationEntryEvent, telemetry.Delete)
-}
-
 // StartFetchRegistrationEntryEventCall return metric
 // for server's datastore, on fetching a registration entry event.
 func StartFetchRegistrationEntryEventCall(m telemetry.Metrics) *telemetry.CallCounter {
@@ -44,18 +32,6 @@ func StartListAttestedNodeEventsCall(m telemetry.Metrics) *telemetry.CallCounter
 // for server's datastore, on pruning attested node events.
 func StartPruneAttestedNodeEventsCall(m telemetry.Metrics) *telemetry.CallCounter {
 	return telemetry.StartCall(m, telemetry.Datastore, telemetry.NodeEvent, telemetry.Prune)
-}
-
-// StartCreateAttestedNodeEventForTestingCall return metric
-// for server's datastore, on creating an attested node event.
-func StartCreateAttestedNodeEventForTestingCall(m telemetry.Metrics) *telemetry.CallCounter {
-	return telemetry.StartCall(m, telemetry.Datastore, telemetry.NodeEvent, telemetry.Create)
-}
-
-// StartDeleteAttestedNodeEventForTestingCall return metric
-// for server's datastore, on deleting an attested node event.
-func StartDeleteAttestedNodeEventForTestingCall(m telemetry.Metrics) *telemetry.CallCounter {
-	return telemetry.StartCall(m, telemetry.Datastore, telemetry.NodeEvent, telemetry.Delete)
 }
 
 // StartFetchAttestedNodeEventCall return metric
