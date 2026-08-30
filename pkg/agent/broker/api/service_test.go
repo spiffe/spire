202bf5b6f72137de76eff3445ba0fdec8e7410d0
Sorin Dumitru <sorin@returnze.ro>
2026-08-30 07:04:57 +0100

    Add FetchJWTSVID tests
    Signed-off-by: Sorin Dumitru <sorin@returnze.ro>

--- pkg/agent/broker/api/service_test.go
+++ pkg/agent/broker/api/service_test.go
@@ -2,13 +2,24 @@ package api
 
 import (
 	"context"
+	"errors"
 	"net"
+	"net/url"
 	"testing"
+	"time"
 
+	"github.com/sirupsen/logrus/hooks/test"
+	"github.com/spiffe/go-spiffe/v2/exp/proto/spiffe/broker"
 	"github.com/spiffe/go-spiffe/v2/spiffeid"
+	"github.com/spiffe/spire/pkg/agent/api/rpccontext"
+	workloadattestor "github.com/spiffe/spire/pkg/agent/attestor/workload"
+	"github.com/spiffe/spire/pkg/agent/client"
+	"github.com/spiffe/spire/pkg/agent/manager"
+	"github.com/spiffe/spire/proto/spire/common"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/anypb"
@@ -170,6 +181,160 @@ func TestAuthorizeReferenceType(t *testing.T) {
 			}
 			require.Error(t, err)
 			assert.Equal(t, tt.expectCode, status.Code(err))
+		})
+	}
+}
+
+func brokerCallerContext(caller spiffeid.ID) context.Context {
+	spiffeURL, _ := url.Parse(caller.String())
+	log, _ := test.NewNullLogger()
+	ctx := rpccontext.WithLogger(context.Background(), log)
+	return peer.NewContext(ctx, &peer.Peer{
+		Addr:     &net.UnixAddr{Name: "/tmp/broker.sock", Net: "unix"},
+		AuthInfo: credentials.TLSInfo{SPIFFEID: spiffeURL},
+	})
+}
+
+var _ workloadattestor.Attestor = fakeAttestor{}
+
+type fakeAttestor struct {
+	selectors []*common.Selector
+	err       error
+}
+
+func (a fakeAttestor) Attest(context.Context, int) ([]*common.Selector, error) {
+	return a.selectors, a.err
+}
+
+func (a fakeAttestor) AttestReference(context.Context, *anypb.Any) ([]*common.Selector, error) {
+	return a.selectors, a.err
+}
+
+type fakeManager struct {
+	manager.Manager
+
+	entries  []*common.RegistrationEntry
+	jwtSVIDs map[string]*client.JWTSVID
+	err      error
+}
+
+func (m *fakeManager) MatchingRegistrationEntries([]*common.Selector) []*common.RegistrationEntry {
+	return m.entries
+}
+
+func (m *fakeManager) FetchJWTSVID(_ context.Context, entry *common.RegistrationEntry, _ []string) (*client.JWTSVID, error) {
+	if m.err != nil {
+		return nil, m.err
+	}
+	svid, ok := m.jwtSVIDs[entry.SpiffeId]
+	if !ok {
+		return nil, errors.New("no SVID for entry")
+	}
+	return svid, nil
+}
+
+func TestFetchJWTSVID(t *testing.T) {
+	caller := spiffeid.RequireFromString("spiffe://example.org/broker")
+	workloadID := "spiffe://example.org/workload"
+	adminID := "spiffe://example.org/admin"
+	downstreamID := "spiffe://example.org/downstream"
+
+	jwtSVID := &client.JWTSVID{
+		Token:     "jwt-token",
+		IssuedAt:  time.Unix(1000, 0),
+		ExpiresAt: time.Unix(2000, 0),
+	}
+
+	for _, tt := range []struct {
+		name        string
+		audience    []string
+		attestErr   error
+		fetchErr    error
+		entries     []*common.RegistrationEntry
+		expectCode  codes.Code
+		expectSvids []*broker.JWTSVID
+	}{
+		{
+			name:       "missing audience",
+			expectCode: codes.InvalidArgument,
+		},
+		{
+			name:       "attestation error",
+			audience:   []string{"aud"},
+			attestErr:  errors.New("attest failed"),
+			expectCode: codes.Unauthenticated,
+		},
+		{
+			name:     "success",
+			audience: []string{"aud"},
+			entries: []*common.RegistrationEntry{
+				{EntryId: "1", SpiffeId: workloadID},
+			},
+			expectCode: codes.OK,
+			expectSvids: []*broker.JWTSVID{
+				{SpiffeId: workloadID, Svid: jwtSVID.Token},
+			},
+		},
+		{
+			name:     "admin entries are excluded",
+			audience: []string{"aud"},
+			entries: []*common.RegistrationEntry{
+				{EntryId: "1", SpiffeId: adminID, Admin: true},
+			},
+			expectCode: codes.PermissionDenied,
+		},
+		{
+			name:     "downstream entries are excluded",
+			audience: []string{"aud"},
+			entries: []*common.RegistrationEntry{
+				{EntryId: "1", SpiffeId: downstreamID, Downstream: true},
+			},
+			expectCode: codes.PermissionDenied,
+		},
+		{
+			name:     "admin and downstream excluded, normal entry returned",
+			audience: []string{"aud"},
+			entries: []*common.RegistrationEntry{
+				{EntryId: "1", SpiffeId: adminID, Admin: true},
+				{EntryId: "2", SpiffeId: downstreamID, Downstream: true},
+				{EntryId: "3", SpiffeId: workloadID},
+			},
+			expectCode: codes.OK,
+			expectSvids: []*broker.JWTSVID{
+				{SpiffeId: workloadID, Svid: jwtSVID.Token},
+			},
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			s := New(Config{
+				Manager: &fakeManager{
+					entries: tt.entries,
+					jwtSVIDs: map[string]*client.JWTSVID{
+						workloadID: jwtSVID,
+					},
+					err: tt.fetchErr,
+				},
+				Attestor: fakeAttestor{err: tt.attestErr},
+			})
+
+			resp, err := s.FetchJWTSVID(brokerCallerContext(caller), &broker.FetchJWTSVIDRequest{
+				Audience:  tt.audience,
+				Reference: &broker.WorkloadReference{Reference: ref(k8sType)},
+			})
+
+			if tt.expectCode != codes.OK {
+				require.Error(t, err)
+				assert.Equal(t, tt.expectCode, status.Code(err))
+				assert.Nil(t, resp)
+				return
+			}
+
+			require.NoError(t, err)
+			require.Len(t, resp.Svids, len(tt.expectSvids))
+			for i, want := range tt.expectSvids {
+				assert.Equal(t, want.SpiffeId, resp.Svids[i].SpiffeId)
+				assert.Equal(t, want.Svid, resp.Svids[i].Svid)
+			}
 		})
 	}
 }
